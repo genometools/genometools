58f1151c2835c8d795a3d6724c74f02463ad4109
Gordon Gremme <gremme@zbh.uni-hamburg.de>
2007-02-09 15:29:33 +0100

    make_debug.sh script added

---
+++ scripts/make_debug.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+# Copyright (c) 2007 Gordon Gremme <gremme@zbh.uni-hamburg.de>
+# Copyright (c) 2007 Center for Bioinformatics, University of Hamburg
+# See LICENSE file or http://genometools.org/license.html for license details.
+#
+
+# the make call used to compile a development version
+make cleanup && make opt=no CFLAGS=-Werror
