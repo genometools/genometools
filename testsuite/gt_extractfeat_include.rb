54ea3b698cc7fa0b1bf0e2a9661c2e8c640bbbab
Gordon Gremme <gremme@zbh.uni-hamburg.de>
2007-02-12 23:57:42 +0100

    failing `gt extractfeat -regionmapping` test added

--- testsuite/gt_extractfeat_include.rb
+++ testsuite/gt_extractfeat_include.rb
@@ -1,34 +1,41 @@
-Name "gt extractfeat test 1"
+Name "gt extractfeat -seqfile test 1"
 Keywords "gt_extractfeat"
 Test do
   run_test "#{$bin}gt extractfeat -type gene -seqfile #{$testdata}/gt_extractfeat_succ_1.fas #{$testdata}/gt_extractfeat_succ_1.gff3"
   run "diff #{$last_stdout} #{$testdata}/gt_extractfeat_succ_1.out"
 end
 
-Name "gt extractfeat test 2"
+Name "gt extractfeat -seqfile test 2"
 Keywords "gt_extractfeat"
 Test do
   run_test "#{$bin}gt extractfeat -type gene -seqfile #{$testdata}/gt_extractfeat_succ_2.fas #{$testdata}/gt_extractfeat_succ_2.gff3"
   run "diff #{$last_stdout} #{$testdata}/gt_extractfeat_succ_2.out1"
 end
 
-Name "gt extractfeat test 3"
+Name "gt extractfeat -seqfile test 3"
 Keywords "gt_extractfeat"
 Test do
   run_test "#{$bin}gt extractfeat -type exon -seqfile #{$testdata}/gt_extractfeat_succ_2.fas #{$testdata}/gt_extractfeat_succ_2.gff3"
   run "diff #{$last_stdout} #{$testdata}/gt_extractfeat_succ_2.out2"
 end
 
-Name "gt extractfeat test 4"
+Name "gt extractfeat -seqfile test 4"
 Keywords "gt_extractfeat"
 Test do
   run_test "#{$bin}gt extractfeat -type exon -join -seqfile #{$testdata}/gt_extractfeat_succ_2.fas #{$testdata}/gt_extractfeat_succ_2.gff3"
   run "diff #{$last_stdout} #{$testdata}/gt_extractfeat_succ_2.out3"
 end
 
-Name "gt extractfeat test 5"
+Name "gt extractfeat -seqfile test 5"
 Keywords "gt_extractfeat"
 Test do
   run_test "#{$bin}gt extractfeat -type exon -join -seqfile #{$testdata}/gt_extractfeat_succ_3.fas #{$testdata}/gt_extractfeat_succ_3.gff3"
   run "diff #{$last_stdout} #{$testdata}/gt_extractfeat_succ_3.out"
+end
+
+Name "gt extractfeat -regionmapping fail 1 (no map file)"
+Keywords "gt_extractfeat"
+Test do
+  run_test("#{$bin}gt extractfeat -type exon -regionmapping #{$testdata}/nonexistent_file #{$testdata}/gt_extractfeat_succ_1.gff3", :retval => 1 )
+  run "diff #{$last_stdout} #{$testdata}/gt_extractfeat_fail_1.out"
 end
