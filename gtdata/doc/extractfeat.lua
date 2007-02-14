0fcf1b67def0082008c8d11f766e692c0481fdb0
Gordon Gremme <gremme@zbh.uni-hamburg.de>
2007-02-14 17:15:04 +0100

    parts of extractfeat.lua commented out

--- gtdata/doc/extractfeat.lua
+++ gtdata/doc/extractfeat.lua
@@ -1,4 +1,5 @@
 print ([[
+
 File format for option -regionmapping:
 
 The file supplied to option -regionmapping defines a ``mapping''.  A mapping
@@ -6,11 +7,14 @@ maps the sequence-regions given in the GFF3_file to a sequence file containing
 the corresponding sequence. Mappings can be defined in of the following two
 forms:
 ]])
+--[=[
 print(io.open("regionmapping_table.lua"):read("*a"))
 print([[
 or
 ]])
 print(io.open("regionmapping_function.lua"):read("*a"))
+]=]
+
 print([[
 The first form defines a Lua (http://www.lua.org/) table named ``mapping''
 which maps each sequence region to the corresponding sequence file.
