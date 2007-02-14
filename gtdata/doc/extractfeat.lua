1d9dec9106e3e29215f3e8355ad3ad2bc6ad7125
Gordon Gremme <gremme@zbh.uni-hamburg.de>
2007-02-14 14:58:31 +0100

    docu files for `gt extracteat` added

---
+++ gtdata/doc/extractfeat.lua
@@ -0,0 +1,19 @@
+print ([[
+File format for option -regionmapping:
+
+The file supplied to option -regionmapping defines a ``mapping''.  A mapping
+maps the sequence-regions given in the GFF3_file to a sequence file containing
+the corresponding sequence. Mappings can be defined in of the following two
+forms:
+]])
+print(io.open("regionmapping_table.lua"):read("*a"))
+print([[
+or
+]])
+print(io.open("regionmapping_function.lua"):read("*a"))
+print([[
+The first form defines a Lua (http://www.lua.org/) table named ``mapping''
+which maps each sequence region to the corresponding sequence file.
+The second one defines a Lua function ``mapping'', which has to return the
+sequence file name when it is called with the sequence_region as argument.
+]])
