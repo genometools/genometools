7bab6d465118e9dde4c075cc75be138cad25f5a1
Gordon Gremme <gremme@zbh.uni-hamburg.de>
2007-02-15 18:41:24 +0100

    cosmetics

--- src/libgt/genome_feature.h
+++ src/libgt/genome_feature.h
@@ -1,6 +1,6 @@
 /*
-  Copyright (c) 2006 Gordon Gremme <gremme@zbh.uni-hamburg.de>
-  Copyright (c) 2006 Center for Bioinformatics, University of Hamburg
+  Copyright (c) 2006-2007 Gordon Gremme <gremme@zbh.uni-hamburg.de>
+  Copyright (c) 2006-2007 Center for Bioinformatics, University of Hamburg
   See LICENSE file or http://genometools.org/license.html for license details.
 */
 
@@ -17,18 +17,18 @@ typedef struct Genome_feature Genome_feature;
 #include "strand.h"
 
 const GenomeNodeClass* genome_feature_class(void);
-GenomeNode*        genome_feature_new(GenomeFeatureType, Range, Strand,
-                                       const char *filename,
-                                       unsigned long line_number);
-const char*         genome_feature_get_source(Genome_feature*);
-GenomeFeatureType genome_feature_get_type(Genome_feature*);
-double              genome_feature_get_score(Genome_feature*);
-Strand              genome_feature_get_strand(Genome_feature*);
-Phase               genome_feature_get_phase(Genome_feature*);
-void                genome_feature_get_exons(Genome_feature*,
-                                             Array *exon_features);
-void                genome_feature_set_end(Genome_feature*, unsigned long);
+GenomeNode*            genome_feature_new(GenomeFeatureType, Range, Strand,
+                                          const char *filename,
+                                          unsigned long line_number);
+const char*            genome_feature_get_source(Genome_feature*);
+GenomeFeatureType      genome_feature_get_type(Genome_feature*);
+double                 genome_feature_get_score(Genome_feature*);
+Strand                 genome_feature_get_strand(Genome_feature*);
+Phase                  genome_feature_get_phase(Genome_feature*);
+void                   genome_feature_get_exons(Genome_feature*,
+                                                Array *exon_features);
+void                   genome_feature_set_end(Genome_feature*, unsigned long);
 /* XXX: move to genome_node */
-void                genome_feature_set_score(Genome_feature*, double);
+void                   genome_feature_set_score(Genome_feature*, double);
 
 #endif
