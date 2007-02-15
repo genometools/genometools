b4b3882bf1d9b22bb7e39dd6800ea38ea0424752
Gordon Gremme <gremme@zbh.uni-hamburg.de>
2007-02-15 17:22:55 +0100

    ctags.sh script added

---
+++ scripts/ctags.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+# Copyright (c) 2007 Gordon Gremme <gremme@zbh.uni-hamburg.de>
+# Copyright (c) 2007 Center for Bioinformatics, University of Hamburg
+# See LICENSE file or http://genometools.org/license.html for license details.
+#
+
+# make a new tags file
+ctags src/*.[ch] src/libgt/*.[ch] src/lua-5.1.1/src/*.[ch] src/tools/*.[ch]
