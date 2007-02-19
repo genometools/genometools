32c567bf92b428b954bac0a32c5acb7261d9c281
Gordon Gremme <gremme@zbh.uni-hamburg.de>
2007-02-19 18:50:33 +0100

    cleanup

--- src/libgt/array2dim.h
+++ src/libgt/array2dim.h
@@ -9,20 +9,20 @@
 
 #include "xansi.h"
 
-#define array2dim_malloc(ARRAY2DIM, ROWS, COLUMNS, TYPE)             \
-        {                                                            \
-          unsigned long i;                                           \
+#define array2dim_malloc(ARRAY2DIM, ROWS, COLUMNS, TYPE)              \
+        {                                                             \
+          unsigned long i;                                            \
 	  ARRAY2DIM = xmalloc(sizeof (TYPE*) * (ROWS));               \
 	  ARRAY2DIM[0] = xmalloc(sizeof (TYPE) * (ROWS) * (COLUMNS)); \
-	  for (i = 1; i < (ROWS); i++)                               \
-            ARRAY2DIM[i] = ARRAY2DIM[i-1] + (COLUMNS);               \
+	  for (i = 1; i < (ROWS); i++)                                \
+            ARRAY2DIM[i] = ARRAY2DIM[i-1] + (COLUMNS);                \
         }
 
 #define array2dim_calloc(ARRAY2DIM, ROWS, COLUMNS, TYPE)             \
         {                                                            \
           unsigned long i;                                           \
-	  ARRAY2DIM = xmalloc(sizeof (TYPE*) * (ROWS));               \
-	  ARRAY2DIM[0] = xcalloc((ROWS) * (COLUMNS), sizeof (TYPE));  \
+	  ARRAY2DIM = xmalloc(sizeof (TYPE*) * (ROWS));              \
+	  ARRAY2DIM[0] = xcalloc((ROWS) * (COLUMNS), sizeof (TYPE)); \
 	  for (i = 1; i < (ROWS); i++)                               \
             ARRAY2DIM[i] = ARRAY2DIM[i-1] + (COLUMNS);               \
         }
@@ -36,9 +36,9 @@
 
   double **a2dim;
 
-  ARRAY2DIM_MALLOC(a2dim, 10, 20, double); /* create a 10 * 20 double array */
+  array2dim_malloc(a2dim, 10, 20, double); /* create a 10 * 20 double array */
   /* ... (use array a2dim in conventional way via a2dim[row][column]) */
-  ARRAY2DIM_FREE(a2dim);
+  array2dim_free(a2dim);
 #endif
 
 #endif
