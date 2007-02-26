599d6d5bab687429ea4bf6c0b1408609b5324185
Gordon Gremme <gremme@zbh.uni-hamburg.de>
2007-02-26 01:20:20 +0100

    test for `gt stat -intronlengthdistri` added

--- testsuite/gt_stat_include.rb
+++ testsuite/gt_stat_include.rb
@@ -19,11 +19,9 @@ Test do
   run "diff #{$last_stdout} #{$testdata}gt_stat_test_3.out"
 end
 
-=begin
 Name "gt stat test 4 (-intronlengthdistri)"
 Keywords "gt_stat"
 Test do
   run "#{$bin}gt stat -intronlengthdistri #{$testdata}standard_gene_with_introns_as_tree.gff3"
   run "diff #{$last_stdout} #{$testdata}gt_stat_test_4.out"
 end
-=end
