115037831671268005655f413260505cf9b50509
Gordon Gremme <gremme@zbh.uni-hamburg.de>
2007-03-01 15:34:18 +0100

    variable naming improved

--- src/libgt/transcript_bittabs.c
+++ src/libgt/transcript_bittabs.c
@@ -20,52 +20,52 @@ TranscriptBittabs* transcript_bittabs_new(unsigned long size_all,
                                           unsigned long size_internal,
                                           unsigned long size_terminal, Env *env)
 {
-  TranscriptBittabs *tc = env_ma_calloc(env, 1, sizeof (TranscriptBittabs));
-  if (size_all) tc->bittab_all = bittab_new(size_all, env);
-  if (size_single) tc->bittab_single = bittab_new(size_single, env);
-  if (size_initial) tc->bittab_initial = bittab_new(size_initial, env);
-  if (size_internal) tc->bittab_internal = bittab_new(size_internal, env);
-  if (size_terminal) tc->bittab_terminal = bittab_new(size_all, env);
-  return tc;
+  TranscriptBittabs *tb = env_ma_calloc(env, 1, sizeof (TranscriptBittabs));
+  if (size_all) tb->bittab_all = bittab_new(size_all, env);
+  if (size_single) tb->bittab_single = bittab_new(size_single, env);
+  if (size_initial) tb->bittab_initial = bittab_new(size_initial, env);
+  if (size_internal) tb->bittab_internal = bittab_new(size_internal, env);
+  if (size_terminal) tb->bittab_terminal = bittab_new(size_all, env);
+  return tb;
 }
 
-Bittab* transcript_bittabs_get_all(const TranscriptBittabs *tc)
+Bittab* transcript_bittabs_get_all(const TranscriptBittabs *tb)
 {
-  assert(tc);
-  return tc->bittab_all;
+  assert(tb);
+  return tb->bittab_all;
 }
 
-Bittab* transcript_bittabs_get_single(const TranscriptBittabs *tc)
+Bittab* transcript_bittabs_get_single(const TranscriptBittabs *tb)
 {
-  assert(tc);
-  return tc->bittab_single;
+  assert(tb);
+  return tb->bittab_single;
 }
 
-Bittab* transcript_bittabs_get_initial(const TranscriptBittabs *tc)
+Bittab* transcript_bittabs_get_initial(const TranscriptBittabs *tb)
 {
-  assert(tc);
-  return tc->bittab_initial;
+  assert(tb);
+  return tb->bittab_initial;
 }
 
-Bittab* transcript_bittabs_get_internal(const TranscriptBittabs *tc)
+Bittab* transcript_bittabs_get_internal(const TranscriptBittabs *tb)
 {
-  assert(tc);
-  return tc->bittab_internal;
+  assert(tb);
+  return tb->bittab_internal;
 }
 
-Bittab* transcript_bittabs_get_terminal(const TranscriptBittabs *tc)
+Bittab* transcript_bittabs_get_terminal(const TranscriptBittabs *tb)
 {
-  assert(tc);
-  return tc->bittab_terminal;
+  assert(tb);
+  return tb->bittab_terminal;
 }
 
-void transcript_bittabs_delete(TranscriptBittabs *tc, Env *env)
+void transcript_bittabs_delete(TranscriptBittabs *tb, Env *env)
 {
-  if (!tc) return;
-  bittab_delete(tc->bittab_all, env);
-  bittab_delete(tc->bittab_single, env);
-  bittab_delete(tc->bittab_initial, env);
-  bittab_delete(tc->bittab_internal, env);
-  bittab_delete(tc->bittab_terminal, env);
-  env_ma_free(tc, env);
+  if (!tb) return;
+  bittab_delete(tb->bittab_all, env);
+  bittab_delete(tb->bittab_single, env);
+  bittab_delete(tb->bittab_initial, env);
+  bittab_delete(tb->bittab_internal, env);
+  bittab_delete(tb->bittab_terminal, env);
+  env_ma_free(tb, env);
 }
