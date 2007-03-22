b339466a2de8d4fbfca83216603f93d63fd56688
Gordon Gremme <gremme@zbh.uni-hamburg.de>
2007-03-22 12:09:34 +0100

    option_ref() added
    Allows to keep references to options after the option parser has been deleted

--- src/libgtcore/option.h
+++ src/libgtcore/option.h
@@ -105,6 +105,7 @@ Option*        option_new_ulong_min(const char *option_str,
 Option*        option_new_string(const char *option_str,
                                  const char *description,
                                  Str *value, const char *default_value, Env*);
+Option*        option_ref(Option*);
 void           option_is_mandatory(Option*);
 void           option_is_mandatory_either(Option*, const Option*);
 void           option_is_development_option(Option*);
