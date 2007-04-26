c0048257084d4cc67ae9d378ff87500a777466d0
Gordon Gremme <gremme@zbh.uni-hamburg.de>
2007-04-26 18:24:05 +0200

    header cosmetics

--- src/tools/gt_gtf_to_gff3.h
+++ src/tools/gt_gtf_to_gff3.h
@@ -4,8 +4,8 @@
   See LICENSE file or http://genometools.org/license.html for license details.
 */
 
-#ifndef GT_GTF2GFF3_H
-#define GT_GTF2GFF3_H
+#ifndef GT_GTF_TO_GFF3_H
+#define GT_GTF_TO_GFF3_H
 
 /* the GTF to GFF3 tool */
 int gt_gtf_to_gff3(int argc, const char **argv, Env*);
