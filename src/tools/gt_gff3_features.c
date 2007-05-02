006eba635a1b8362265999a7e8c783c819e51e94
Sascha Steinbiss <satta@ebony.(none)>
2007-05-02 16:25:23 +0200

    Added source files for the new tool.

---
+++ src/tools/gt_gff3_features.c
@@ -0,0 +1,107 @@
+/*
+  Copyright (c) 2005-2007 Gordon Gremme <gremme@zbh.uni-hamburg.de>
+  Copyright (c) 2005-2007 Center for Bioinformatics, University of Hamburg
+  See LICENSE file or http://genometools.org/license.html for license details.
+*/
+
+#include "gt.h"
+
+typedef struct {
+  bool verbose;
+  long offset;
+  GenFile *outfp;
+} Gff3_features_arguments;
+
+static OPrval parse_options(int *parsed_args, Gff3_features_arguments *arguments,
+                            int argc, const char **argv, Env *env)
+{
+  OptionParser *op;
+  OutputFileInfo *ofi;
+  Option  *option;
+  OPrval oprval;
+  env_error_check(env);
+
+  /* init */
+  op = option_parser_new("[option ...] [GFF3_file ...]",
+                         "Work with and test GFF3 feature indexes.",
+                         env);
+  ofi = outputfileinfo_new(env);
+
+  /* -offset */
+  option = option_new_long("offset",
+                           "transform all features by the given offset",
+                           &arguments->offset, UNDEF_LONG, env);
+  /* XXX: do not make this a ``normal option'' until the necessary envor checks
+     have been added to range_offset() in range.c */
+  option_is_development_option(option);
+  option_parser_add_option(op, option, env);
+
+  /* -v */
+  option = option_new_verbose(&arguments->verbose, env);
+  option_parser_add_option(op, option, env);
+
+  /* output file options */
+  outputfile_register_options(op, &arguments->outfp, ofi, env);
+
+  /* parse options */
+  oprval = option_parser_parse(op, parsed_args, argc, argv, versionfunc, env);
+
+  /* free */
+  outputfileinfo_delete(ofi, env);
+  option_parser_delete(op, env);
+
+  return oprval;
+}
+
+int gt_gff3_features(int argc, const char **argv, Env *env)
+{
+  GenomeStream *gff3_in_stream,
+							 *feature_stream = NULL;
+/*               *gff3_out_stream; */
+  Gff3_features_arguments arguments;
+  GenomeNode *gn;
+	Hashtable *features = NULL;
+  int parsed_args, has_err;
+  env_error_check(env);
+
+  /* option parsing */
+  switch (parse_options(&parsed_args, &arguments, argc, argv, env)) {
+    case OPTIONPARSER_OK: break;
+    case OPTIONPARSER_ERROR: return -1;
+    case OPTIONPARSER_REQUESTS_EXIT: return 0;
+  }
+
+  /* create a gff3 input stream */
+  gff3_in_stream = gff3_in_stream_new_unsorted(argc - parsed_args,
+                                               argv + parsed_args,
+                                               arguments.verbose &&
+                                               arguments.outfp, env);
+
+
+  /* set offset (if necessary) */
+  if (arguments.offset != UNDEF_LONG)
+    gff3_in_stream_set_offset(gff3_in_stream, arguments.offset);
+
+	
+	/* create feature stream */
+		features = hashtable_new(HASH_STRING, NULL, (FreeFunc) array_delete, env);
+    feature_stream = feature_stream_new(gff3_in_stream, features, env);
+
+  /* create gff3 output stream */
+  /* gff3_out_stream = gff3_out_stream_new(feature_stream, arguments.outfp, env); */
+
+  /* pull the features through the stream and free them afterwards */
+  while (!(has_err = genome_stream_next_tree(feature_stream, &gn, env)) &&
+         gn) {
+    genome_node_rec_delete(gn, env);
+  }
+
+  /* free */
+/*   genome_stream_delete(gff3_out_stream, env); */
+  genome_stream_delete(feature_stream, env);
+  genome_stream_delete(gff3_in_stream, env);
+	hashtable_delete(features, env);
+  genfile_xclose(arguments.outfp, env);
+
+  return has_err;
+}
---
+++ src/tools/gt_gff3_features.h
@@ -0,0 +1,13 @@
+/*
+  Copyright (c) 2005-2007 Gordon Gremme <gremme@zbh.uni-hamburg.de>
+  Copyright (c) 2005-2007 Center for Bioinformatics, University of Hamburg
+  See LICENSE file or http://genometools.org/license.html for license details.
+*/
+
+#ifndef GT_GFF3_FEATURES_H
+#define GT_GFF3_FEATURES_H
+
+/* the GFF3 tool */
+int gt_gff3_features(int argc, const char **argv, Env*);
+
+#endif
