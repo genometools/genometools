ae282e4bff7213535df229cbd32cb4a6cbd249d5
Gordon Gremme <gremme@zbh.uni-hamburg.de>
2007-05-30 11:27:30 +0200

    test files for the png tool have been added

---
+++ testdata/gt_dev_png.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -ex
+
+gt dev png -force png_devel_1.png png_devel_1.gff3
+gt dev png -force png_devel_2.png png_devel_2.gff3
+gt dev png -force png_devel_3.png png_devel_3.gff3
+gt dev png -force png_devel_4.png png_devel_4.gff3
+gt dev png -force png_devel_5.png png_devel_5.gff3
+gt dev png -force png_devel_6.png png_devel_6.gff3
+gt dev png -force png_devel_7.png png_devel_7.gff3
+gt dev png -from 190000 -to 199000 -force png_devel_8.png png_devel_8.gff3
+gt dev png -from 190000 -to 199000 -force png_devel_9.png png_devel_9.gff3
+gt dev png -from 190000 -to 199000 -force png_devel_10.png png_devel_10.gff3
+gt dev png -from 190000 -to 199000 -force png_devel_11.png png_devel_11.gff3
+gt dev png -from 5867900 -to 5872900 -force png_devel_12.png png_devel_12.gff3
