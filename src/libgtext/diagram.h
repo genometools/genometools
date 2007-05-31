310039acca388204700fef15064783729e18fd49
Malte Mader <mmader@zbh.uni-hamburg.de>
2007-05-31 18:36:56 +0000

    diagram unit test + comment

--- src/libgtext/diagram.h
+++ src/libgtext/diagram.h
@@ -4,18 +4,22 @@
   See LICENSE file or http://genometools.org/license.html for license details.
 */
 
-#ifndef FEATUREINDEX_H
-#define FEATUREINDEX_H
+#ifndef DIAGRAM_H
+#define DIAGRAM_H
 
 #include <libgtcore/array.h>
-#include <libgtcore/str.h>
 #include <libgtext/config.h>
 #include <libgtcore/range.h>
+#include <libgtcore/hashtable.h>
+
 
 typedef struct Diagram Diagram;
 
 Diagram* diagram_new(Array*,Range,Config*,Env*);
 void diagram_set_config(Diagram*,Config*,Env*);
+Hashtable* diagram_get_tracks(Diagram*);
+int* diagram_get_total_lines(Diagram*, Env*);
 void diagram_delete(Diagram*,Env*);
+int diagram_unit_test(Env*);
 
 #endif
