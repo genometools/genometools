66ba1a13434b1a9a6031b9284f7d040548cb8509
Stefan Kurtz <kurtz@zbh.uni-hamburg.de>
2007-06-01 18:54:04 +0200

    add Atinsert.fna testcase.

--- testsuite/gt_suffixerator_include.rb
+++ testsuite/gt_suffixerator_include.rb
@@ -1,7 +1,8 @@
 Name "gt suffixerator"
 Keywords "gt_suffixerator"
 Test do
-  run "#{$bin}gt suffixerator -pl 3 -tis -suf -indexname sfx #{$testdata}Random.fna"
+  run "#{$bin}gt suffixerator -pl 3 -tis -suf -indexname sfx " +
+      "#{$testdata}Random.fna #{$testdata}Atinsert.fna"
   run "#{$bin}gt dev sfxmap sfx"
   run "grep -v '^#' #{$last_stdout}"
   run "cmp -s sfx.prj #{$last_stdout}"
