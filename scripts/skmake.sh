ffbe348ad3d86dabb7d1bb9c5d4238f81bc0ae28
Stefan Kurtz <kurtz@zbh.uni-hamburg.de>
2007-06-02 01:04:21 +0200

    add make and test script for Stefan Kurtz
    add protein test data
    add new cases
    
    	new file:   scripts/skmake.sh
    	new file:   scripts/sktest.sh
    	new file:   testdata/sw100K1.fna
    	new file:   testdata/sw100K2.fna
    	modified:   testsuite/gt_suffixerator_include.rb

---
+++ scripts/skmake.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+# Copyright (c) 2007 Gordon Gremme <gremme@zbh.uni-hamburg.de>
+# Copyright (c) 2007 Center for Bioinformatics, University of Hamburg
+# See LICENSE file or http://genometools.org/license.html for license details.
+#
+
+# the make call normally used for development
+gmake CC='ccache gcc' CFLAGS=-Werror
--- testsuite/gt_suffixerator_include.rb
+++ testsuite/gt_suffixerator_include.rb
@@ -1,9 +1,46 @@
-Name "gt suffixerator"
-Keywords "gt_suffixerator"
-Test do
-  run "#{$bin}gt suffixerator -pl 3 -tis -suf -indexname sfx " +
-      "#{$testdata}Random.fna #{$testdata}Atinsert.fna"
+def checkmapped(parts,extraargs,filelist)
+  filearg=""
+  filelist.each do |filename|
+    filearg += "#{$testdata}#{filename} "
+  end
+  run "#{$bin}gt suffixerator -parts #{parts} #{extraargs} -tis -suf -indexname sfx " +
+       filearg
   run "#{$bin}gt dev sfxmap sfx"
   run "grep -v '^#' #{$last_stdout}"
   run "cmp -s sfx.prj #{$last_stdout}"
+end
+
+1.upto(3) do |parts|
+  1.upto(2) do |withsmap|
+    if withsmap == 1
+      extra="-smap TransProt11"
+    else
+      extra=""
+    end
+    Name "gt suffixerator protein #{extra} #{parts} parts"
+    Keywords "gt_suffixerator"
+    Test do
+      checkmapped(parts,"-pl 3 " + extra,["sw100K1.fna","sw100K2.fna"])
+    end
+  end
+end
+
+1.upto(3) do |parts|
+  1.upto(2) do |withsmap|
+    if withsmap == 1
+      extra="-smap TransDNA"
+    else
+      extra=""
+    end
+    Name "gt suffixerator dna #{extra} #{parts} parts"
+    Keywords "gt_suffixerator"
+    Test do
+      checkmapped(parts,"-pl 3 " + extra,["Random-Small.fna"])
+      checkmapped(parts,"-pl 3 " + extra,["Random.fna"])
+      checkmapped(parts,"-pl 3 " + extra,["RandomN.fna"])
+      checkmapped(parts,"-pl 3 " + extra,["Random.fna","Atinsert.fna"])
+      checkmapped(parts,"-pl 3 " + extra,["RandomN.fna","Random.fna","Atinsert.fna"])
+      checkmapped(parts,"-pl 7 " + extra,["RandomN.fna","Random.fna","Atinsert.fna"])
+    end
+  end
 end
