c75058d13fbb5f078f127fd746034a96db53b358
Gordon Gremme <gremme@zbh.uni-hamburg.de>
2007-08-23 19:31:58 +0200

    fix documentation bug

--- src/libgtcore/phase.h
+++ src/libgtcore/phase.h
@@ -1,6 +1,6 @@
 /*
-  Copyright (c) 2006 Gordon Gremme <gremme@zbh.uni-hamburg.de>
-  Copyright (c) 2006 Center for Bioinformatics, University of Hamburg
+  Copyright (c) 2006-2007 Gordon Gremme <gremme@zbh.uni-hamburg.de>
+  Copyright (c) 2006-2007 Center for Bioinformatics, University of Hamburg
   See LICENSE file or http://genometools.org/license.html for license details.
 */
 
@@ -16,7 +16,7 @@ typedef enum {
 
 #define PHASECHARS "012."
 
-/* returns NUM_OF_PHASES if phase_char is not a valid one */
+/* an assertion will fail if <phase_char> is not a valid one */
 Phase phase_get(char phase_char);
 
 #endif
