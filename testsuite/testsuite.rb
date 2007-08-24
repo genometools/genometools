80f88394be5bf1f398a31a75f5c53e38705a86bd
Gordon Gremme <gremme@zbh.uni-hamburg.de>
2007-08-24 13:37:29 +0200

    cosmetics

--- testsuite/testsuite.rb
+++ testsuite/testsuite.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/env ruby
-# 
+#
 # Copyright (c) 2006-2007 Gordon Gremme <gremme@zbh.uni-hamburg.de>
-# Copyright (c) 2006-2007 Center for Bioinformatics, University of Hamburg 
+# Copyright (c) 2006-2007 Center for Bioinformatics, University of Hamburg
 # See LICENSE file or http://genometools.org/license.html for license details.
-# 
+#
 
 # the GenomeTools test suite (employs ``stest'').
 
