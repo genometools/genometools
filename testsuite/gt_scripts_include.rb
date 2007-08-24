d3f9b1c0eebcb06842e2286f3044074505654a93
Gordon Gremme <gremme@zbh.uni-hamburg.de>
2007-08-24 14:15:20 +0200

    Revert "*.lua -> *.gts (GenomeTools script)"
    This reverts commit 4d22dc7e4e1b01fdee38fa812afc166494a4578c.

--- testsuite/gt_scripts_include.rb
+++ testsuite/gt_scripts_include.rb
@@ -1,17 +1,17 @@
 Name "bittab bindings"
 Keywords "gt_scripts"
 Test do
-  run_test "#{$bin}gt #{$testdata}/gtscripts/bittab.gts"
+  run_test "#{$bin}gt #{$testdata}/gtscripts/bittab.lua"
 end
 
 Name "genome_node bindings"
 Keywords "gt_scripts"
 Test do
-  run_test "#{$bin}gt #{$testdata}/gtscripts/genome_node.gts"
+  run_test "#{$bin}gt #{$testdata}/gtscripts/genome_node.lua"
 end
 
 Name "genome_stream bindings"
 Keywords "gt_scripts"
 Test do
-  run_test "#{$bin}gt #{$testdata}/gtscripts/genome_stream.gts"
+  run_test "#{$bin}gt #{$testdata}/gtscripts/genome_stream.lua"
 end
