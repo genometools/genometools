4d22dc7e4e1b01fdee38fa812afc166494a4578c
Gordon Gremme <gremme@zbh.uni-hamburg.de>
2007-08-24 12:03:18 +0200

    *.lua -> *.gts (GenomeTools script)

--- testsuite/gt_scripts_include.rb
+++ testsuite/gt_scripts_include.rb
@@ -1,17 +1,17 @@
 Name "bittab bindings"
 Keywords "gt_scripts"
 Test do
-  run_test "#{$bin}gt #{$testdata}/gtscripts/bittab.lua"
+  run_test "#{$bin}gt #{$testdata}/gtscripts/bittab.gts"
 end
 
 Name "genome_node bindings"
 Keywords "gt_scripts"
 Test do
-  run_test "#{$bin}gt #{$testdata}/gtscripts/genome_node.lua"
+  run_test "#{$bin}gt #{$testdata}/gtscripts/genome_node.gts"
 end
 
 Name "genome_stream bindings"
 Keywords "gt_scripts"
 Test do
-  run_test "#{$bin}gt #{$testdata}/gtscripts/genome_stream.lua"
+  run_test "#{$bin}gt #{$testdata}/gtscripts/genome_stream.gts"
 end
