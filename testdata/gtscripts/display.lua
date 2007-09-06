d8e4aa968349a19c13f88951c24ca408d5dfe8b7
Gordon Gremme <gremme@zbh.uni-hamburg.de>
2007-09-06 18:59:55 +0200

    split evalviz
    evalviz creates a directory containing PNG files now
    display shows such a directory

---
+++ testdata/gtscripts/display.lua
@@ -0,0 +1,56 @@
+--[[
+  Copyright (c) 2007 Gordon Gremme <gremme@zbh.uni-hamburg.de>
+  Copyright (c) 2007 Center for Bioinformatics, University of Hamburg
+
+  Permission to use, copy, modify, and distribute this software for any
+  purpose with or without fee is hereby granted, provided that the above
+  copyright notice and this permission notice appear in all copies.
+
+  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+]]
+
+require 'lfs'
+
+function usage()
+  io.stderr:write(string.format("Usage: %s PNG_dir\n", arg[0]))
+  io.stderr:write("Display numbered PNG files in PNG_dir.\n")
+  os.exit(1)
+end
+
+if #arg == 1 then
+  png_dir  = arg[1]
+  -- make sure png_dir is a directory
+  rval, err = lfs.attributes(png_dir, "mode")
+  if rval ~= "directory" then
+    io.stderr:write(string.format("PNG_dir '%s' is not a directory\n", png_dir))
+     os.exit(1)
+  end
+else
+  usage()
+end
+
+function file_exists(filename)
+  assert(filename)
+  if lfs.attributes(filename, "mode") then
+    return true
+  else
+    return false
+  end
+end
+
+filenumber = 1
+while (true) do
+  local filename = png_dir .. "/" .. filenumber .. ".png"
+  if file_exists(filename) then
+    os.execute("display " .. filename)
+    filenumber = filenumber + 1
+  else
+    break
+  end
+end
