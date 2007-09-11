ab5af600014f2e36520d5173315472b520da3b59
Gordon Gremme <gremme@zbh.uni-hamburg.de>
2007-09-11 18:53:58 +0200

    read compressed GFF3 files

--- src/libgtext/gff3_parser.h
+++ src/libgtext/gff3_parser.h
@@ -38,7 +38,7 @@ int         gff3parser_parse_genome_nodes(int *status_code, GFF3Parser*,
                                           Queue *genome_nodes,
                                           Str *filenamestr,
                                           unsigned long *line_number,
-                                          FILE *fpin, Env*);
+                                          GenFile *fpin, Env*);
 /* resets the GFF3 parser (necessary if the processed input file is switched) */
 void         gff3parser_reset(GFF3Parser*, Env*);
 void         gff3parser_delete(GFF3Parser*, Env*);
