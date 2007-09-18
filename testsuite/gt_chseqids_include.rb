308d3e57a33297ca5117ae797f49b3e4cee4bc9a
Gordon Gremme <gremme@zbh.uni-hamburg.de>
2007-09-18 20:46:04 +0200

    full coverage for src/tools/gt_chseqids.c

--- testsuite/gt_chseqids_include.rb
+++ testsuite/gt_chseqids_include.rb
@@ -1,3 +1,22 @@
+Name "gt chseqids -help"
+Keywords "gt_chseqids"
+Test do
+  run_test "#{$bin}gt chseqids -help"
+end
+
+Name "gt chseqids -noop"
+Keywords "gt_chseqids"
+Test do
+  run_test("#{$bin}gt chseqids -noop", :retval => 1)
+end
+
+Name "gt chseqids empty_file"
+Keywords "gt_chseqids"
+Test do
+  run_test("#{$bin}gt chseqids #{$testdata}empty_file", :retval => 1)
+  grep $last_stderr, "not defined"
+end
+
 Name "gt chseqids test 1"
 Keywords "gt_chseqids"
 Test do
