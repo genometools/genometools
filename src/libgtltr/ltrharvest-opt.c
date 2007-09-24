4538de9817ed36338db92355e77b08d91eb87321
Gordon Gremme <gremme@zbh.uni-hamburg.de>
2007-09-24 17:38:34 +0200

    Copyright (C) -> Copyright (c) (consistency)

--- src/libgtltr/ltrharvest-opt.c
+++ src/libgtltr/ltrharvest-opt.c
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2007 David Ellinghaus <dellinghaus@zbh.uni-hamburg.de>
+  Copyright (c) 2007 David Ellinghaus <dellinghaus@zbh.uni-hamburg.de>
   Copyright (c) 2007 Center for Bioinformatics, University of Hamburg
   See LICENSE file or http://genometools.org/license.html for license details.
 */
