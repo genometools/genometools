a0fcd83bec19495b95c87d5c7909432c4b90d9aa
Gordon Gremme <gremme@zbh.uni-hamburg.de>
2007-10-10 11:54:20 +0200

    ownership comment added

--- src/libgtext/extractfeat_visitor.h
+++ src/libgtext/extractfeat_visitor.h
@@ -26,6 +26,7 @@ typedef struct ExtractFeatVisitor ExtractFeatVisitor;
 #include "libgtext/regionmapping.h"
 
 const GenomeVisitorClass* extractfeat_visitor_class(void);
+/* takes ownership of <rm> */
 GenomeVisitor*            extractfeat_visitor_new(RegionMapping *rm,
                                                   GenomeFeatureType, bool join,
                                                   bool translate, Env*);
