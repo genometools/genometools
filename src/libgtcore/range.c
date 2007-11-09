40e17bd41142e258d4c6c6f3f232389e2299b333
Gordon Gremme <gremme@zbh.uni-hamburg.de>
2007-11-09 19:41:24 +0100

    get rid of compiler warnings

--- src/libgtcore/range.c
+++ src/libgtcore/range.c
@@ -118,7 +118,7 @@ Range range_join(Range range_a, Range range_b)
 
 Range range_offset(Range range, long offset)
 {
-  Range transformed_range;
+  Range transformed_range = { 0, 0 };
   assert(range.start <= range.end);
   safeadd(transformed_range.start, range.start, offset);
   safeadd(transformed_range.end, range.end, offset);
