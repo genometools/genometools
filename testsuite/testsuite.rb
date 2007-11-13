eee19fcdaf2421db58343b3d98570a5f0dc1a312
Thomas Jahns <Thomas.Jahns@gmx.net>
2007-11-13 17:18:37 +0100

    Activate inclusion of packedindex tests.

--- testsuite/testsuite.rb
+++ testsuite/testsuite.rb
@@ -97,6 +97,7 @@ require 'gt_scripts_include'
 require 'gt_splicesiteinfo_include'
 require 'gt_stat_include'
 require 'gt_suffixerator_include'
+require 'gt_packedindex_include'
 require 'gt_trieins_include'
 require 'gt_uniq_include'
 if $arguments["libgtview"] then
