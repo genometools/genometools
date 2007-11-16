b0e93d370db24c998f7e8b3e4014008e71adc473
Gordon Gremme <gremme@zbh.uni-hamburg.de>
2007-11-16 19:13:04 +0100

    bsearch_data() interface added

--- src/libgtext/bsearch.c
+++ src/libgtext/bsearch.c
@@ -19,25 +19,15 @@
 #include "libgtcore/ensure.h"
 #include "libgtext/bsearch.h"
 
-void bsearch_all(Array *members, const void *key, const void *base,
-                 size_t nmemb, size_t size,
-                 int (*compar)(const void*, const void*, const void*),
-                 void *data, Env *env)
+static void* bsearch_generic(Array *members, const void *key, const void *base,
+                             size_t nmemb, size_t size, Compar compar,
+                             void *data, Bittab *b, Env *env)
 {
-  bsearch_all_mark(members, key, base, nmemb, size, compar, data, NULL, env);
-}
-
-void bsearch_all_mark(Array *members, const void *key, const void *base,
-                      size_t nmemb, size_t size,
-                      int (*compar)(const void*, const void*, const void*),
-                      void *data, Bittab *b, Env *env)
-{
-  const void *baseptr = base;
-  const void *ptr, /* the current element we consider */
-             *tmp_ptr;
+  void *baseptr = (void*) base, *tmp_ptr,
+       *ptr; /* the current element we consider */
   int limit, rval;
 
-  assert(members && key && size && compar);
+  assert(key && size && compar);
   assert(!b || bittab_size(b) == nmemb);
 
   /* the actual binary search */
@@ -45,7 +35,10 @@ void bsearch_all_mark(Array *members, const void *key, const void *base,
     ptr = baseptr + (limit >> 1) * size;
     if ((rval = compar(key, ptr, data)) == 0) {
       /* element found */
-      array_add(members, ptr, env);
+      if (members)
+        array_add(members, ptr, env);
+      else
+        return ptr; /* because we got no <members> array */
       if (b)
         bittab_set_bit(b, (ptr - base) / size); /* mark found element */
       /* looking left for equal elements */
@@ -64,13 +57,38 @@ void bsearch_all_mark(Array *members, const void *key, const void *base,
         if (b)
           bittab_set_bit(b, (tmp_ptr - base) / size); /* mark found element */
       }
-      return;
+      return ptr;
     }
     if (rval > 0) {
       baseptr = ptr + size;
       limit--;
     }
   }
+  return NULL;
+}
+
+void* bsearch_data(const void *key, const void *base, size_t nmemb, size_t size,
+                   Compar compar, void *data)
+{
+  return bsearch_generic(NULL, key, base, nmemb, size, compar, data, NULL,
+                         NULL);
+}
+
+void bsearch_all(Array *members, const void *key, const void *base,
+                 size_t nmemb, size_t size, Compar compar, void *data, Env *env)
+{
+  env_error_check(env);
+  assert(members);
+  bsearch_generic(members, key, base, nmemb, size, compar, data, NULL, env);
+}
+
+void bsearch_all_mark(Array *members, const void *key, const void *base,
+                      size_t nmemb, size_t size, Compar compar, void *data,
+                      Bittab *b, Env *env)
+{
+  env_error_check(env);
+  assert(members);
+  bsearch_generic(members, key, base, nmemb, size, compar, data, b, env);
 }
 
 static int cmp(const void *a_ptr, const void *b_ptr, const void *unsused)
@@ -105,6 +123,8 @@ int bsearch_unit_test(Env *env)
   bsearch_all(members, &key, array_get_space(elements), array_size(elements),
               sizeof (int), cmp, NULL, env);
   ensure(had_err, !array_size(members)); /* no member found */
+  ensure(had_err, !bsearch_data(&key, array_get_space(elements),
+                                array_size(elements), sizeof (int), cmp, NULL));
 
   /* 1 element */
   key = 7;
@@ -114,6 +134,9 @@ int bsearch_unit_test(Env *env)
               sizeof (int), cmp, NULL, env);
   ensure(had_err, array_size(members) == 1); /* one member found */
   member_ptr = *(int**) array_get(members, 0);
+  ensure(had_err, *member_ptr == element);
+  member_ptr = bsearch_data(&key, array_get_space(elements),
+                            array_size(elements), sizeof (int), cmp, NULL);
   ensure(had_err, *member_ptr == element);
 
   key = -7;
@@ -134,12 +157,16 @@ int bsearch_unit_test(Env *env)
   ensure(had_err, *member_ptr == element);
   member_ptr = *(int**) array_get(members, 1);
   ensure(had_err, *member_ptr == element);
+  ensure(had_err, bsearch_data(&key, array_get_space(elements),
+                               array_size(elements), sizeof (int), cmp, NULL));
 
   key = -7;
   array_reset(members);
   bsearch_all(members, &key, array_get_space(elements), array_size(elements),
               sizeof (int), cmp, NULL, env);
   ensure(had_err, !array_size(members)); /* no member found */
+  ensure(had_err, !bsearch_data(&key, array_get_space(elements),
+                                array_size(elements), sizeof (int), cmp, NULL));
 
   /* 3 elements */
   key = 7;
@@ -155,12 +182,16 @@ int bsearch_unit_test(Env *env)
   ensure(had_err, *member_ptr == element);
   member_ptr = *(int**) array_get(members, 2);
   ensure(had_err, *member_ptr == element);
+  ensure(had_err, bsearch_data(&key, array_get_space(elements),
+                               array_size(elements), sizeof (int), cmp, NULL));
 
   key = -7;
   array_reset(members);
   bsearch_all(members, &key, array_get_space(elements), array_size(elements),
               sizeof (int), cmp, NULL, env);
   ensure(had_err, !array_size(members)); /* no member found */
+  ensure(had_err, !bsearch_data(&key, array_get_space(elements),
+                                array_size(elements), sizeof (int), cmp, NULL));
 
   /* large case: -10 -5 -3 -3 -3 0 1 2 3 */
   array_reset(elements);
@@ -192,6 +223,8 @@ int bsearch_unit_test(Env *env)
   ensure(had_err, *member_ptr == -3);
   member_ptr = *(int**) array_get(members, 2);
   ensure(had_err, *member_ptr == -3);
+  ensure(had_err, bsearch_data(&key, array_get_space(elements),
+                               array_size(elements), sizeof (int), cmp, NULL));
 
   /* test bsearch_all_mark() with large case */
   array_reset(members);
--- src/libgtext/bsearch.h
+++ src/libgtext/bsearch.h
@@ -21,22 +21,26 @@
 #include "libgtcore/array.h"
 #include "libgtcore/bittab.h"
 
-/* similar interface to bsearch(3), except that all members which compare as
-   equal are stored in the ``members'' array. The order in which the elements
+typedef int (*Compar)(const void*, const void*, const void *data);
+
+/* similar interface to bsearch(3), except that the Compar function gets an
+   additional <data> pointer */
+void* bsearch_data(const void *key, const void *base, size_t nmemb, size_t size,
+                   Compar, void *data);
+
+/* similar interface to bsearch_data(), except that all members which compare as
+   equal are stored in the <members> array. The order in which the elements
    are added is undefined */
-void bsearch_all(Array *members, const void *key, const void *base,
-                 size_t nmemb, size_t size,
-                 int (*compar)(const void*, const void*, const void*), void*,
-                 Env*);
+void  bsearch_all(Array *members, const void *key, const void *base,
+                  size_t nmemb, size_t size, Compar, void *data, Env*);
 
 /* similar interface to bsearch_all(). Additionally, if a bittab is given (which
-   must be of size ``nmemb''), the bits corresponding to the found elements are
+   must be of size <nmemb>), the bits corresponding to the found elements are
    marked (i.e., set) */
-void bsearch_all_mark(Array *members, const void *key, const void *base,
-                      size_t nmemb, size_t size,
-                      int (*compar)(const void*, const void*, const void*),
-                      void*, Bittab*, Env*);
+void  bsearch_all_mark(Array *members, const void *key, const void *base,
+                       size_t nmemb, size_t size, Compar, void *data, Bittab*,
+                       Env*);
 
-int bsearch_unit_test(Env*);
+int   bsearch_unit_test(Env*);
 
 #endif
