27c8677038683502cfe8085d71e0ff6098b8092c
Gordon Gremme <gremme@zbh.uni-hamburg.de>
2007-12-20 20:04:08 +0100

    feature_index:render_to_png() documented

--- gtdata/modules/gtlua/feature_index.lua
+++ gtdata/modules/gtlua/feature_index.lua
@@ -58,7 +58,7 @@ function GenomeTools_feature_index_mt:get_coverage(seqid, maxdist)
   return coverage
 end
 
--- render
+-- render PNG file <png_file> for <seqid> in <range> with optional <width>
 function GenomeTools_feature_index_mt:render_to_png(png_file, seqid, range, width)
   assert(self and png_file and seqid and range)
   if not width then width = 1600 end
