8bccd10041578624b0ef8f98dc38624769dd3479
Gordon Gremme <gremme@zbh.uni-hamburg.de>
2008-01-09 15:08:45 +0100

    make sure the outgoing sequence is '\0' terminated

--- src/libgtcore/seqiterator.c
+++ src/libgtcore/seqiterator.c
@@ -103,6 +103,9 @@ int seqiterator_next(SeqIterator *seqit,
       *len = seqit->sequencebuffer.nextfreeUchar;
       if (seqit->withsequence)
       {
+        /* make sure the outgoing sequence is '\0' terminated */
+        seqit->sequencebuffer.spaceUchar
+          [seqit->sequencebuffer.nextfreeUchar] = '\0';
         *sequence = seqit->sequencebuffer.spaceUchar;
       }
       seqit->sequencebuffer.nextfreeUchar = 0;
