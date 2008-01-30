c81c12e60596d4b942b9bafeb28c391204ddbc38
Gordon Gremme <gremme@zbh.uni-hamburg.de>
2008-01-30 17:50:20 +0100

    improve documentation

--- src/libgtext/qgram.h
+++ src/libgtext/qgram.h
@@ -1,6 +1,6 @@
 /*
-  Copyright (c) 2006-2007 Gordon Gremme <gremme@zbh.uni-hamburg.de>
-  Copyright (c) 2006-2007 Center for Bioinformatics, University of Hamburg
+  Copyright (c) 2006-2008 Gordon Gremme <gremme@zbh.uni-hamburg.de>
+  Copyright (c) 2006-2008 Center for Bioinformatics, University of Hamburg
 
   Permission to use, copy, modify, and distribute this software for any
   purpose with or without fee is hereby granted, provided that the above
@@ -20,19 +20,19 @@
 
 #include "libgtcore/array.h"
 
-/* encodes a word w of length q over an alphabet of size alpha_size as a unique
-   number */
+/* Encodes a word <w> of length <q> over an alphabet of size <alpha_size> as a
+   unique number. */
 unsigned long qgram_encode(const char *w, unsigned long q,
                            unsigned long alphabet_size);
 
-/* computes the next encoding */
+/* Computes the next encoding. */
 unsigned long qgram_step(unsigned long current_code, char previous, char next,
                          unsigned long alphabet_size,
                          unsigned long
                          alpha_size_raised_to_the_power_of_q_minus_1);
 
-/* computes all q-grams of the given encoded_seq (over an alphabet of size
-   alpha_size) and stores them in the array qgrams. */
+/* Computes all q-grams of the given <encoded_seq> (over an alphabet of size
+   <alpha_size>) and stores them in the array <qgrams>. */
 void          qgram_compute(Array *qgrams, const char *encoded_seq,
                             unsigned long seqlen, unsigned long alpha_size,
                             unsigned int q);
