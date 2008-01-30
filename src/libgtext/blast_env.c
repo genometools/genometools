e18a4a771e7689306e6f1289d863503097bb4817
Gordon Gremme <gremme@zbh.uni-hamburg.de>
2008-01-30 22:00:51 +0100

    improve readability

--- src/libgtext/blast_env.c
+++ src/libgtext/blast_env.c
@@ -28,13 +28,6 @@ typedef struct{
   Hashtable *mapping;
 } Pos;
 
-struct BlastEnv {
-  Alpha *alpha;
-  unsigned long q;
-  Bittab *V; /* the vector V of r^q bits */
-  Pos *pos; /* the set of position lists */
-};
-
 Pos* pos_new(void)
 {
   Pos *pos = ma_malloc(sizeof *pos);
@@ -100,22 +93,22 @@ static long* compute_max_pos_scores(const char *w, unsigned long wlen,
   return max_pos_scores;
 }
 
-static void blast_env_add_q_word(BlastEnv *be, const char *qgram_rest,
-                                 char *current_word, long *max_cumul_scores,
-                                 Alpha *alpha, unsigned long q,
-                                 unsigned long q_rest, long k,
-                                 long current_score, unsigned long position,
-                                 const ScoreMatrix *score_matrix)
+static void add_q_word_to_env(Bittab *V, Pos *pos, const char *qgram_rest,
+                              char *current_word, long *max_cumul_scores,
+                              Alpha *alpha, unsigned long q,
+                              unsigned long q_rest, long k, long current_score,
+                              unsigned long position,
+                              const ScoreMatrix *score_matrix)
 {
-  assert(be && qgram_rest && alpha);
+  assert(V && pos && qgram_rest && alpha);
   if (q_rest == 0) {
     if (current_score >= k) {
       unsigned long qgram_code = qgram_encode(current_word, q,
                                               alpha_size(alpha));
       /* set V[qgram_code] */
-      bittab_set_bit(be->V, qgram_code);
+      bittab_set_bit(V, qgram_code);
       /* store position */
-      pos_add(be->pos, qgram_code, position);
+      pos_add(pos, qgram_code, position);
     }
   }
   else if (current_score + max_cumul_scores[q - q_rest] >= k) { /* lookahead */
@@ -124,22 +117,20 @@ static void blast_env_add_q_word(BlastEnv *be, const char *qgram_rest,
       long char_score = score_matrix_get_score(score_matrix, qgram_rest[0], i);
       assert(i < CHAR_MAX);
       current_word[q - q_rest] = i;
-      blast_env_add_q_word(be, qgram_rest+1, current_word, max_cumul_scores,
-                           alpha, q, q_rest-1, k, current_score+char_score,
-                           position, score_matrix);
+      add_q_word_to_env(V, pos, qgram_rest+1, current_word, max_cumul_scores,
+                        alpha, q, q_rest-1, k, current_score+char_score,
+                        position, score_matrix);
     }
   }
 }
 
-static void blast_env_compute(BlastEnv *be, const char *w, unsigned long wlen,
-                              Alpha *alpha, unsigned long q, long k,
-                              const ScoreMatrix *score_matrix)
+static void compute_env(Bittab *V, Pos *pos, const char *w, unsigned long wlen,
+                        Alpha *alpha, unsigned long q, long k,
+                        const ScoreMatrix *score_matrix)
 {
   long *max_pos_scores, *max_cumul_scores;
   unsigned long i;
   char *current_word;
-  be->V = bittab_new(pow(alpha_size(alpha), q));
-  be->pos = pos_new();
   /* prepare space for current word */
   current_word = ma_malloc(sizeof (char) * (q+1));
   current_word[q] = '\0';
@@ -153,14 +144,21 @@ static void blast_env_compute(BlastEnv *be, const char *w, unsigned long wlen,
     max_cumul_scores[q-1] = max_pos_scores[i+q-1];
     for (j = q-2; j >= 0; j--)
       max_cumul_scores[j] = max_pos_scores[i+j] + max_cumul_scores[j+1];
-    blast_env_add_q_word(be, w+i, current_word, max_cumul_scores, alpha, q, q,
-                         k, 0, i, score_matrix);
+    add_q_word_to_env(V, pos, w+i, current_word, max_cumul_scores, alpha, q, q,
+                      k, 0, i, score_matrix);
   }
   /* free */
   ma_free(max_cumul_scores);
   ma_free(max_pos_scores);
   ma_free(current_word);
 }
+
+struct BlastEnv {
+  Alpha *alpha;
+  unsigned long q;
+  Bittab *V; /* the vector V of r^q bits */
+  Pos *pos; /* the set of position lists */
+};
 
 BlastEnv* blast_env_new(const char *w, unsigned long wlen, Alpha *alpha,
                         unsigned long q, long k,
@@ -172,8 +170,11 @@ BlastEnv* blast_env_new(const char *w, unsigned long wlen, Alpha *alpha,
   be = ma_calloc(1, sizeof *be);
   be->alpha = alpha_ref(alpha);
   be->q = q;
-  if (wlen >= q)
-    blast_env_compute(be, w, wlen, alpha, q, k, score_matrix);
+  if (wlen >= q) {
+    be->V = bittab_new(pow(alpha_size(alpha), q));
+    be->pos = pos_new();
+    compute_env(be->V, be->pos, w, wlen, alpha, q, k, score_matrix);
+  }
   return be;
 }
 
