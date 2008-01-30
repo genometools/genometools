ed707d63675e4d2e4c18950f0c81a25916fc929a
Gordon Gremme <gremme@zbh.uni-hamburg.de>
2008-01-30 22:28:03 +0100

    add extensive documentation

--- src/libgtext/blast_env.c
+++ src/libgtext/blast_env.c
@@ -24,10 +24,12 @@
 #include "libgtext/blast_env.h"
 #include "libgtext/qgram.h"
 
-typedef struct{
+/* Internal class to hold position lists. */
+typedef struct {
   Hashtable *mapping;
 } Pos;
 
+/* Return a new Pos object. */
 Pos* pos_new(void)
 {
   Pos *pos = ma_malloc(sizeof *pos);
@@ -35,6 +37,7 @@ Pos* pos_new(void)
   return pos;
 }
 
+/* Delete <pos> object. */
 void pos_delete(Pos *pos)
 {
   if (!pos) return;
@@ -42,6 +45,7 @@ void pos_delete(Pos *pos)
   ma_free(pos);
 }
 
+/* Add <position> to position list for <code>. */
 void pos_add(Pos *pos, unsigned long code, unsigned long position)
 {
   Array *position_list;
@@ -56,12 +60,19 @@ void pos_add(Pos *pos, unsigned long code, unsigned long position)
     array_add(position_list, position);
 }
 
+/* Get position list for <code>. */
 Array* pos_get(Pos *pos, unsigned long code)
 {
   assert(pos && pos->mapping);
   return hashtable_get(pos->mapping, (void*) code);
 }
 
+/*
+  Compute the maximal position scores for sequence <w> of length <wlen> under
+  score matrix <score_matrix>. A maximal position score is the maximal score
+  which a certain position of <w> can contribute to the total score.
+  Returns an array of length <wlen> which has to be freed by the caller.
+*/
 static long* compute_max_pos_scores(const char *w, unsigned long wlen,
                                     const ScoreMatrix *score_matrix)
 {
@@ -69,7 +80,6 @@ static long* compute_max_pos_scores(const char *w, unsigned long wlen,
   unsigned int dimension;
   unsigned long i;
 
-  /* prepare space for lookahead data */
   dimension = score_matrix_get_dimension(score_matrix);
   max_matrix_scores = ma_malloc(sizeof (long) * dimension);
   max_pos_scores = ma_malloc(sizeof (long) * wlen);
@@ -93,6 +103,33 @@ static long* compute_max_pos_scores(const char *w, unsigned long wlen,
   return max_pos_scores;
 }
 
+/*
+  This function does the actual work of adding all strings with score >= <k> to
+  the Blast environment (which consists of the bit vector <V> and the set of
+  position lists <pos>).
+  This is achieved by constructing an implicit trie (via recursive
+  function calls) representing all possible strings of length <q> over <alpha>.
+  - <qgram_rest> is the rest of the current q-gram for which the trie is
+    constructed.
+  - <current_word> holds the word which denotes the characters along
+    the path to the current node.
+  - <max_cumul_scores> contains a the cumulative scores necessary for the
+    lookahead mechanism.
+  - <alpha> is the alphabet.
+  - <q> is the q-gram size.
+  - <q_rest> is the size of <qgram_rest>.
+  - <k> is the minimum score which is necessary to add the <current_word> to the
+    Blast environment).
+  - <current_score> denotes the score of the <curren_word>.
+  - <position> denotes the position of the current q-gram in the query string.
+
+  The lookahead mechanism works as follows:
+  If the <current_score> of the <current_word> plus the maximal score achievable
+  during the rest of the traversal (stored in <max_cumul_scores>) is smaller
+  than the minimum score <k>, then the traversal of this branch is aborted
+  (because it is not possible to find a word with a sufficient score on this
+  branch).
+*/
 static void add_q_word_to_env(Bittab *V, Pos *pos, const char *qgram_rest,
                               char *current_word, long *max_cumul_scores,
                               Alpha *alpha, unsigned long q,
@@ -113,10 +150,12 @@ static void add_q_word_to_env(Bittab *V, Pos *pos, const char *qgram_rest,
   }
   else if (current_score + max_cumul_scores[q - q_rest] >= k) { /* lookahead */
     unsigned int i;
+    /* enumerate all possible characters (at this node) */
     for (i = 0; i < alpha_size(alpha); i++) {
       long char_score = score_matrix_get_score(score_matrix, qgram_rest[0], i);
       assert(i < CHAR_MAX);
       current_word[q - q_rest] = i;
+      /* recursive call (descend into branch) */
       add_q_word_to_env(V, pos, qgram_rest+1, current_word, max_cumul_scores,
                         alpha, q, q_rest-1, k, current_score+char_score,
                         position, score_matrix);
@@ -124,6 +163,11 @@ static void add_q_word_to_env(Bittab *V, Pos *pos, const char *qgram_rest,
   }
 }
 
+/*
+  This function fills the Blast environment, which consists of the bit vector
+  <V> and the set of position lists <pos>, for the encoded word <w> of length
+  <wlen>.
+*/
 static void compute_env(Bittab *V, Pos *pos, const char *w, unsigned long wlen,
                         Alpha *alpha, unsigned long q, long k,
                         const ScoreMatrix *score_matrix)
@@ -144,6 +188,7 @@ static void compute_env(Bittab *V, Pos *pos, const char *w, unsigned long wlen,
     max_cumul_scores[q-1] = max_pos_scores[i+q-1];
     for (j = q-2; j >= 0; j--)
       max_cumul_scores[j] = max_pos_scores[i+j] + max_cumul_scores[j+1];
+    /* process the current q-word */
     add_q_word_to_env(V, pos, w+i, current_word, max_cumul_scores, alpha, q, q,
                       k, 0, i, score_matrix);
   }
@@ -153,11 +198,13 @@ static void compute_env(Bittab *V, Pos *pos, const char *w, unsigned long wlen,
   ma_free(current_word);
 }
 
+/* the actual class implementation */
 struct BlastEnv {
   Alpha *alpha;
   unsigned long q;
-  Bittab *V; /* the vector V of r^q bits */
-  Pos *pos; /* the set of position lists */
+  Bittab *V; /* The vector V of r^q bits. */
+  Pos *pos;  /* The set of position lists. If a bit in <V> is set then <pos>
+                contains the corresponding position list for that code. */
 };
 
 BlastEnv* blast_env_new(const char *w, unsigned long wlen, Alpha *alpha,
@@ -170,6 +217,7 @@ BlastEnv* blast_env_new(const char *w, unsigned long wlen, Alpha *alpha,
   be = ma_calloc(1, sizeof *be);
   be->alpha = alpha_ref(alpha);
   be->q = q;
+  /* if <w> is long enough fill the Blast environment */
   if (wlen >= q) {
     be->V = bittab_new(pow(alpha_size(alpha), q));
     be->pos = pos_new();
