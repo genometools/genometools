910510a975ff041f77d5214fe01059c945564642
Gordon Gremme <gremme@zbh.uni-hamburg.de>
2008-02-21 16:53:13 +0100

    document GenomeNode bindings

--- src/libgtlua/genome_node_lua.h
+++ src/libgtlua/genome_node_lua.h
@@ -1,6 +1,6 @@
 /*
-  Copyright (c) 2007 Gordon Gremme <gremme@zbh.uni-hamburg.de>
-  Copyright (c) 2007 Center for Bioinformatics, University of Hamburg
+  Copyright (c) 2007-2008 Gordon Gremme <gremme@zbh.uni-hamburg.de>
+  Copyright (c) 2007-2008 Center for Bioinformatics, University of Hamburg
 
   Permission to use, copy, modify, and distribute this software for any
   purpose with or without fee is hereby granted, provided that the above
@@ -23,18 +23,42 @@
 
 /* exports the GenomeNode interface and its implementors to Lua:
 
-   genome_node     = gt.genome_feature_new(type, range, strand)
-   sequence_region = gt.sequence_region_new(seqid, range)
-   string          = genome_node:get_filename()
-   range           = genome_node:get_range()
-   string          = genome_node:get_seqid()
-                     genome_node:set_seqid(seqid)
-                     genome_node:accept(genome_visitor)
-                     parent_node:is_part_of_genome_node(child_node)
-                     genome_node:mark()
-   boolean         = genome_node:is_marked()
-   boolean         = genome_node:contains_marked()
-                     genome_feature:output_leading()
+   -- Returns a new genome feature of <type> spanning <range> on <strand>.
+   function genome_feature_new(type, range, strand)
+
+   -- Returns a new sequence region for sequence id <seqid> spanning <range>.
+   function sequence_region_new(seqid, range)
+
+   -- Returns the filenname of <genome_node>.
+
+   function genome_node:get_filename()
+
+   -- Returns the range of <genome_node>.
+   function genome_node:get_range()
+
+   -- Returns the sequence id of <genome_node>.
+   function genome_node:get_seqid()
+
+   -- Set the sequence id of <genome_node> to <seqid>.
+   function genome_node:set_seqid(seqid)
+
+   -- Accept <genome_visitor>.
+   function genome_node:accept(genome_visitor)
+
+   -- Make <genome_node> the parent of <child_node>.
+   function genome_node:is_part_of_genome_node(child_node)
+
+   -- Mark <genome_node>.
+   function genome_node:mark()
+
+   -- Returns true if <genome_node> is marked, false otherwise.
+   function genome_node:is_marked()
+
+   -- Returns true if <genome_node> contains a marked node, false otherwise.
+   function genome_node:contains_marked()
+
+   -- Show leading part of GFF3 output for <genome_feature>
+   function genome_feature:output_leading()
 */
 int luaopen_genome_node(lua_State*);
 
