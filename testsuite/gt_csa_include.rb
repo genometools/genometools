5ccdb2b40d68e6c3063a00b5952d77656b315ea2
Gordon Gremme <gremme@zbh.uni-hamburg.de>
2008-03-05 11:43:54 +0100

    test target attribute joining in csa tool (with arabidopsis data)

--- testsuite/gt_csa_include.rb
+++ testsuite/gt_csa_include.rb
@@ -1,15 +1,15 @@
 Name "gt csa test"
 Keywords "gt_csa"
 Test do
-  run_test("#{$bin}gt csa #{$testdata}/gt_csa_test_1", :retval => 1)
+  run_test("#{$bin}gt csa #{$testdata}gt_csa_test_1", :retval => 1)
 end
 
 1.upto(4) do |i|
   Name "gt csa prob #{i}"
   Keywords "gt_csa"
   Test do
-    run_test "#{$bin}gt csa #{$testdata}/gt_csa_prob_#{i}.in"
-    run "diff #{$last_stdout} #{$testdata}/gt_csa_prob_#{i}.out"
+    run_test "#{$bin}gt csa #{$testdata}gt_csa_prob_#{i}.in"
+    run "diff #{$last_stdout} #{$testdata}gt_csa_prob_#{i}.out"
   end
 end
 
@@ -17,7 +17,14 @@ end
   Name "gt -debug csa prob #{i}"
   Keywords "gt_csa"
   Test do
-    run_test "#{$bin}gt -debug csa #{$testdata}/gt_csa_prob_#{i}.in"
-    run "diff #{$last_stderr} #{$testdata}/gt_csa_prob_#{i}.debug"
+    run_test "#{$bin}gt -debug csa #{$testdata}gt_csa_prob_#{i}.in"
+    run "diff #{$last_stderr} #{$testdata}gt_csa_prob_#{i}.debug"
   end
+end
+
+Name "gt csa arabidopsis"
+Keywords "gt_csa"
+Test do
+  run_test "#{$bin}gt csa #{$testdata}U89959_sas.gff3"
+  run "diff #{$last_stdout} #{$testdata}U89959_csas.gff3"
 end
