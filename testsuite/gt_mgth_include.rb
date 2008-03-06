290cb9d7ae4c9fd7dfa9f8b07a78c6931aeff872
David Schmitz-Hübsch <davidschmitzhuebsch@gmx.de>
2008-03-06 11:18:21 +0100

    update testsuite/gt_mgth_include.rb

--- testsuite/gt_mgth_include.rb
+++ testsuite/gt_mgth_include.rb
@@ -55,28 +55,29 @@ Name "gttestdata mgth mixed options 1"
 Keywords "gt_mgth"
 Test do
   run_test "#{$bin}gt mgth -o Metagenome -s 3.17 -n 2.32 -b -13.64 -q -3.45 -h -4.77 -l -1.5 -p 325.50 -f 225.25 -t yes -g yes -m no -x yes -r 1 -e 1 -d 0.01 #{$gttestdata}mgth/Metagenome_NT.xml.gz #{$gttestdata}mgth/Metagenome.txt.gz #{$gttestdata}mgth/Hits_NT_Metagenome.txt.gz", :maxtime => 100
-  run "diff #{$cur}/testsuite/stest_testsuite/test340/Metagenome.txt #{$gttestdata}mgth/Metagenome_ori_1.txt"
+  run "diff #{$cur}/testsuite/stest_testsuite/test371/Metagenome.txt #{$gttestdata}mgth/Metagenome_ori_1.txt"
 end
 
 Name "gttestdata mgth mixed options 2"
 Keywords "gt_mgth"
 Test do
   run_test "#{$bin}gt mgth -o Metagenome -s 1.17 -n 3 -b -9.10 -q -3.0 -h -3.98 -l -1.3 -p 368 -f 217.0 -t yes -g yes -m no -x yes -r 2 -e 3 -d 0.05  #{$gttestdata}mgth/Metagenome_NTo1.xml.gz #{$gttestdata}mgth/Metagenome.txt.gz #{$gttestdata}mgth/Hits_NTo1_Metagenome.txt.gz", :maxtime => 100
-  run "diff #{$cur}/testsuite/stest_testsuite/test341/Metagenome.html #{$gttestdata}mgth/Metagenome_ori_2.html"
+  run "diff #{$cur}/testsuite/stest_testsuite/test372/Metagenome.html #{$gttestdata}mgth/Metagenome_ori_2.html"
 end
 
 Name "gttestdata mgth mixed options 3"
 Keywords "gt_mgth"
 Test do
   run_test "#{$bin}gt mgth -o Metagenome_454 -s 3.17 -n 2.32 -b -13.64 -q -3.45 -h -4.77 -l -1.5 -p 325.50 -f 225.25 -t yes -g yes -m no -x yes -r 1 -e 1 -d 0.01  #{$gttestdata}mgth/Metagenome_NT_454.xml.gz #{$gttestdata}mgth/Metagenome_454.txt.gz #{$gttestdata}mgth/Hits_NT_Metagenome_454.txt.gz", :maxtime => 100
-  run "diff #{$cur}/testsuite/stest_testsuite/test338/Metagenome_454.txt #{$gttestdata}mgth/Metagenome_454_ori_3.txt"
+  run "diff #{$cur}/testsuite/stest_testsuite/test373/Metagenome_454.txt #{$gttestdata}mgth/Metagenome_454_ori_3.txt"
 end
 
 Name "gttestdata mgth mixed options 4"
 Keywords "gt_mgth"
 Test do
   run_test "#{$bin}gt mgth -o Metagenome_454 -s 1.17 -n 3 -b -9.10 -q -3.0 -h -3.98 -l -1.3 -p 368 -f 217.0 -t yes -g yes -m no -x yes -r 2 -e 3 -d 0.05  #{$gttestdata}mgth/Metagenome_NTo1_454.xml.gz #{$gttestdata}mgth/Metagenome_454.txt.gz #{$gttestdata}mgth/Hits_NTo1_Metagenome_454.txt.gz", :maxtime => 100
-  run "diff #{$cur}/testsuite/stest_testsuite/test338/Metagenome_454.html #{$gttestdata}mgth/Metagenome_454_ori_4.html"
+  run "diff #{$cur}/testsuite/stest_testsuite/test374/Metagenome_454.html #{$gttestdata}mgth/Metagenome_454_ori_4.html"
+  run "rm #{$gttestdata}mgth/*.gt_bsi #{$gttestdata}mgth/*.gt_bsr"
 end
 
 end
