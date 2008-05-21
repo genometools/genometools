eb8ef2f5b53713469238c769ca04e96cad7a5ac3
Gordon Gremme <gremme@zbh.uni-hamburg.de>
2008-05-21 13:18:33 +0200

    gtruby tests depend on libgtview

--- testsuite/gt_ruby_include.rb
+++ testsuite/gt_ruby_include.rb
@@ -1,20 +1,20 @@
-Name "gtruby: genome_stream bindings (output stream)"
-Keywords "gt_ruby"
-Test do
-  run_ruby "#{$testdata}/gtruby/gff3.rb #{$testdata}/gff3_file_1_short.txt"
-  run "env LC_ALL=C sort #{$last_stdout}"
-  run "diff #{$last_stdout} #{$testdata}gff3_file_1_short_sorted.txt"
-end
-
-Name "gtruby: genome_visitor bindings (output stream)"
-Keywords "gt_ruby"
-Test do
-  run_ruby "#{$testdata}/gtruby/genome_visitor.rb #{$testdata}/gff3_file_1_short.txt"
-  run "env LC_ALL=C sort #{$last_stdout}"
-  run "diff #{$last_stdout} #{$testdata}gff3_file_1_short_sorted.txt"
-end
-
 if $arguments["libgtview"] then
+  Name "gtruby: genome_stream bindings (output stream)"
+  Keywords "gt_ruby"
+  Test do
+    run_ruby "#{$testdata}/gtruby/gff3.rb #{$testdata}/gff3_file_1_short.txt"
+    run "env LC_ALL=C sort #{$last_stdout}"
+    run "diff #{$last_stdout} #{$testdata}gff3_file_1_short_sorted.txt"
+  end
+
+  Name "gtruby: genome_visitor bindings (output stream)"
+  Keywords "gt_ruby"
+  Test do
+    run_ruby "#{$testdata}/gtruby/genome_visitor.rb #{$testdata}/gff3_file_1_short.txt"
+    run "env LC_ALL=C sort #{$last_stdout}"
+    run "diff #{$last_stdout} #{$testdata}gff3_file_1_short_sorted.txt"
+  end
+
   Name "gtruby: feature_index and feature_stream bindings"
   Keywords "gt_ruby"
   Test do
