36364a5977c2d88b616ad7b2676d612ec640dbda
Thomas Jahns <Thomas.Jahns@gmx.net>
2008-06-16 19:58:38 +0200

    miniscule copyright date adjustment

--- src/tools/gt_packedindex_trsuftab.c
+++ src/tools/gt_packedindex_trsuftab.c
@@ -1,5 +1,5 @@
 /*
-  Copyright (c) 2007 Thomas Jahns <Thomas.Jahns@gmx.net>
+  Copyright (c) 2008 Thomas Jahns <Thomas.Jahns@gmx.net>
 
   Permission to use, copy, modify, and distribute this software for any
   purpose with or without fee is hereby granted, provided that the above
--- src/tools/gt_packedindex_trsuftab.h
+++ src/tools/gt_packedindex_trsuftab.h
@@ -1,6 +1,6 @@
 /*
-  Copyright (c) 2007 Thomas Jahns <Thomas.Jahns@gmx.net>
-  Copyright (c) 2007 Center for Bioinformatics, University of Hamburg
+  Copyright (c) 2008 Thomas Jahns <Thomas.Jahns@gmx.net>
+  Copyright (c) 2008 Center for Bioinformatics, University of Hamburg
 
   Permission to use, copy, modify, and distribute this software for any
   purpose with or without fee is hereby granted, provided that the above
