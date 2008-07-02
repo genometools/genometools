d00b44dc4929807c4e5b6755473a2aa15c34071d
Stefan Kurtz <kurtz@zbh.uni-hamburg.de>
2008-07-02 20:37:06 +0200

    Add -rw and use -nospecials only for -pck.

--- scripts/failure.sh
+++ scripts/failure.sh
@@ -12,14 +12,9 @@ reference=$1
 query=$2
 
 # exec in gt root dir
-# works without -sprank and fails with -sprank
-
-#SPRANK="-sprank"
 
 gt suffixerator -tis -suf -dna -pl -db ${reference} -indexname esa-fwd
 gt packedindex mkindex -sprank -tis -dna -pl -bsize 10 -locfreq 32\
                        -dir rev -db ${reference} -indexname pck-rev
-gt tagerator -cmp -esa esa-fwd -k 1 -nospecials -t ${query}
-gt tagerator -cmp -pck pck-rev -k 1 -nospecials -t ${query} 
-
-# bin/gt tagerator -t Q1 -pck pck
+gt tagerator -rw -cmp -esa esa-fwd -k 1 -t ${query}
+gt tagerator -rw -cmp -pck pck-rev -k 1 -nospecials -t ${query} 
