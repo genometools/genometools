89a648f8d1ec92230e06215219e952ab7f0b1c50
Gordon Gremme <gremme@zbh.uni-hamburg.de>
2008-07-04 14:57:04 +0200

    improve `gt mutate` tests (using -seed)

--- testsuite/gt_mutate_include.rb
+++ testsuite/gt_mutate_include.rb
@@ -1,20 +1,20 @@
 Name "gt mutate test 1"
 Keywords "gt_mutate"
 Test do
-  run_test "#{$bin}gt mutate #{$testdata}gt_mutate_test_1.fas"
-  grep($last_stdout, /mutated/);
+  run_test "#{$bin}gt -seed 1 mutate #{$testdata}gt_mutate_test_1.fas"
+  run "diff #{$last_stdout} #{$testdata}gt_mutate_test_1.out"
 end
 
 Name "gt mutate test 2"
 Keywords "gt_mutate"
 Test do
-  run_test "#{$bin}gt mutate #{$testdata}gt_mutate_test_1.fas"
-  grep($last_stdout, /mutated/);
+  run_test "#{$bin}gt -seed 987654321 mutate #{$testdata}gt_mutate_test_2.fas"
+  run "diff #{$last_stdout} #{$testdata}gt_mutate_test_2.out"
 end
 
 Name "gt mutate test both"
 Keywords "gt_mutate"
 Test do
-  run_test "#{$bin}gt mutate -rate 2 #{$testdata}gt_mutate_test_1.fas #{$testdata}gt_mutate_test_2.fas"
-  grep($last_stdout, /mutated/);
+  run_test "#{$bin}gt -seed 555555555 mutate -rate 2 #{$testdata}gt_mutate_test_1.fas #{$testdata}gt_mutate_test_2.fas"
+  run "diff #{$last_stdout} #{$testdata}gt_mutate_test_both.out"
 end
