bbcd9e0773babc99bed9cc6b6d51367ce40fe3ad
Sascha Steinbiss <ssteinbiss@stud.zbh.uni-hamburg.de>
2008-08-27 22:03:04 +0200

    extend Timer class to show microseconds

--- src/libgtcore/timer.c
+++ src/libgtcore/timer.c
@@ -59,13 +59,36 @@ void timer_stop(Timer *t)
   }
 }
 
+static int timeval_subtract (struct timeval *result,
+                             struct timeval *x,
+                             struct timeval *y)
+{
+  if (x->tv_usec < y->tv_usec) {
+    int nsec = (y->tv_usec - x->tv_usec) / 1000000 + 1;
+    y->tv_usec -= 1000000 * nsec;
+    y->tv_sec += nsec;
+  }
+  if (x->tv_usec - y->tv_usec > 1000000) {
+    int nsec = (x->tv_usec - y->tv_usec) / 1000000;
+    y->tv_usec += 1000000 * nsec;
+    y->tv_sec -= nsec;
+  }
+  result->tv_sec = x->tv_sec - y->tv_sec;
+  result->tv_usec = x->tv_usec - y->tv_usec;
+  return x->tv_sec < y->tv_sec;
+}
+
 void timer_show(Timer *t, FILE *fp)
 {
+  struct timeval elapsed_tv;
+
   if (t->state == TIMER_RUNNING)
     timer_stop(t);
   assert(t->state == TIMER_STOPPED);
-  fprintf(fp, "%lds real %lds user %lds system\n",
-          (long)(t->stop_tv.tv_sec - t->start_tv.tv_sec),
+  timeval_subtract(&elapsed_tv, &t->stop_tv, &t->start_tv);
+  fprintf(fp, "%ld.%06lds real %lds user %lds system\n",
+          (long)(elapsed_tv.tv_sec),
+          (long)(elapsed_tv.tv_usec),
           (long)(t->stop_ru.ru_utime.tv_sec - t->stop_ru.ru_utime.tv_sec),
           (long)(t->stop_ru.ru_stime.tv_sec - t->stop_ru.ru_stime.tv_sec));
 }
