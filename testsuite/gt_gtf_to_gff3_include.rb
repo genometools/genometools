a1e7d4851cbc252ae9120d8ed56107b390c2226f
Sascha Steinbiss <ssteinbiss@stud.zbh.uni-hamburg.de>
2008-10-06 13:58:44 +0200

    use sorted GFF3 output for testing GTF to GFF3 converter

--- testsuite/gt_gtf_to_gff3_include.rb
+++ testsuite/gt_gtf_to_gff3_include.rb
@@ -9,10 +9,13 @@ if $gttestdata then
   Name "gt gtf_to_gff3 test"
   Keywords "gt_gtf_to_gff3 large_gtf"
   Test do
+  run "#{$bin}gt gff3 -sort #{$gttestdata}gff3/Drosophila_melanogaster.BDGP5.4.50.gff3 " +
+      "> ref_sorted.gff3"
   run_test("#{$bin}gt gtf_to_gff3 " +
-           "#{$gttestdata}gtf/Drosophila_melanogaster.BDGP5.4.50.gtf",
+           "#{$gttestdata}gtf/Drosophila_melanogaster.BDGP5.4.50.gtf " +
+           "| #{$bin}gt gff3 -sort ",
            :maxtime => 30)
   run "diff #{$last_stdout} " +
-      "#{$gttestdata}gff3/Drosophila_melanogaster.BDGP5.4.50.gff3"
+      "ref_sorted.gff3"
   end
 end
