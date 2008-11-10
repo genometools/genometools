1750e085b516c7496c6862e31b9078533350b8b8
Gordon Gremme <gremme@zbh.uni-hamburg.de>
2008-11-10 16:08:01 +0100

    cosmetics

--- src/tools/gt_gff3.c
+++ src/tools/gt_gff3.c
@@ -266,8 +266,8 @@ static int gt_gff3_runner(int argc, const char **argv, int parsed_args,
 GtTool* gt_gff3(void)
 {
   return gt_tool_new(gt_gff3_arguments_new,
-                  gt_gff3_arguments_delete,
-                  gt_gff3_option_parser_new,
-                  NULL,
-                  gt_gff3_runner);
+                     gt_gff3_arguments_delete,
+                     gt_gff3_option_parser_new,
+                     NULL,
+                     gt_gff3_runner);
 }
