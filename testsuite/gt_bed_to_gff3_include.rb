cd2969bdeb755cce6e875140d5cd659f3abe79ba
Gordon Gremme <gremme@zbh.uni-hamburg.de>
2008-11-11 01:18:30 +0100

    test BED files in gttestdata

--- testsuite/gt_bed_to_gff3_include.rb
+++ testsuite/gt_bed_to_gff3_include.rb
@@ -1,11 +1,17 @@
-i = 1
-for infile in `ls #{$testdata}/bed_files/*.bed` do
-  Name "gt bed_to_gff3 #{File.basename(infile).chomp!}"
-  Keywords "gt_bed_to_gff3"
-  Test do
-    run_test "#{$bin}gt bed_to_gff3 #{infile}"
-    #outfile = infile.gsub(/\.bed$/, ".gff3")
-    #run "diff #{$last_stdout} #{outfile}"
+def process_bed_files(dir)
+  for infile in `ls #{dir}/*.bed` do
+    Name "gt bed_to_gff3 #{File.basename(infile).chomp!}"
+    Keywords "gt_bed_to_gff3"
+    Test do
+      run_test "#{$bin}gt bed_to_gff3 #{infile}"
+      #outfile = infile.gsub(/\.bed$/, ".gff3")
+      #run "diff #{$last_stdout} #{outfile}"
+    end
   end
-  i += 1
+end
+
+process_bed_files("#{$testdata}/bed_files")
+
+if $gttestdata then
+  process_bed_files("#{$gttestdata}/bed")
 end
