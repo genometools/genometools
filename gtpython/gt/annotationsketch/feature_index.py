65e437532b5522e9314b90447332d8d7bb8c63c4
Sascha Steinbiss <steinbiss@zbh.uni-hamburg.de>
2008-11-12 10:41:51 +0100

    add get_feature_for_range() to python bindings

--- gtpython/gt/annotationsketch/feature_index.py
+++ gtpython/gt/annotationsketch/feature_index.py
@@ -74,6 +74,21 @@ class FeatureIndex:
     gtlib.gt_feature_index_get_range_for_seqid(self.fi, byref(range), seqid)
     return range
 
+  def get_features_for_range(self, start, end, seqid):
+    from ctypes import byref
+    a = Array()
+    err = Error()
+    rng = Range(start, end)
+    rval = gtlib.gt_feature_index_get_features_for_range(self.fi, a, \
+                                                         seqid, byref(rng),\
+                                                         err)
+    if rval != 0:
+        gterror(err)
+    result = []
+    for i in range(a.size()):
+        result.append(GenomeNode(gtlib.gt_genome_node_rec_ref(a.get(i))))
+    return result
+
   def register(cls, gtlib):
     from ctypes import c_char_p, c_void_p, c_int, POINTER
     gtlib.gt_feature_index_get_features_for_seqid.restype = c_void_p
