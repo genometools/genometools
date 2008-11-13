c014fb6ffd045203e9174a47d3e165982de221b0
Sascha Steinbiss <steinbiss@zbh.uni-hamburg.de>
2008-11-13 11:17:58 +0100

    improve FeatureIndex argtypes

--- gtpython/gt/annotationsketch/feature_index.py
+++ gtpython/gt/annotationsketch/feature_index.py
@@ -92,15 +92,18 @@ class FeatureIndex:
   def register(cls, gtlib):
     from ctypes import c_char_p, c_void_p, c_int, POINTER
     gtlib.gt_feature_index_get_features_for_seqid.restype = c_void_p
-    gtlib.gt_feature_index_add_gff3file.argtypes = [FeatureIndex, c_char_p, \
+    gtlib.gt_feature_index_add_gff3file.argtypes = [c_void_p, c_char_p, \
                                                     Error]
     gtlib.gt_feature_index_get_first_seqid.restype = c_char_p
     gtlib.gt_feature_index_get_seqids.restype = c_void_p
     gtlib.gt_feature_index_has_seqid.argtypes = [c_void_p, c_char_p]
-    gtlib.gt_feature_index_has_seqid.restype = c_int
     gtlib.gt_feature_index_get_range_for_seqid.argtypes = [c_void_p, \
                                                            POINTER(Range), \
                                                            c_char_p]
+    gtlib.gt_feature_index_get_features_for_range.argtypes = [c_void_p, Array, \
+                                                              c_char_p, \
+                                                              POINTER(Range), \
+                                                              Error]
   register = classmethod(register)
 
 
