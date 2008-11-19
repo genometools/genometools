0d9f1b60715867e9e2a8203cefd5371d76df8826
Gordon Gremme <gremme@zbh.uni-hamburg.de>
2008-11-19 16:26:27 +0100

    run -> run_test

--- testsuite/gt_stat_include.rb
+++ testsuite/gt_stat_include.rb
@@ -15,42 +15,46 @@ end
 Name "gt stat (no options)"
 Keywords "gt_stat"
 Test do
-  run "#{$bin}gt stat #{$testdata}standard_gene_as_tree.gff3"
+  run_test "#{$bin}gt stat #{$testdata}standard_gene_as_tree.gff3"
   run "diff #{$last_stdout} #{$testdata}gt_stat_test_1.out"
 end
 
 Name "gt stat (-genelengthdistri)"
 Keywords "gt_stat"
 Test do
-  run "#{$bin}gt stat -genelengthdistri #{$testdata}standard_gene_as_tree.gff3"
+  run_test "#{$bin}gt stat -genelengthdistri " +
+           "#{$testdata}standard_gene_as_tree.gff3"
   run "diff #{$last_stdout} #{$testdata}gt_stat_test_2.out"
 end
 
 Name "gt stat (-exonlengthdistri)"
 Keywords "gt_stat"
 Test do
-  run "#{$bin}gt stat -exonlengthdistri #{$testdata}standard_gene_as_tree.gff3"
+  run_test "#{$bin}gt stat -exonlengthdistri " +
+           "#{$testdata}standard_gene_as_tree.gff3"
   run "diff #{$last_stdout} #{$testdata}gt_stat_test_3.out"
 end
 
 Name "gt stat (-intronlengthdistri)"
 Keywords "gt_stat"
 Test do
-  run "#{$bin}gt stat -intronlengthdistri #{$testdata}standard_gene_with_introns_as_tree.gff3"
+  run_test "#{$bin}gt stat -intronlengthdistri " +
+           "#{$testdata}standard_gene_with_introns_as_tree.gff3"
   run "diff #{$last_stdout} #{$testdata}gt_stat_test_4.out"
 end
 
 Name "gt stat (-genescoredistri)"
 Keywords "gt_stat"
 Test do
-  run "#{$bin}gt stat -genescoredistri #{$testdata}standard_gene_as_tree.gff3"
+  run_test "#{$bin}gt stat -genescoredistri " +
+           "#{$testdata}standard_gene_as_tree.gff3"
   run "diff #{$last_stdout} #{$testdata}gt_stat_test_5.out"
 end
 
 Name "gt stat (count LTR retrotransposons)"
 Keywords "gt_stat"
 Test do
-  run "#{$bin}gt stat -v #{$testdata}gt_eval_ltr_test_1.in"
+  run_test "#{$bin}gt stat -v #{$testdata}gt_eval_ltr_test_1.in"
   run "grep -v 'processing file' #{$last_stdout}"
   run "diff #{$last_stdout} #{$testdata}gt_stat_test_6.out"
 end
@@ -58,21 +62,23 @@ end
 Name "gt stat (-exonnumberdistri standard gene)"
 Keywords "gt_stat"
 Test do
-  run "#{$bin}gt stat -exonnumberdistri #{$testdata}standard_gene_as_tree.gff3"
+  run_test "#{$bin}gt stat -exonnumberdistri " +
+           "#{$testdata}standard_gene_as_tree.gff3"
   run "diff #{$last_stdout} #{$testdata}gt_stat_exonnumberdistri_standard.out"
 end
 
 Name "gt stat (-exonnumberdistri encode)"
 Keywords "gt_stat"
 Test do
-  run "#{$bin}gt stat -exonnumberdistri #{$testdata}encode_known_genes_Mar07.gff3"
+  run_test "#{$bin}gt stat -exonnumberdistri " +
+           "#{$testdata}encode_known_genes_Mar07.gff3"
   run "diff #{$last_stdout} #{$testdata}gt_stat_exonnumberdistri_encode.out"
 end
 
 Name "gt stat (unsorted)"
 Keywords "gt_stat"
 Test do
-  run "#{$bin}gt stat #{$testdata}unsorted_gff3_file.txt"
+  run_test "#{$bin}gt stat #{$testdata}unsorted_gff3_file.txt"
 end
 
 Name "gt stat (embedded FASTA sequence)"
