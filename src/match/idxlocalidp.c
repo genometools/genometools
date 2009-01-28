166485aba4e3a813b99a09e337accbda39a7037e
Stefan Kurtz <kurtz@zbh.uni-hamburg.de>
2009-01-28 15:32:28 +0100

    Added DP method of Z. Tang.

---
+++ src/match/idxlocalidp.c
@@ -0,0 +1,468 @@
+#include <stdarg.h>
+#include "core/ma_api.h"
+#include "core/symboldef.h"
+#include "core/unused_api.h"
+#include "absdfstrans-imp.h"
+
+typedef struct
+{
+  long **alpha,
+    g,
+    s;                          /* g and s must be negative */
+} Cost;
+
+typedef struct
+{
+  const Cost *cost;
+  const Uchar *qseq;
+  unsigned long lengthofqseq,
+                threshold;
+} Limdfsconstinfo;
+
+typedef struct
+{
+  long Rvalue,
+       Dvalue,
+       Ivalue,
+       bestvalue;
+} Matrixvalue;
+
+typedef struct
+{
+  Matrixvalue *colvalues;
+  unsigned long pprefixlen;
+  long maxvalue;
+} Column;
+
+typedef struct
+{
+  Limdfsstatus status;
+  unsigned long qseqendpos;
+  long alignmentscore;
+} Idxlocaliresult;
+
+static long max2 (long a,long b)
+{
+  return (a < b) ? b : a;
+}
+
+static long max3 (long a,long b,long c)
+{
+  long temp;
+
+  temp = (a < b) ? b : a;
+  return (temp < c) ? c : temp;
+}
+
+static void firstcolumn (Column *column,
+                         const Cost *cost,
+                         unsigned long lengthofqseq)
+{
+  unsigned long j;
+
+  column->colvalues[0].Rvalue = 0;
+  column->colvalues[0].Dvalue = cost->g;
+  column->colvalues[0].Ivalue = cost->g;
+  column->colvalues[0].bestvalue = 0;
+  column->maxvalue = 0;
+  for (j = 1; j <= lengthofqseq; j++)
+  {
+    column->colvalues[j].Rvalue = -1;
+    column->colvalues[j].Dvalue = -1;
+    if (column->colvalues[j - 1].Ivalue > 0)
+    {
+      if (column->colvalues[j - 1].bestvalue > 0)
+      {
+        column->colvalues[j].Ivalue =
+          max2 (column->colvalues[j - 1].Ivalue + cost->s,
+                column->colvalues[j - 1].bestvalue + cost->g +
+                cost->s);
+      } else
+      {
+        column->colvalues[j].Ivalue =
+          column->colvalues[j - 1].Ivalue + cost->s;
+      }
+    } else
+    {
+      if (column->colvalues[j - 1].bestvalue > 0)
+      {
+        column->colvalues[j].Ivalue =
+          column->colvalues[j - 1].bestvalue + cost->g + cost->s;
+      } else
+      {
+        column->colvalues[j].Ivalue = -1;
+      }
+    }
+    column->colvalues[j].bestvalue =
+      max3 (column->colvalues[j].Rvalue,
+            column->colvalues[j].Dvalue,
+            column->colvalues[j].Ivalue);
+    if (column->colvalues[j].bestvalue > column->maxvalue)
+    {
+      column->maxvalue = column->colvalues[j].bestvalue;
+      column->pprefixlen = j;
+    }
+  }
+}
+
+static void nextcolumn (Column *outcol,
+                        const Cost *cost,
+                        const Uchar dbchar,
+                        const Uchar *qseq,
+                        unsigned long lengthofqseq,
+                        const Column *incol)
+{
+  unsigned long j;
+
+  /*
+  outcol = gt_malloc (sizeof (Column));
+  outcol->colvalues =
+    gt_malloc (sizeof (Matrixvalue) * (lengthofqseq + 1));
+  */
+  outcol->colvalues[0].Rvalue = outcol->colvalues[0].Ivalue = -1;
+  if (incol->colvalues[0].Dvalue > 0)
+  {
+    if (incol->colvalues[0].bestvalue > 0)
+    {
+      outcol->colvalues[0].Dvalue =
+        max2 (incol->colvalues[0].Dvalue + cost->s,
+              incol->colvalues[0].bestvalue + cost->g + cost->s);
+    } else
+    {
+      outcol->colvalues[0].Dvalue =
+        incol->colvalues[0].Dvalue + cost->s;
+    }
+  } else
+  {
+    if (incol->colvalues[0].bestvalue > 0)
+    {
+      outcol->colvalues[0].Dvalue =
+        incol->colvalues[0].bestvalue + cost->g + cost->s;
+    } else
+    {
+      outcol->colvalues[0].Dvalue = -1;
+    }
+  }
+  outcol->colvalues[0].bestvalue =
+    max3 (outcol->colvalues[0].Rvalue,
+          outcol->colvalues[0].Dvalue,
+          outcol->colvalues[0].Ivalue);
+  outcol->maxvalue = 0;
+  outcol->pprefixlen = 0;
+  for (j = 1; j <= lengthofqseq; j++)
+  {
+    //replacement
+    if (incol->colvalues[j - 1].bestvalue > 0)
+    {
+      outcol->colvalues[j].Rvalue = incol->colvalues[j - 1].bestvalue + 
+                                    cost->alpha[dbchar][qseq[j]];   //compare
+    } else
+    {
+      outcol->colvalues[j].Rvalue = -1;
+    }
+    //deletion
+    if (incol->colvalues[j].Dvalue > 0)
+    {
+      if (incol->colvalues[j].bestvalue > 0)
+      {
+        outcol->colvalues[j].Dvalue =
+          max2 (incol->colvalues[j].Dvalue + cost->s,
+                incol->colvalues[j].bestvalue + cost->g + cost->s);
+      } else
+      {
+        outcol->colvalues[j].Dvalue = incol->colvalues[j].Dvalue + cost->s;
+      }
+    } else
+    {
+      if (incol->colvalues[j].bestvalue > 0)
+      {
+        outcol->colvalues[j].Dvalue =
+          incol->colvalues[j].bestvalue + cost->g + cost->s;
+      } else
+      {
+        outcol->colvalues[j].Dvalue = -1;
+      }
+    }
+    //insertion
+    if (outcol->colvalues[j - 1].Ivalue > 0)
+    {
+      if (outcol->colvalues[j - 1].bestvalue > 0)
+      {
+        outcol->colvalues[j].Ivalue =
+          max2 (outcol->colvalues[j - 1].Ivalue + cost->s,
+                outcol->colvalues[j - 1].bestvalue + cost->g +
+                cost->s);
+      } else
+      {
+        outcol->colvalues[j].Ivalue =
+          outcol->colvalues[j - 1].Ivalue + cost->s;
+      }
+    } else
+    {
+      if (outcol->colvalues[j - 1].bestvalue > 0)
+      {
+        outcol->colvalues[j].Ivalue =
+          outcol->colvalues[j - 1].bestvalue + cost->g + cost->s;
+      } else
+      {
+        outcol->colvalues[j].Ivalue = -1;
+      }
+    }
+
+    //best score
+    outcol->colvalues[j].bestvalue =
+      max3 (outcol->colvalues[j].Rvalue,
+            outcol->colvalues[j].Dvalue,
+            outcol->colvalues[j].Ivalue);
+    if (outcol->colvalues[j].bestvalue > outcol->maxvalue)
+    {
+      outcol->maxvalue = outcol->colvalues[j].bestvalue;
+      outcol->pprefixlen = j;
+    }
+  }
+}
+
+static void inplacenextcolumn (const Cost *cost,
+                               const Uchar dbchar,
+                               const Uchar *qseq,
+                               unsigned long lengthofqseq,
+                               Column *column)
+{
+  unsigned long j;
+  Matrixvalue nw, west;
+
+  column->colvalues[0].Rvalue = column->colvalues[0].Ivalue = -1;
+  if (column->colvalues[0].Dvalue > 0)
+  {
+    if (column->colvalues[0].bestvalue > 0)
+    {
+      column->colvalues[0].Dvalue =
+        max2 (column->colvalues[0].Dvalue + cost->s,
+              column->colvalues[0].bestvalue + cost->g +
+              cost->s);
+    } else
+    {
+      column->colvalues[0].Dvalue =
+        column->colvalues[0].Dvalue + cost->s;
+    }
+  } else
+  {
+    if (column->colvalues[0].bestvalue > 0)
+    {
+      column->colvalues[0].Dvalue =
+      column->colvalues[0].bestvalue + cost->g + cost->s;
+    } else
+    {
+      column->colvalues[0].Dvalue = -1;
+    }
+  }
+  column->colvalues[0].bestvalue =
+    max3 (column->colvalues[0].Rvalue,
+          column->colvalues[0].Dvalue,
+          column->colvalues[0].Ivalue);
+  column->maxvalue = 0;
+  nw = column->colvalues[0];
+  for (j = 1; j <= lengthofqseq; j++)
+
+  {
+    west = column->colvalues[j];
+    if (nw.bestvalue > 0)
+    {
+      column->colvalues[j].Rvalue = nw.bestvalue + cost->alpha[dbchar][qseq[j]];
+    } else
+    {
+      column->colvalues[j].Rvalue = -1;
+    }
+
+    //deletion
+    if (west.Dvalue > 0)
+    {
+      if (west.bestvalue > 0)
+      {
+        column->colvalues[j].Dvalue =
+          max2 (west.Dvalue + cost->s,
+                west.bestvalue + cost->g + cost->s);
+      } else
+      {
+        column->colvalues[j].Dvalue = west.Dvalue + cost->s;
+      }
+    } else
+    {
+      if (west.bestvalue > 0)
+      {
+        column->colvalues[j].Dvalue = west.bestvalue + cost->g + cost->s;
+      } else
+      {
+        column->colvalues[j].Dvalue = -1;
+      }
+    }
+
+    //insertion
+    if (column->colvalues[j - 1].Ivalue > 0)
+    {
+      if (column->colvalues[j - 1].bestvalue > 0)
+      {
+        column->colvalues[j].Ivalue =
+          max2 (column->colvalues[j - 1].Ivalue + cost->s,
+                column->colvalues[j - 1].bestvalue + cost->g +
+                cost->s);
+      } else
+      {
+        column->colvalues[j].Ivalue =
+          column->colvalues[j - 1].Ivalue + cost->s;
+      }
+    } else
+    {
+      if (column->colvalues[j - 1].bestvalue > 0)
+      {
+        column->colvalues[j].Ivalue =
+          column->colvalues[j - 1].bestvalue + cost->g + cost->s;
+      } else
+      {
+        column->colvalues[j].Ivalue = -1;
+      }
+    }
+
+    //best score
+    nw = west;
+    column->colvalues[j].bestvalue =
+      max3 (column->colvalues[j].Rvalue,
+            column->colvalues[j].Dvalue,
+            column->colvalues[j].Ivalue);
+    if (column->colvalues[j].bestvalue > column->maxvalue)
+    {
+      column->maxvalue = column->colvalues[j].bestvalue;
+      column->pprefixlen = j;
+    }
+  }
+}
+
+  /*
+     allokiert eine strukur vom Typ Limdfsconstrinfo und liefert einen 
+     Zeiger darauf zr"uck 
+   */
+
+static void *locali_allocatedfsconstinfo (GT_UNUSED unsigned int alphasize)
+{
+  Limdfsconstinfo *lci = gt_malloc (sizeof (Limdfsconstinfo));
+
+  return lci;
+}
+
+static void locali_initdfsconstinfo (void *dfsconstinfo,
+                                     unsigned int alphasize,
+                                     ...) /* initializiert Limdfsconstinfo */
+{
+  va_list ap;
+  Limdfsconstinfo *lci = (Limdfsconstinfo *) dfsconstinfo;
+
+  va_start (ap, alphasize);
+  lci->cost = va_arg (ap, const Cost *);
+  lci->qseq = va_arg (ap, const Uchar *);
+  lci->lengthofqseq = va_arg (ap, unsigned long);
+  lci->threshold = va_arg (ap, unsigned long);
+}
+
+static void locali_freedfsconstinfo (void **dfsconstinfo)
+          /* gibt Limdfsconstinfo wieder frei */
+{
+  Limdfsconstinfo *lci = (Limdfsconstinfo *) *dfsconstinfo;
+
+  gt_free (lci);
+  *dfsconstinfo = NULL;
+}
+
+static void locali_initLimdfsstate (DECLAREPTRDFSSTATE (aliascolumn),
+                                    void *dfsconstinfo)
+  /*
+     initializiert die erste Spalte mit firstcolumn 
+   */
+{
+  Column *column = (Column *) aliascolumn;
+  const Limdfsconstinfo *lci = (Limdfsconstinfo *) dfsconstinfo;
+
+  firstcolumn (column, lci->cost, lci->lengthofqseq);
+}
+
+static void locali_fullmatchLimdfsstate (Limdfsresult *limdfsresult,
+                                         DECLAREPTRDFSSTATE(aliascolumn),
+                                         GT_UNUSED Seqpos leftbound,
+                                         GT_UNUSED Seqpos rightbound,
+                                         GT_UNUSED Seqpos width,
+                                         GT_UNUSED unsigned long currentdepth,
+                                         void *dfsconstinfo)
+{
+  Column *column = (Column *) aliascolumn;
+  const Limdfsconstinfo *lci = (Limdfsconstinfo *) dfsconstinfo;
+
+  if (column->maxvalue >= lci->threshold)
+  {
+    limdfsresult->status = Limdfssuccess;
+    limdfsresult->pprefixlen = column->pprefixlen;
+    limdfsresult->distance = column->maxvalue;
+  } else
+  {
+    if (column->maxvalue > 0)
+    {
+      limdfsresult->status = Limdfscontinue;
+    } else
+    {
+      limdfsresult->status = Limdfsstop;
+    }
+  }
+}
+
+  /*
+     caste die void Zeiger f"ur Limdfsconstinfo und Idxlocaliresult und 
+     rufe nextcolumn auf 
+   */
+
+static void locali_nextLimdfsstate (
+  const void *dfsconstinfo,
+  DECLAREPTRDFSSTATE (aliasoutcol),
+  GT_UNUSED unsigned long currentdepth,
+  Uchar currentchar,
+  const DECLAREPTRDFSSTATE (aliasincol))
+{
+  const Limdfsconstinfo *lci = (const Limdfsconstinfo *) dfsconstinfo;
+  Column *outcol = (Column *) aliasoutcol;
+  const Column *incol = (const Column *) aliasincol;
+
+  nextcolumn (outcol,lci->cost,currentchar,lci->qseq,lci->lengthofqseq,incol);
+} 
+
+  /*
+     sowie vorherige Funktion aber mit nur einer Spalte,
+     auf der inplace gearbeitet wird 
+   */
+
+static void locali_inplacenextLimdfsstate (
+  const void *dfsconstinfo,
+  DECLAREPTRDFSSTATE (aliascolumn),
+  GT_UNUSED unsigned long currentdepth,
+  Uchar currentchar)
+{
+  Column *column = (Column *) aliascolumn;
+  const Limdfsconstinfo *lci = (const Limdfsconstinfo *) dfsconstinfo;
+
+  inplacenextcolumn (lci->cost,currentchar,lci->qseq,lci->lengthofqseq,column);
+} 
+
+const AbstractDfstransformer *locali_AbstractDfstransformer (void)
+{
+  static const AbstractDfstransformer locali_adfst = 
+  { sizeof (Column),
+    locali_allocatedfsconstinfo, /* allokiert eine strukur vom Typ Limddfsconstrinfo und liefert einen Zeiger darauf zr"uck */
+    locali_initdfsconstinfo,           /* initializiert Limdfsconstinfo */
+    NULL,                              /* no extract dfsconstinfo */
+    locali_freedfsconstinfo,           /* gibt Limdfsconstinfo wieder frei */
+    locali_initLimdfsstate,            /* initializiere die erste Spalte mit firstcolumn */
+    locali_fullmatchLimdfsstate,       /* "ubertrage die status und qseqendpos aufden Idxlocaliresult zeiger */
+    locali_nextLimdfsstate,            /* caste die void Zeiger f"ur Limdfsconstinfo und Idxlocaliresult und rufe nextcolumn auf */
+    locali_inplacenextLimdfsstate,     /* sowie vorherige Funktion aber mit nureiner Spalte,auf der inplace gearbeitet wird */
+#ifdef SKDEBUG
+    locali_showLimdfsstate,
+#endif /*  */
+  };
+  return &locali_adfst;
+}
