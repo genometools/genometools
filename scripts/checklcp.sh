3c82ea0c43e4b21d9d2ce1fa75eb7e80081924fb
Stefan Kurtz <kurtz@zbh.uni-hamburg.de>
2009-03-11 02:26:55 +0100

    Added the script to check the correctness of the lcp-table.

---
+++ scripts/checklcp.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e -x
+
+if test $# -ne 1
+then
+  echo "Usage: $0 <inputfile>"
+  exit 1
+fi
+
+gt suffixerator -db $1  -dna -suf -tis -lcp -indexname sideeff
+gt dev sfxmap -lcp -tis -suf sideeff
+gt suffixerator -db $1  -dna -suf -tis -lcp -maxdepth -indexname linear
+gt dev sfxmap -lcp -tis -suf linear
