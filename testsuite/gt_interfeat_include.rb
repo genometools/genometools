cb387ebb5fb1d06c0a1be56179949491b639fb65
Gordon Gremme <gremme@zbh.uni-hamburg.de>
2009-04-30 21:26:15 +0200

    add test for `gt interfeat`

---
+++ testsuite/gt_interfeat_include.rb
@@ -0,0 +1,6 @@
+Name "gt interfeat test"
+Keywords "gt_interfeat"
+Test do
+  run_test "#{$bin}gt interfeat #{$testdata}addintrons.gff3"
+  run "diff #{$last_stdout} #{$testdata}addintrons.out"
+end
