419545a439c1b1150804401c58b37f3ea8c0df1c
Sascha Steinbiss <steinbiss@zbh.uni-hamburg.de>
2009-05-09 00:34:38 +0200

    document interval_tree and add *_api.h

--- src/core/interval_tree.h
+++ src/core/interval_tree.h
@@ -18,42 +18,10 @@
 #ifndef INTERVAL_TREE_H
 #define INTERVAL_TREE_H
 
-#include "core/array.h"
 #include "core/error.h"
-#include "core/fptr_api.h"
 
-/* Interval tree data structure, implemented according to
-   Cormen et al., Introduction to Algorithms, 2nd edition, MIT Press,
-   Cambridge, MA, USA, 2001 */
+#include "core/interval_tree_api.h"
 
-typedef struct GtIntervalTree GtIntervalTree;
-typedef struct GtIntervalTreeNode GtIntervalTreeNode;
-
-typedef int (*GtIntervalTreeIteratorFunc)(GtIntervalTreeNode*, void*);
-
-/* transfers ownership of <data> to interval tree
-   if GtIntervalTreeDataFreeFunc is given */
-GtIntervalTreeNode* gt_interval_tree_node_new(void *data,
-                                               unsigned long low,
-                                               unsigned long high);
-void*                gt_interval_tree_node_get_data(GtIntervalTreeNode* n);
-
-GtIntervalTree*     gt_interval_tree_new(GtFree);
-unsigned long        gt_interval_tree_size(GtIntervalTree*);
-GtIntervalTreeNode* gt_interval_tree_find_first_overlapping(GtIntervalTree*,
-                                                            unsigned long start,
-                                                            unsigned long end);
-void                 gt_interval_tree_insert(GtIntervalTree*,
-                                             GtIntervalTreeNode*);
-/* collects data pointers of all overlapping nodes in array */
-void              gt_interval_tree_find_all_overlapping(GtIntervalTree*,
-                                                        unsigned long start,
-                                                        unsigned long end,
-                                                        GtArray*);
-int               gt_interval_tree_traverse(GtIntervalTree *it,
-                                            GtIntervalTreeIteratorFunc func,
-                                            void *data);
-void              gt_interval_tree_delete(GtIntervalTree*);
 int               gt_interval_tree_unit_test(GtError*);
 
 #endif
