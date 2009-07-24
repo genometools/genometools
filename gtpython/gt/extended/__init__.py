820c70d57b468f0df603c87eeb1835a67645b37f
Sascha Steinbiss <steinbiss@zbh.uni-hamburg.de>
2009-07-24 14:47:21 +0200

    gtpython: fix imports

--- gtpython/gt/extended/__init__.py
+++ gtpython/gt/extended/__init__.py
@@ -17,26 +17,32 @@
 # OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 #
 
+from comment_node import *
 from feature_node import *
 from custom_stream import *
 from custom_stream_example import *
+from custom_visitor import *
 from genome_node import *
 from genome_stream import *
 from gff3_in_stream import *
 from gff3_out_stream import *
 from gff3_visitor import *
+from node_visitor import *
+from region_node import *
+from sequence_node import *
 from add_introns_stream import *
 from inter_feature_stream import *
 from dup_feature_stream import *
 from merge_feature_stream import *
 
+CommentNode.register(gtlib)
 GenomeNode.register(gtlib)
+SequenceNode.register(gtlib)
 CustomStream.register(gtlib)
 FeatureNode.register(gtlib)
 FeatureNodeIterator.register(gtlib)
 GFF3InStream.register(gtlib)
 AddIntronsStream.register(gtlib)
-
 InterFeatureStream.register(gtlib)
 DuplicateFeatureStream.register(gtlib)
 MergeFeatureStream.register(gtlib)
