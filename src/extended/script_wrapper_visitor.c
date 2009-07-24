ebab1a38f6f674c335ae80bea2017e78c99deebb
Sascha Steinbiss <steinbiss@zbh.uni-hamburg.de>
2009-07-24 11:35:39 +0200

    add GtScriptWrapperVisitor

---
+++ src/extended/script_wrapper_visitor.c
@@ -0,0 +1,126 @@
+/*
+  Copyright (c) 2009 Sascha Steinbiss <steinbiss@zbh.uni-hamburg.de>
+  Copyright (c) 2009 Center for Bioinformatics, University of Hamburg
+
+  Permission to use, copy, modify, and distribute this software for any
+  purpose with or without fee is hereby granted, provided that the above
+  copyright notice and this permission notice appear in all copies.
+
+  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+
+#include "core/assert_api.h"
+#include "extended/script_wrapper_visitor.h"
+#include "extended/node_visitor_rep.h"
+
+struct GtScriptWrapperVisitor {
+  const GtNodeVisitor parent_instance;
+  GtScriptWrapperVisitorCommentNodeFunc comment_node_func;
+  GtScriptWrapperVisitorFeatureNodeFunc feature_node_func;
+  GtScriptWrapperVisitorRegionNodeFunc region_node_func;
+  GtScriptWrapperVisitorSequenceNodeFunc sequence_node_func;
+  GtScriptWrapperVisitorFreeFunc free_func;
+};
+
+#define gt_script_wrapper_visitor_cast(GV)\
+        gt_node_visitor_cast(gt_script_wrapper_visitor_class(), GV)
+
+static void script_wrapper_visitor_free(GtNodeVisitor *gv)
+{
+  GtScriptWrapperVisitor *swv;
+  gt_assert(gv);
+  swv  = gt_script_wrapper_visitor_cast(gv);
+  if (swv->free_func)
+    swv->free_func(NULL);
+}
+
+static int script_wrapper_visitor_feature_node(GtNodeVisitor *nv,
+                                               GtFeatureNode *fn,
+                                               GtError *err)
+{
+  GtScriptWrapperVisitor *swv;
+  int had_err = 0;
+  gt_error_check(err);
+  swv = gt_script_wrapper_visitor_cast(nv);
+  if (swv->feature_node_func)
+    had_err = swv->feature_node_func(fn, err);
+  return had_err;
+}
+
+static int script_wrapper_visitor_region_node(GtNodeVisitor *nv,
+                                              GtRegionNode *rn,
+                                              GtError *err)
+{
+  GtScriptWrapperVisitor *swv;
+  int had_err = 0;
+  gt_error_check(err);
+  swv = gt_script_wrapper_visitor_cast(nv);
+  if (swv->region_node_func)
+    had_err = swv->region_node_func(rn, err);
+  return had_err;
+}
+
+static int script_wrapper_visitor_comment_node(GtNodeVisitor *nv,
+                                               GtCommentNode *cn,
+                                               GtError *err)
+{
+  GtScriptWrapperVisitor *swv;
+  int had_err = 0;
+  gt_error_check(err);
+  swv = gt_script_wrapper_visitor_cast(nv);
+  if (swv->comment_node_func)
+    had_err = swv->comment_node_func(cn, err);
+  return had_err;
+}
+
+static int script_wrapper_visitor_sequence_node(GtNodeVisitor *nv,
+                                                GtSequenceNode *sn,
+                                                GtError *err)
+{
+  GtScriptWrapperVisitor *swv;
+  int had_err = 0;
+  gt_error_check(err);
+  swv = gt_script_wrapper_visitor_cast(nv);
+  if (swv->sequence_node_func)
+    had_err = swv->sequence_node_func(sn, err);
+  return had_err;
+}
+
+const GtNodeVisitorClass* gt_script_wrapper_visitor_class()
+{
+  static const GtNodeVisitorClass *nvc = NULL;
+  if (!nvc) {
+    nvc = gt_node_visitor_class_new(sizeof (GtScriptWrapperVisitor),
+                                    script_wrapper_visitor_free,
+                                    script_wrapper_visitor_comment_node,
+                                    script_wrapper_visitor_feature_node,
+                                    script_wrapper_visitor_region_node,
+                                    script_wrapper_visitor_sequence_node);
+  }
+  return nvc;
+}
+
+GtNodeVisitor*
+gt_script_wrapper_visitor_new(GtScriptWrapperVisitorCommentNodeFunc cn,
+                              GtScriptWrapperVisitorFeatureNodeFunc fn,
+                              GtScriptWrapperVisitorRegionNodeFunc rn,
+                              GtScriptWrapperVisitorSequenceNodeFunc sn,
+                              GtScriptWrapperVisitorFreeFunc free_func)
+{
+  GtNodeVisitor *nv;
+  GtScriptWrapperVisitor *swv;
+  nv = gt_node_visitor_create(gt_script_wrapper_visitor_class());
+  swv = gt_script_wrapper_visitor_cast(nv);
+  swv->comment_node_func = cn;
+  swv->feature_node_func = fn;
+  swv->region_node_func = rn;
+  swv->sequence_node_func = sn;
+  swv->free_func = free_func;
+  return nv;
+}
