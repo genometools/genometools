a7d1b51fda6a2d8c6ab7d4c33c11aacb52e93f9b
Stefan Kurtz <kurtz@zbh.uni-hamburg.de>
2009-08-05 21:52:32 +0200

    Corrected year.

--- src/match/giextract.c
+++ src/match/giextract.c
@@ -1,6 +1,6 @@
 /*
   Copyright (c) 2007-2009 Stefan Kurtz <kurtz@zbh.uni-hamburg.de>
-  Copyright (c) 2007-2008 Center for Bioinformatics, University of Hamburg
+  Copyright (c) 2007-2009 Center for Bioinformatics, University of Hamburg
 
   Permission to use, copy, modify, and distribute this software for any
   purpose with or without fee is hereby granted, provided that the above
