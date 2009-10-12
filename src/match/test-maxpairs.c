5a0f69c93e41c1907f1cee28422192729ad5d0b7
Stefan Kurtz <kurtz@zbh.uni-hamburg.de>
2009-10-12 09:48:29 +0200

    Put code from esa-selfmatch into test-maxpairs.c

--- src/match/test-maxpairs.c
+++ src/match/test-maxpairs.c
@@ -34,6 +34,7 @@ int testmaxpairs(GT_UNUSED const GtStr *indexname,
 
 #else
 
+#include <limits.h>
 #include "core/alphabet.h"
 #include "core/array.h"
 #include "core/arraydef.h"
@@ -44,9 +45,136 @@ int testmaxpairs(GT_UNUSED const GtStr *indexname,
 #include "format64.h"
 #include "echoseq.h"
 #include "encseq-def.h"
+#include "seqpos-def.h"
+#include "esa-seqread.h"
+#include "sfx-suffixer.h"
+#include "verbose-def.h"
+#include "sfx-apfxlen.h"
+#include "sfx-progress.h"
+#include "esa-maxpairs.h"
 
-#include "esa-selfmatch.pr"
 #include "arrcmp.pr"
+
+typedef struct
+{
+  unsigned int minlength;
+  Encodedsequence *encseq;
+  int (*processmaxmatch)(void *,Seqpos,Seqpos,Seqpos,GtError *);
+  void *processmaxmatchinfo;
+} Substringmatchinfo;
+
+static int constructsarrandrunmaxpairs(
+                 Substringmatchinfo *ssi,
+                 Readmode readmode,
+                 unsigned int prefixlength,
+                 unsigned int numofparts,
+                 Sfxprogress *sfxprogress,
+                 Verboseinfo *verboseinfo,
+                 GtError *err)
+{
+  const Seqpos *suftabptr;
+  Seqpos numberofsuffixes;
+  bool haserr = false;
+  Sfxiterator *sfi;
+  bool specialsuffixes = false;
+
+  sfi = newSfxiterator(ssi->encseq,
+                       readmode,
+                       prefixlength,
+                       numofparts,
+                       NULL, /* oulcpinfo */
+                       NULL, /* sfxstrategy */
+                       sfxprogress,
+                       NULL, /* verbosinfo */
+                       err);
+  if (sfi == NULL)
+  {
+    haserr = true;
+  } else
+  {
+    Sequentialsuffixarrayreader *ssar = NULL;
+    bool firstpage = true;
+
+    ssar = newSequentialsuffixarrayreaderfromRAM(ssi->encseq,
+                                                 readmode);
+    while (true)
+    {
+      suftabptr = nextSfxiterator(&numberofsuffixes,&specialsuffixes,sfi);
+      if (suftabptr == NULL || specialsuffixes)
+      {
+        break;
+      }
+      updateSequentialsuffixarrayreaderfromRAM(ssar,
+                                               suftabptr,
+                                               firstpage,
+                                               numberofsuffixes);
+      firstpage = false;
+      if (enumeratemaxpairs(ssar,
+                            ssi->encseq,
+                            readmode,
+                            ssi->minlength,
+                            ssi->processmaxmatch,
+                            ssi->processmaxmatchinfo,
+                            verboseinfo,
+                            err) != 0)
+      {
+        haserr = true;
+      }
+    }
+    if (ssar != NULL)
+    {
+      freeSequentialsuffixarrayreader(&ssar);
+    }
+  }
+  if (sfi != NULL)
+  {
+    freeSfxiterator(&sfi);
+  }
+  return haserr ? -1 : 0;
+}
+
+int sarrselfsubstringmatch(const GtUchar *dbseq,
+                           Seqpos dblen,
+                           const GtUchar *query,
+                           unsigned long querylen,
+                           unsigned int minlength,
+                           const GtAlphabet *alpha,
+                           int (*processmaxmatch)(void *,Seqpos,
+                                                  Seqpos,Seqpos,GtError *),
+                           void *processmaxmatchinfo,
+                           Verboseinfo *verboseinfo,
+                           GtError *err)
+{
+  Substringmatchinfo ssi;
+  unsigned int numofchars;
+  bool haserr = false;
+
+  ssi.encseq = plain2encodedsequence(true,
+                                     dbseq,
+                                     dblen,
+                                     query,
+                                     querylen,
+                                     alpha,
+                                     verboseinfo);
+  ssi.minlength = minlength;
+  ssi.processmaxmatch = processmaxmatch;
+  ssi.processmaxmatchinfo = processmaxmatchinfo;
+  numofchars = gt_alphabet_num_of_chars(alpha);
+  if (constructsarrandrunmaxpairs(&ssi,
+                                  Forwardmode,
+                                  recommendedprefixlength(numofchars,
+                                                          dblen+querylen+1),
+                                  1U, /* parts */
+                                  NULL,
+                                  verboseinfo,
+                                  err) != 0)
+  {
+    haserr = true;
+  }
+  removealpharef(ssi.encseq);
+  encodedsequence_free(&ssi.encseq);
+  return haserr ? -1 : 0;
+}
 
 static unsigned long getrecordnumulong(const unsigned long *recordseps,
                                        unsigned long numofrecords,
