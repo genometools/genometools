d6a74eeeb2ed2ff5ff6b410d99b358060ecf6c49
Gordon Gremme <gremme@zbh.uni-hamburg.de>
2010-05-18 01:27:31 +0200

    fix copyright

--- src/match/sfx-ri-def.h
+++ src/match/sfx-ri-def.h
@@ -1,5 +1,5 @@
 /*
-  Copyright (c) 2005-2007 Gordon Gremme <gremme@zbh.uni-hamburg.de>
+  Copyright (c) 2005-2007 Stefan Kurtz <kurtz@zbh.uni-hamburg.de>
   Copyright (c) 2005-2007 Center for Bioinformatics, University of Hamburg
 
   Permission to use, copy, modify, and distribute this software for any
