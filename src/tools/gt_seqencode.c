800060ebe18aca898a29d79e6dec4ae01f737785
Gordon Gremme <gremme@zbh.uni-hamburg.de>
2010-05-19 14:53:27 +0200

    show encoded statistics

--- src/tools/gt_seqencode.c
+++ src/tools/gt_seqencode.c
@@ -14,8 +14,10 @@
   OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 */
 
+#include "core/alphabet.h"
 #include "core/ma.h"
 #include "core/encseq_api.h"
+#include "core/fileutils.h"
 #include "core/str_array_api.h"
 #include "core/unused_api.h"
 #include "tools/gt_seqencode.h"
@@ -50,12 +52,40 @@ static int encode_sequence_file(const char *seqfile, GtError *err)
   return had_err;
 }
 
+static off_t index_size(const char *prefix, const char *suffix)
+{
+ off_t size;
+ GtStr *index_name = gt_str_new_cstr(prefix);
+ gt_str_append_cstr(index_name, suffix);
+ size = gt_file_size(gt_str_get(index_name));
+ gt_str_delete(index_name);
+ return size;
+}
+
+static void show_encoded_statistics(const char *seqfile)
+{
+  off_t orig_size, enc_size;
+  gt_assert(seqfile);
+  orig_size = gt_file_size(seqfile);
+  enc_size  = index_size(seqfile, GT_ALPHABETFILESUFFIX);
+  enc_size += index_size(seqfile, GT_ENCSEQFILESUFFIX);
+  enc_size += index_size(seqfile, GT_SSPTABFILESUFFIX);
+  enc_size += index_size(seqfile, GT_DESTABFILESUFFIX);
+  enc_size += index_size(seqfile, GT_SDSTABFILESUFFIX);
+  printf("encoded sequence file(s) are %.1f%% of original file size\n",
+         ((double) enc_size / orig_size) * 100.0);
+}
+
 static int gt_seqencode_runner(GT_UNUSED int argc, const char **argv,
                                int parsed_args, GT_UNUSED void *tool_arguments,
                                GtError *err)
 {
+  int had_err;
   gt_error_check(err);
-  return encode_sequence_file(argv[parsed_args], err);
+  had_err = encode_sequence_file(argv[parsed_args], err);
+  if (!had_err)
+    show_encoded_statistics(argv[parsed_args]);
+  return had_err;
 }
 
 GtTool* gt_seqencode(void)
