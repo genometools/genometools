53e2c06eb38223c91ac8f3455156e9a5c14f5ddf
Sascha Steinbiss <steinbiss@zbh.uni-hamburg.de>
2010-06-10 14:49:10 +0200

    mention `showtime' environment option in help text

--- gtdata/doc/gt.lua
+++ gtdata/doc/gt.lua
@@ -2,5 +2,11 @@ print ([[
 
 Set the environment variable GT_MEM_BOOKKEEPING=on to enable memory bookkeeping
 (e.g., like this: env GT_MEM_BOOKKEEPING=on gt).
+
 Set the environment variable GT_ENV_OPTIONS=-spacepeak to show a spacepeak after
-program run (GT_ENV_OPTIONS=-help shows possible ``environment options'').]])
+program run .
+Set the environment variable GT_ENV_OPTIONS=-showtime to show processing times
+for some program parts if implemented.
+
+Combinations are possible. Running the `gt' binary with GT_ENV_OPTIONS=-help
+shows all possible ``environment options'')]])
