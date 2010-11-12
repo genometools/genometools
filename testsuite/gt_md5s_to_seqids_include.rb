3e9d7c8ff3c4ed9381bc2fc8a9b2023e9a43fc86
Gordon Gremme <gremme@zbh.uni-hamburg.de>
2010-11-12 01:35:09 +0100

    fix tests

--- testsuite/gt_md5s_to_seqids_include.rb
+++ testsuite/gt_md5s_to_seqids_include.rb
@@ -1,7 +1,7 @@
 Name "gt md5s_to_seqids (U89959_sas.gff3)"
 Keywords "gt_md5s_to_seqids"
 Test do
-  run_test "#{$bin}gt dev md5s_to_seqids -seqfiles " +
+  run_test "#{$bin}gt md5s_to_seqids -seqfiles " +
            "#{$testdata}U89959_genomic.fas #{$testdata}U89959_ests.fas -- " +
            "#{$testdata}U89959_sas.gff3md5"
   run "diff #{$last_stdout} #{$testdata}U89959_sas.gff3"
@@ -10,7 +10,7 @@ end
 Name "gt md5s_to_seqids (U89959_csas.gff3)"
 Keywords "gt_md5s_to_seqids"
 Test do
-  run_test "#{$bin}gt dev md5s_to_seqids -seqfiles " +
+  run_test "#{$bin}gt md5s_to_seqids -seqfiles " +
            "#{$testdata}U89959_genomic.fas #{$testdata}U89959_ests.fas -- " +
            "#{$testdata}U89959_csas.gff3md5"
   run "diff #{$last_stdout} #{$testdata}U89959_csas.gff3"
--- testsuite/gt_seqids_to_md5s_include.rb
+++ testsuite/gt_seqids_to_md5s_include.rb
@@ -1,7 +1,7 @@
 Name "gt seqids_to_md5s (U89959_sas.gff3)"
 Keywords "gt_seqids_to_md5s"
 Test do
-  run_test "#{$bin}gt dev seqids_to_md5s -seqfiles " +
+  run_test "#{$bin}gt seqids_to_md5s -seqfiles " +
            "#{$testdata}U89959_genomic.fas #{$testdata}U89959_ests.fas " +
            "-matchdesc #{$testdata}U89959_sas.gff3"
   run "diff #{$last_stdout} #{$testdata}U89959_sas.gff3md5"
@@ -10,7 +10,7 @@ end
 Name "gt seqids_to_md5s (U89959_csas.gff3)"
 Keywords "gt_seqids_to_md5s"
 Test do
-  run_test "#{$bin}gt dev seqids_to_md5s -seqfiles " +
+  run_test "#{$bin}gt seqids_to_md5s -seqfiles " +
            "#{$testdata}U89959_genomic.fas #{$testdata}U89959_ests.fas " +
            "-matchdesc #{$testdata}U89959_csas.gff3"
   run "diff #{$last_stdout} #{$testdata}U89959_csas.gff3md5"
