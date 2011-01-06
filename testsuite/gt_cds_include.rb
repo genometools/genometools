5cf1b24f3e9e48e10f341034d58577276acea289
Gordon Gremme <gremme@zbh.uni-hamburg.de>
2011-01-06 22:22:08 +0100

    fix large test

--- testsuite/gt_cds_include.rb
+++ testsuite/gt_cds_include.rb
@@ -105,7 +105,8 @@ if $gttestdata then
   Name "gt cds bug"
   Keywords "gt_cds"
   Test do
-    run_test "#{$bin}gt cds -seqfile #{$gttestdata}cds/marker_region.fas " +
+    run_test "#{$bin}gt cds -startcodon yes -minorflen 1 " +
+             "-seqfile #{$gttestdata}cds/marker_region.fas " +
              "#{$gttestdata}cds/marker_bug.gff3"
     run "diff #{$last_stdout} #{$gttestdata}cds/marker_bug.out"
   end
