858196b27a05a5cf1421cf32984997e8c72e0eaf
Stefan Kurtz <kurtz@zbh.uni-hamburg.de>
2011-05-20 14:17:54 +0200

    Added first version of new esa bottom up traversal.

---
+++ src/match/esa-bottomup.c
@@ -0,0 +1,137 @@
+/*
+  Copyright (c) 2011 Stefan Kurtz <kurtz@zbh.uni-hamburg.de>
+  Copyright (c) 2011 Center for Bioinformatics, University of Hamburg
+
+  Permission to use, copy, modify, and distribute this software for any
+  purpose with or without fee is hereby granted, provided that the above
+  copyright notice and this permission notice appear in all copies.
+
+  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+
+#include <limits.h>
+#include "core/unused_api.h"
+#include "core/ma.h"
+#include "esa-bottomup.h"
+
+#define TOP           stackspace[nextfreeItvinfo-1]
+#define INCSTACKSIZE  32
+
+#define PUSHDFS(LCP,LB,RMOSTBD,PREVIOUSPTR)\
+        if (nextfreeItvinfo >= allocatedItvinfo)\
+        {\
+          gt_assert(nextfreeItvinfo == allocatedItvinfo);\
+          stackspace = allocItvinfo(PREVIOUSPTR,\
+                                    allocatedItvinfo,\
+                                    allocatedItvinfo+INCSTACKSIZE,\
+                                    allocateBUinfo,\
+                                    state);\
+          allocatedItvinfo += INCSTACKSIZE;\
+        }\
+        stackspace[nextfreeItvinfo].lcp = LCP;\
+        stackspace[nextfreeItvinfo].lb = LB;\
+        stackspace[nextfreeItvinfo].rb = ULONG_MAX;\
+        stackspace[nextfreeItvinfo].rmostbd = RMOSTBD;\
+        nextfreeItvinfo++
+
+typedef struct
+{
+  unsigned long lcp, lb, rb, rmostbd;
+  GtBUinfo *info;
+} GtBUItvinfo;
+
+static GtBUItvinfo *allocBUItvinfo(GtBUItvinfo *ptr,
+                                 unsigned long currentallocated,
+                                 unsigned long allocated,
+                                 GtBUinfo *(*allocateBUinfo)(GtBUstate *),
+                                 GtBUstate *state)
+{
+  unsigned long i;
+  GtBUItvinfo *itvinfo;
+
+  itvinfo = gt_realloc(ptr,sizeof(*itvinfo) * allocated);
+  if (allocateBUinfo != NULL)
+  {
+    gt_assert(allocated > currentallocated);
+    for (i=currentallocated; i<allocated; i++)
+    {
+      itvinfo[i].info = allocateBUinfo(state);
+    }
+  }
+  gt_assert(itvinfo != NULL);
+  return itvinfo;
+}
+
+static void freeBUItvinfo(GtBUItvinfo *ptr,
+                          unsigned long allocated,
+                          void (*freeBUinfo)(GtBUinfo *,GtBUstate *),
+                          GtBUstate *state)
+{
+  unsigned long idx;
+
+  for (idx=0; idx<allocated; idx++)
+  {
+    freeBUinfo(ptr[idx].info,state);
+  }
+  gt_free(ptr);
+}
+
+typedef struct
+{
+  unsigned long index, suffix;
+} GtSuftabelem;
+
+typedef struct
+{
+  GtSuftabelem leftelem, rightelem;
+} GtQueuepair;
+
+#define GT_SUFTABELEMUNDEFFLAG  ULONG_MAX
+#define GT_SUFTABELEMISUNDEF(X) ((X).suffix == GT_SUFTABELEMUNDEFFLAG ? true\
+                                                                      : false)
+#define GT_SUFTABELEMSETUNDEF(X) (X).suffix = GT_SUFTABELEMUNDEFFLAG
+
+static void GtQueuepair_init(GtQueuepair *queuepair)
+{
+  queuepair->leftelem.index = queuepair->rightelem.index = 0;
+  GT_SUFTABELEMSETUNDEF(queuepair->leftelem);
+  GT_SUFTABELEMSETUNDEF(queuepair->rightelem);
+}
+
+static void GtQueuepair_add(GtQueuepair *queuepair,unsigned long index,
+                            unsigned long suffix)
+{
+  gt_assert(GT_SUFTABELEMISUNDEF(queuepair->leftelem));
+  queuepair->leftelem = queuepair->rightelem;
+  queuepair->rightelem.index = index;
+  queuepair->rightelem.suffix = suffix;
+}
+
+static void GtQueuepair_enumleaves(GtQueuepair *queuepair,unsigned long endpos)
+{
+  if (!GT_SUFTABELEMISUNDEF(queuepair->leftelem))
+  {
+    if (queuepair->leftelem.index <= endpos)
+    {
+      printf("elem=%lu\n",queuepair->leftelem.suffix);
+      GT_SUFTABELEMSETUNDEF(queuepair->leftelem);
+    } else
+    {
+      return;
+    }
+  }
+  if (!GT_SUFTABELEMISUNDEF(queuepair->rightelem))
+  {
+    if (queuepair->rightelem.index <= endpos)
+    {
+      printf("elem=%lu\n",queuepair->rightelem.suffix);
+      GT_SUFTABELEMSETUNDEF(queuepair->rightelem);
+    }
+  }
+}
