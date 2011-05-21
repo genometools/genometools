260054b5cf525b576d209916bf761146b7a360b0
Gordon Gremme <gremme@zbh.uni-hamburg.de>
2011-05-21 23:48:24 +0200

    improve docu

--- src/core/array_api.h
+++ src/core/array_api.h
@@ -84,19 +84,19 @@ unsigned long gt_array_size(const GtArray *array);
 void          gt_array_sort(GtArray *array, GtCompare compar);
 /* Sort <array> in a stable way with the given compare function <compar>. */
 void          gt_array_sort_stable(GtArray *array, GtCompare compar);
-/* Sort <array> with the given compare function <compar>. Passes a pointer to
-   userdata at <data>. */
+/* Sort <array> with the given compare function <compar>. Passes a pointer with
+   userdata <data> to <compar>. */
 void          gt_array_sort_with_data(GtArray *array,
                                       GtCompareWithData compar,
                                       void *data);
 /* Sort <array> in a stable way with the given compare function <compar>. Passes
-   a pointer to userdata at <data>. */
+   a pointer with userdata <data> to <compar>. */
 void          gt_array_sort_stable_with_data(GtArray *array,
                                              GtCompareWithData compar,
                                              void *data);
 /* Compare the content of <array_a> with the content of <array_b>.
-   <array_a> and <array_b> must have the same gt_array_size() and
-   gt_array_elem_size(). */
+   <array_a> and <array_b> must have the same <gt_array_size()> and
+   <gt_array_elem_size()>. */
 int           gt_array_cmp(const GtArray *array_a, const GtArray *array_b);
 /* Decrease the reference count for <array> or delete it, if this was the last
    reference. */
