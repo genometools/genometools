69cbbb8fba152b4d8e67b78480db6f1975667902
Gordon Gremme <gremme@zbh.uni-hamburg.de>
2011-05-22 01:40:07 +0200

    `gt eval`: fix -help output (reality -> reference)

--- gtdata/doc/eval.lua
+++ gtdata/doc/eval.lua
@@ -3,7 +3,7 @@ print ([[
 The program shows sensitivity and specificity values for certain feature types
 (e.g., gene, mRNA, and exon). For some feature types the number of missing and
 wrong features of that type is also shown. Thereby, ``missing'' means the number
-of features of that type from the ``reality'' without overlap to a feature of
+of features of that type from the ``reference'' without overlap to a feature of
 that type from the ``prediction''. Vice versa, ``wrong'' denotes the number of
 features of that type from the ``prediction'' without overlap to a feature of
-that type from the ``reality''.]])
+that type from the ``reference''.]])
