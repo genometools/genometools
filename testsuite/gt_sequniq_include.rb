0e8c4a5cdc5fe33a2ef393d830a4fc588d158711
Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
2011-06-09 10:46:44 +0200

    more tests for sequniq

--- testsuite/gt_sequniq_include.rb
+++ testsuite/gt_sequniq_include.rb
@@ -1,6 +1,43 @@
-Name "gt sequniq test "
+["", " -seqit", " -seqit -r"].each do |opt|
+  Name "gt sequniq#{opt} 2xfoo test"
+  Keywords "gt_sequniq"
+  Test do
+    run_test "#{$bin}gt sequniq#{opt} #{$testdata}foofoo.fas"
+    run "diff #{$last_stdout} #{$testdata}foo.fas"
+  end
+
+  Name "gt sequniq#{opt} 3xfoo test "
+  Keywords "gt_sequniq"
+  Test do
+    run_test "#{$bin}gt sequniq#{opt} #{$testdata}foofoofoo.fas"
+    run "diff #{$last_stdout} #{$testdata}foo.fas"
+  end
+end
+
+Name "gt sequniq -seqit foo + rc(foo) test "
 Keywords "gt_sequniq"
 Test do
-  run_test "#{$bin}gt sequniq #{$testdata}foofoo.fas"
+  run_test "#{$bin}gt sequniq -seqit #{$testdata}foorcfoo.fas"
+  run "diff #{$last_stdout} #{$testdata}foorcfoo.fas"
+end
+
+Name "gt sequniq -seqit -r foo + rc(foo) test "
+Keywords "gt_sequniq"
+Test do
+  run_test "#{$bin}gt sequniq -seqit -r #{$testdata}foorcfoo.fas"
+  run "diff #{$last_stdout} #{$testdata}foo.fas"
+end
+
+Name "gt sequniq -seqit 2xfoo + rc(foo) test "
+Keywords "gt_sequniq"
+Test do
+  run_test "#{$bin}gt sequniq -seqit #{$testdata}foorcfoofoo.fas"
+  run "diff #{$last_stdout} #{$testdata}foorcfoo.fas"
+end
+
+Name "gt sequniq -seqit -r 2xfoo + rc(foo) test "
+Keywords "gt_sequniq"
+Test do
+  run_test "#{$bin}gt sequniq -seqit -r #{$testdata}foorcfoofoo.fas"
   run "diff #{$last_stdout} #{$testdata}foo.fas"
 end
