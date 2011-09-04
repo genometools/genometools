f50f9faee74479240eb589c3efef475d619e3a19
Stefan Kurtz <kurtz@zbh.uni-hamburg.de>
2011-09-04 07:44:02 +0200

    Added qsortbench testcases.

---
+++ testsuite/gt_qsortbench_include.rb
@@ -0,0 +1,14 @@
+methods = ["thomas","system","inlinedptr","inlinedarr",
+           "direct","radix","radixrec"]
+
+lenlist=[1000,2000,4000,1000000,2000000]
+
+methods.each do |met|
+  lenlist.each do |len|
+    Name "gt qsortbench #{met}"
+    Keywords "gt_qsortbench"
+    Test do
+      run "#{$bin}gt dev qsortbench -impl #{met} -size #{len}"
+    end
+  end
+end
