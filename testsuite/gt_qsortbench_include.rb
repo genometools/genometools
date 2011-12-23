167fcdab21ba8014fe89c78d6229cb0cb125f736
Stefan Kurtz <kurtz@zbh.uni-hamburg.de>
2011-12-23 15:00:26 +0100

    Integrating the radixsort tests in qsortbench_include.rb.

--- testsuite/gt_qsortbench_include.rb
+++ testsuite/gt_qsortbench_include.rb
@@ -1,15 +1,18 @@
 methods = ["thomas","system","inlinedptr","inlinedarr",
            "direct","radixlinsmall","radixlinlarge","radixrec","radixdiv"]
 
-lenlist=[1000,2000,4000,1000000,2000000]
+lenlist=[10,20,30,1000,2000,4000,1000000,2000000]
+
+maxvallist = [922332036854775807,10000]
 
 methods.each do |met|
   lenlist.each do |len|
-
-    Name "gt qsortbench #{met}"
-    Keywords "gt_qsortbench"
-    Test do
-      run "#{$bin}gt dev qsortbench -impl #{met} -size #{len}"
+    maxvallist.each do |maxv|
+      Name "gt qsortbench #{met}"
+      Keywords "gt_qsortbench"
+      Test do
+        run "#{$bin}gt dev qsortbench -impl #{met} -size #{len} -maxval #{maxv}"
+      end
     end
   end
 end
