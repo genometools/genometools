8a721ffcac7adb2a89ad4f78c068b71a4d5574c0
Sascha Steinbiss <steinbiss@zbh.uni-hamburg.de>
2012-02-16 14:36:31 +0100

    fix plenking

--- gtdata/doc/gt.lua
+++ gtdata/doc/gt.lua
@@ -4,7 +4,7 @@ Set the environment variable GT_MEM_BOOKKEEPING=on to enable memory bookkeeping
 (e.g., like this: env GT_MEM_BOOKKEEPING=on gt).
 
 Set the environment variable GT_ENV_OPTIONS=-spacepeak to show a spacepeak after
-program run .
+program run.
 Set the environment variable GT_ENV_OPTIONS=-showtime to show processing times
 for some program parts if implemented.
 
