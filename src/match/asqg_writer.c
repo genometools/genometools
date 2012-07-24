98b46d61f40302bcbc0432fcd969784d568aa442
Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
2012-07-24 19:37:38 +0200

    implement asqg writer module

---
+++ src/match/asqg_writer.c
@@ -0,0 +1,120 @@
+/*
+  Copyright (c) 2012 Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
+  Copyright (c) 2012 Center for Bioinformatics, University of Hamburg
+
+  Permission to use, copy, modify, and distribute this software for any
+  purpose with or without fee is hereby granted, provided that the above
+  copyright notice and this permission notice appear in all copies.
+
+  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+
+#include "core/unused_api.h"
+#include "match/asqg_writer.h"
+
+struct GtAsqgWriter
+{
+  GtFile *file;
+  const GtEncseq *encseq;
+};
+
+GtAsqgWriter* gt_asqg_writer_new(GtFile *file, const GtEncseq *encseq)
+{
+  GtAsqgWriter *aw;
+  aw = gt_malloc(sizeof (*aw));
+  aw->file = file;
+  aw->encseq = encseq;
+  return aw;
+}
+
+int gt_asqg_writer_show_header(GtAsqgWriter *aw, float erate,
+    unsigned long minlen, const char *inputfilename, bool has_containments,
+    bool has_transitives, GT_UNUSED GtError *err)
+{
+  gt_assert(aw != NULL);
+  gt_file_xprintf(aw->file,
+      "HT\tVN:i:%lu\tER:f:%g\tOL:i:%lu\tIN:Z:%s\tCN:i:%c\tTE:i:%c\n",
+      GT_ASQG_VERSION, erate, minlen, inputfilename,
+      has_containments ? '1' : '0', has_transitives ? '1' : '0');
+  return 0;
+}
+
+static inline void gt_asqg_writer_show_vertex_line(GtFile *file,
+    unsigned long seqnum, const char *sequence, bool subsequence)
+{
+  gt_file_xprintf(file, "VT\t%lu\t%s\tSS:i:%c\n", seqnum, sequence,
+      subsequence ? '1' : '0');
+}
+
+int gt_asqg_writer_show_vertices(GtAsqgWriter *aw, GtError *err)
+{
+  GtEncseqReader *esr;
+  unsigned long seqnum = 0, pos = 0;
+  unsigned long i, tlen = gt_encseq_total_length(aw->encseq);
+  char cc, *seqbuffer;
+  gt_assert(aw != NULL);
+  esr = gt_encseq_create_reader_with_readmode(aw->encseq,
+      GT_READMODE_FORWARD, 0);
+  if (esr == NULL)
+  {
+    gt_error_set(err, "error creating encseq reader");
+    return -1;
+  }
+  seqbuffer = gt_malloc(sizeof (*seqbuffer) *
+      (gt_encseq_max_seq_length(aw->encseq) + 1UL));
+  for (i = 0; i < tlen; i++)
+  {
+    if ((cc = gt_encseq_reader_next_decoded_char(esr)) != (char) SEPARATOR)
+    {
+      seqbuffer[pos++] = cc;
+    }
+    else
+    {
+      seqbuffer[pos] = '\0';
+      gt_asqg_writer_show_vertex_line(aw->file, seqnum, seqbuffer, false);
+      seqnum++;
+      pos = 0;
+    }
+  }
+  gt_asqg_writer_show_vertex_line(aw->file, seqnum, seqbuffer, false);
+  gt_free(seqbuffer);
+  gt_encseq_reader_delete(esr);
+  return 0;
+}
+
+static inline void gt_asqg_writer_show_edge_line(GtFile *file,
+    unsigned long seqnum1, unsigned long seqnum2, unsigned long start1,
+    unsigned long end1, unsigned long seqlen1, unsigned long start2,
+    unsigned long end2, unsigned long seqlen2, bool revcompl,
+    unsigned long edist)
+{
+  gt_file_xprintf(file, "ED\t%lu %lu %lu %lu %lu %lu %lu %lu %c %lu\n",
+      seqnum1, seqnum2, start1, end1, seqlen1, start2, end2, seqlen2,
+      revcompl ? '1' : '0', edist);
+}
+
+void gt_spmproc_show_asgq(unsigned long suffix_readnum,
+    unsigned long prefix_readnum, unsigned long length,
+    bool suffixseq_direct, bool prefixseq_direct, void *asqg_writer)
+{
+  GtAsqgWriter *aw = asqg_writer;
+  const unsigned long sl1 = gt_encseq_seqlength(aw->encseq, suffix_readnum),
+                      sl2 = gt_encseq_seqlength(aw->encseq, prefix_readnum);
+  gt_asqg_writer_show_edge_line(aw->file, suffix_readnum, prefix_readnum,
+      suffixseq_direct ? sl1 - length : 0,
+      suffixseq_direct ? sl1 - 1UL : length - 1UL, sl1,
+      prefixseq_direct ? 0 : sl2 - length,
+      prefixseq_direct ? length - 1UL : sl2 - 1UL, sl2,
+      !suffixseq_direct || !prefixseq_direct, 0);
+}
+
+void gt_asqg_writer_delete(GtAsqgWriter *aw)
+{
+  gt_free(aw);
+}
---
+++ src/match/asqg_writer.h
@@ -0,0 +1,64 @@
+/*
+  Copyright (c) 2012 Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
+  Copyright (c) 2012 Center for Bioinformatics, University of Hamburg
+
+  Permission to use, copy, modify, and distribute this software for any
+  purpose with or without fee is hereby granted, provided that the above
+  copyright notice and this permission notice appear in all copies.
+
+  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+
+#ifndef ASQG_WRITER_H
+#define ASQG_WRITER_H
+
+#include "core/file.h"
+#include "core/encseq.h"
+#include "core/error.h"
+
+/* The <GtAsqgWriter> class allows to write string graph information in
+   the ASQG format of SGA (Simpsons and Durbin, 2012). */
+typedef struct GtAsqgWriter GtAsqgWriter;
+
+/* <GT_ASQG_VERSION> is the supported version of the asqg format. */
+#define GT_ASQG_VERSION 1UL
+
+/* Creates a new <GtAsqgWriter> object, using <file> for output
+   and <encseq> as source of information. */
+GtAsqgWriter* gt_asqg_writer_new(GtFile *file, const GtEncseq *encseq);
+
+/* Writes the header using the provided information: <minlen> is the
+   minimal match length, <inputfilename> is a string to use as input filename,
+   <erate> the error rate, <has_containments> shall be true, if containments
+   are present, <has_transitives> shall be true, if transitive edges are
+   present. Returns 0 on success, -1 on error and sets <err>. */
+int           gt_asqg_writer_show_header(GtAsqgWriter *aw,
+                                         float erate,
+                                         unsigned long minlen,
+                                         const char *inputfilename,
+                                         bool has_containments,
+                                         bool has_transitives,
+                                         GtError *err);
+
+/* Writes the vertices. Returns 0 on success, -1 on error and sets <err>. */
+int           gt_asqg_writer_show_vertices(GtAsqgWriter *aw,
+                                           GtError *err);
+
+/* Writes an edge using Readjoiner SPM information. */
+void          gt_spmproc_show_asgq(unsigned long suffix_readnum,
+                                   unsigned long prefix_readnum,
+                                   unsigned long length,
+                                   bool suffixseq_direct,
+                                   bool prefixseq_direct,
+                                   void *asqg_writer);
+
+/* Deletes a <GtAsqg> object. */
+void          gt_asqg_writer_delete(GtAsqgWriter *aw);
+
+#endif
