eec50a93cfb2a17165670bc912b864cea1034a78
Stefan Kurtz <kurtz@zbh.uni-hamburg.de>
2012-08-09 12:40:15 +0200

    Add a script which enumerates all correclty formatted ...
    fasta files in testdata and $GTTESTDATA.

---
+++ scripts/findfasta.rb
@@ -0,0 +1,56 @@
+#!/usr/bin/ruby
+
+def listdirectory(directory)
+  # prepare regexp for entries to ignore 
+  # saves time for repeated regexp use, since it stays the same
+  ignore_dirs = Regexp.compile(/^\.\.?$/)
+  stack = [directory]
+  loop do
+    if stack.empty? 
+      break
+    end
+    d = stack.pop
+    Dir.foreach(d) do |entry|
+      if not ignore_dirs.match(entry)
+        if File.stat("#{d}/#{entry}").file?
+          yield "#{d}/#{entry}"
+        else
+          stack.push("#{d}/#{entry}")
+        end
+      end
+    end
+  end
+end
+
+def listselected(dirname,excludelist)
+  suffixes = ["fastq","fasta","fna","fa","fsa.gz","fsa","FASTA.gz"]
+  listdirectory(dirname) do |filename|
+    suffixes.each do |suffix|
+      if filename.match(/\.#{suffix}$/) and
+         not excludelist.member?(File.basename(filename))
+        yield filename
+      end
+    end
+  end
+end
+
+testdata_exclude = ["solid_color_reads.fastq",
+                    "test2_wrong_begin.fastq",
+                    "test9_uneven_length.fastq",
+                    "test7_empty_seq.fastq",
+                    "test6_premature_end.fastq",
+                    "test4_different_seqlengths.fastq",
+                    "test3_different_seqnames.fastq",
+                    "corruptpatternfile.fna",
+                    "sw100K1.fsa",
+                    "sw100K2.fsa"]
+
+listselected("testdata",testdata_exclude) do |filename|
+  puts filename
+end
+
+gttestdata_exclude = ["trembl-section.fsa.gz"]
+
+listselected(ENV["GTTESTDATA"],gttestdata_exclude) do |filename|
+  puts filename
+end
