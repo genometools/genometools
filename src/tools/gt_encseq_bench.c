cd8bc231f3fe56aba4497f66acb55b60f9a3b436
Stefan Kurtz <kurtz@zbh.uni-hamburg.de>
2012-09-12 15:52:56 +0200

    Compute distribution of length of Sstar-substrings.

--- src/tools/gt_encseq_bench.c
+++ src/tools/gt_encseq_bench.c
@@ -292,6 +292,29 @@ unsigned long gt_sortedlengthinfo_seqstart(const GtEncseq *encseq,
   }
 }
 
+#define GT_SSTARLENGTH_MAX 100
+
+static void showSstarlendist(const unsigned long *lendist,
+                             unsigned long longerthanmax,
+                             unsigned long countSstar)
+{
+  unsigned long idx;
+
+  for (idx = 0; idx <= (unsigned long) GT_SSTARLENGTH_MAX; idx++)
+  {
+    if (lendist[idx] > 0)
+    {
+      printf("%lu %lu (%.2f)\n",idx,lendist[idx],
+                                (double) lendist[idx]/countSstar);
+    }
+  }
+  if (longerthanmax)
+  {
+    printf(">%d %lu (%.2f)\n",GT_SSTARLENGTH_MAX,longerthanmax,
+                              (double) longerthanmax/countSstar);
+  }
+}
+
 static void gt_count_sain_labels(const GtEncseq *encseq)
 {
   unsigned long position,
@@ -300,6 +323,8 @@ static void gt_count_sain_labels(const GtEncseq *encseq)
                 nextcc = GT_UNIQUEINT(SEPARATOR),
                 nextSstartypepos,
                 totalSstarlength = 0,
+                lendist[GT_SSTARLENGTH_MAX+1] = {0},
+                longerthanmax = 0,
                 totallength = gt_encseq_total_length(encseq);
   bool nextisStype = true;
   GtEncseqReader *esr;
@@ -323,9 +348,19 @@ static void gt_count_sain_labels(const GtEncseq *encseq)
     }
     if (!currentisStype && nextisStype)
     {
+      unsigned long currentlen;
+
       countSstar++;
       gt_assert(position < nextSstartypepos);
-      totalSstarlength += nextSstartypepos - position + 1;
+      currentlen = nextSstartypepos - position + 1;
+      totalSstarlength += currentlen;
+      if (currentlen <= (unsigned long) GT_SSTARLENGTH_MAX)
+      {
+        lendist[currentlen]++;
+      } else
+      {
+        longerthanmax++;
+      }
       nextSstartypepos = position;
     }
     nextisStype = currentisStype;
@@ -339,6 +374,7 @@ static void gt_count_sain_labels(const GtEncseq *encseq)
   printf("Sstar-type: %lu (%.2f)\n",countSstar,(double) countSstar/countS);
   printf("Sstar-type.length: %lu (%.2f)\n",totalSstarlength,
                    (double) totalSstarlength/countSstar);
+  showSstarlendist(lendist,longerthanmax,countSstar);
   gt_encseq_reader_delete(esr);
 }
 
