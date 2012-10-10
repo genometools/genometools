785a35d2dced3a892f2a7173b1801a4d5c0da29b
Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
2012-10-10 17:52:45 +0200

    avoid breaking scripts with readjoiner help msg

--- gtdata/doc/readjoiner.lua
+++ gtdata/doc/readjoiner.lua
@@ -1,13 +1,9 @@
 print ([[
 
+Please consult Readjoiner manual for more information.
+
 Tools:
-- prefilter:  prepare the readset for Readjoiner
-- overlap:    calculate suffix-prefix matches
-- assembly:   construct string graph and output contigs
 
-Please consult the Readjoiner manual for more information.
-
-Development/Test tools:
-- asqg:       convert string graph in SGA-compatible format
-- cnttest:    test tool for containments detection
-- spmtest:    test tool for suffix-prefix match calculation]])
+prefilter    prepare the readset for Readjoiner
+overlap      calculate suffix-prefix matches
+assembly     construct string graph and output contigs]])
