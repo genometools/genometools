6e8301d59b2c821dc9b85faacbb040b1cf72a004
Dirk Willrodt <willrodt@zbh.uni-hamburg.de>
2013-01-22 15:52:45 +0100

    make headercheck parallel capable

--- scripts/src_check_header.rb
+++ scripts/src_check_header.rb
@@ -9,19 +9,20 @@ ARGV.each do |file|
     puts "file #{file} does not exist!"
     had_err = true
   end
+  filename=File.basename(file)
   if !had_err
-    File.open("src/header_test.c", 'w') do |cfile|
+    File.open("src/header_test_#{filename}.c", 'w') do |cfile|
       cfile.printf("#include \"%s\"\nint main()\n{" +
                    "  return 0;\n}", file.sub(/.*src\//, ''))
     end
-    err = `make obj/src/header_test.o`
+    err = `make obj/src/header_test_#{filename}.o`
     if $? != 0
       puts "missing inclusions in #{file}!"
       puts err
       had_err = true
     end
-    FileUtils.rm_f(["obj/src/header_test.o",
-                    "src/header_test.c"])
+    FileUtils.rm_f(["obj/src/header_test_#{filename}.o",
+                    "src/header_test_#{filename}.c"])
   end
 end
 exit 0 if !had_err
