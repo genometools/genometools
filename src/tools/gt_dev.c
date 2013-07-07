d3b58aa7ec8df293e69a1d1eda1e3954607dbe5f
Gordon Gremme <gordon@gremme.org>
2013-07-07 15:34:02 +0200

    add gthbssmprint to dev tools

--- src/tools/gt_dev.c
+++ src/tools/gt_dev.c
@@ -22,6 +22,7 @@
 #include "core/versionfunc.h"
 #include "gth/gt_gthbssmbuild.h"
 #include "gth/gt_gthbssmfileinfo.h"
+#include "gth/gt_gthbssmprint.h"
 #include "gth/gt_gthmkbssmfiles.h"
 #include "tools/gt_compressedbits.h"
 #include "tools/gt_consensus_sa.h"
@@ -52,6 +53,7 @@ static void* gt_dev_arguments_new(void)
      gt_toolbox_add(dev_toolbox, "devtool", gt_devtool); */
   gt_toolbox_add(dev_toolbox, "gthbssmbuild", gt_gthbssmbuild);
   gt_toolbox_add(dev_toolbox, "gthbssmfileinfo", gt_gthbssmfileinfo);
+  gt_toolbox_add(dev_toolbox, "gthbssmprint", gt_gthbssmprint);
   gt_toolbox_add(dev_toolbox, "gthmkbssmfiles", gt_gthmkbssmfiles);
   gt_toolbox_add(dev_toolbox, "guessprot", gt_guessprot);
   gt_toolbox_add(dev_toolbox, "mergeesa", gt_mergeesa);
