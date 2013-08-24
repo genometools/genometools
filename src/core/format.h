afc9a4824bcc4719dadd41377cea882fc3509fbb
Gordon Gremme <gordon@gremme.org>
2013-08-24 21:44:20 +0100

    define the conversion string for '%llu'
    in platform independent fashion

---
+++ src/core/format.h
@@ -0,0 +1,27 @@
+/*
+  Copyright (c) 2013 Gordon Gremme <gordon@gremme.org>
+
+  Permission to use, copy, modify, and distribute this software for any
+  purpose with or without fee is hereby granted, provided that the above
+  copyright notice and this permission notice appear in all copies.
+
+  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+
+#ifndef FORMAT_H
+#define FORMAT_H
+
+/* Define the conversion string for '%llu' in platform independent fashion. */
+#ifndef _WIN32
+#define GT_LLU "%llu"
+#else
+#define GT_LLU "%I64u"
+#endif
+
+#endif
