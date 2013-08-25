b5afc776d2ea5f35359f93197164965f1615fbc4
Gordon Gremme <gordon@gremme.org>
2013-08-25 20:44:08 +0100

    improve build scripts

--- scripts/build_windows.sh
+++ scripts/build_windows.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -ex
 
-make CC=i686-w64-mingw32-gcc AR=i686-w64-mingw32-ar fpic=no cairo=no CFLAGS='-Wno-error=attributes -Wno-error=unused-parameter' $*
+make CC=i686-w64-mingw32-gcc AR=i686-w64-mingw32-ar fpic=no cairo=no curses=no CFLAGS='-Wno-error=attributes -Wno-error=unused-parameter -DSQLITE_OS_WIN' $*
