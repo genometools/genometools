b7a6b5901c62f0d27afc3f884cdf746b452f53d6
Sascha Steinbiss <steinbiss@zbh.uni-hamburg.de>
2013-08-26 09:52:39 +0200

    add documentation for GT_SEED

--- gtdata/doc/gt.lua
+++ gtdata/doc/gt.lua
@@ -8,5 +8,8 @@ after program run.
 Set the environment variable `GT_ENV_OPTIONS=-showtime` to show processing times
 for some program parts if implemented.
 
+Set the environment variable `GT_SEED` to an integer value to supply a seed for
+the random number generator. Can be overridden by the `-seed` option.
+
 Combinations are possible. Running the `gt` binary with `GT_ENV_OPTIONS=-help`
 shows all possible ``environment options'')]])
