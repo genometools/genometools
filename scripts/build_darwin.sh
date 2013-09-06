c129e49cb4fce23697517931b08b5e04604f0ffa
Gordon Gremme <gordon@gremme.org>
2013-09-07 00:05:07 +0200

    extend Darwin build script for 64-bit

--- scripts/build_darwin.sh
+++ scripts/build_darwin.sh
@@ -4,11 +4,25 @@
 make cleanup
 make manuals
 
-# Darwin
+# Darwin 32-bit
 # https://launchpad.net/~flosoft/+archive/cross-apple
 make clean
 make SYSTEM=Darwin                     \
      MACHINE=i386                      \
+     CC=i686-apple-darwin10-gcc        \
+     AR=i686-apple-darwin10-ar         \
+     RANLIB=i686-apple-darwin10-ranlib \
+     STRIP=i686-apple-darwin10-strip   \
+     cairo=no                          \
+     curses=no                         \
+     dist $*
+
+# Darwin 64-bit
+# https://launchpad.net/~flosoft/+archive/cross-apple
+make clean
+make SYSTEM=Darwin                     \
+     MACHINE=i386                      \
+     64bit=yes                         \
      CC=i686-apple-darwin10-gcc        \
      AR=i686-apple-darwin10-ar         \
      RANLIB=i686-apple-darwin10-ranlib \
