c380c57c57ba8637407ba7e51170ebf7e2e5f090
Gordon Gremme <gordon@gremme.org>
2013-09-10 15:32:59 +0200

    add MACHINE flag

--- scripts/build_windows.sh
+++ scripts/build_windows.sh
@@ -1,11 +1,12 @@
 #!/bin/sh -ex
 
-make CC=i686-w64-mingw32-gcc                                    \
+make SYSTEM=Windows                                             \
+     MACHINE=i686                                               \
+     CC=i686-w64-mingw32-gcc                                    \
      AR=i686-w64-mingw32-ar                                     \
      fpic=no                                                    \
      cairo=no                                                   \
      with-sqlite=no                                             \
      sharedlib=no                                               \
-     SYSTEM=Windows                                             \
      CFLAGS='-Wno-error=attributes -Wno-error=unused-parameter' \
      $*
--- scripts/build_windows_64.sh
+++ scripts/build_windows_64.sh
@@ -1,12 +1,13 @@
 #!/bin/sh -ex
 
-make CC=x86_64-w64-mingw32-gcc                                  \
+make SYSTEM=Windows                                             \
+     MACHINE=i686                                               \
+     CC=x86_64-w64-mingw32-gcc                                  \
      AR=x86_64-w64-mingw32-ar                                   \
      fpic=no                                                    \
      cairo=no                                                   \
      with-sqlite=no                                             \
      sharedlib=no                                               \
      64bit=yes                                                  \
-     SYSTEM=Windows                                             \
      CFLAGS='-Wno-error=attributes -Wno-error=unused-parameter' \
      $*
