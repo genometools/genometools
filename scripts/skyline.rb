ea916ad32fca55f626fbd79573c60755eda67ebf
Stefan Kurtz <kurtz@zbh.uni-hamburg.de>
2013-10-23 22:20:27 +0200

    Fixed skyline script.

--- scripts/skyline.rb
+++ scripts/skyline.rb
@@ -1,15 +1,30 @@
 #!/usr/bin/env ruby
 
-/*
-Skyline is an artificial string for which eSAIS has maximum recursion depth. To
-achieve this, the string’s suffixes must have type sequence LSLS . . . LS at
-each level of recursion. Such a string can be constructed for a length n = 2p,
-p ≥ 1, using the alphabet Σ = [ $, σ1, . . . , σp ] and the grammar {S → T1$,
-Ti → Ti+1σiTi+1 for i = 1,...,p−1 and Tp → σp}. For p = 4 and Σ = [$,a,b,c,d],
-we get dcdbdcdadcdbdcd$; for the test runs we replaced $ with σ0. The input
-Skyline is generated depending on the experiment size, all other inputs are cut
-to size.
-*/
+=begin
+\documentclass[12pt,a4paper]{scrartcl}
+\begin{document}
+Skyline is an artificial string for which eSAIS has maximum recursion depth.
+To achieve this, the string’s suffixes must have type sequence
+\(LSLS\ldots LS\) at each level of recursion. Such a string can be constructed
+for a length \(n = 2^{p}\), \(p\geq 1\), using the alphabet
+\(\Sigma=[\$,\sigma_{1},\ldots,\sigma_{p}]\) and the grammar
+\[\{S\toT_{1}\$\}\cup\{T_{i}\to T_{i+1}\sigma_{i}T_{i+1}\mid 1\leq i\leq p-1\}
+                 \cup\{T_{p}\to\sigma_{p}\}\]
+For \(p=4\) and \(\Sigma=\{\$,a,b,c,d\}\), we get $dcdbdcdadcdbdcd$. For
+the test runs we replaced \$ with \(\sigma_0\). The input skyline
+is generated depending on the experiment size, all other inputs are cut to size.
+\end{document}
+=end
+
+def generate(p,i=1)
+  chars = "$abcdefghijklmnopqrstuvwxyz"
+  if i == p
+    return "#{chars[i,1]}"
+  else
+    s = generate(p,i+1)
+    return "#{s}#{chars[i,1]}#{s}"
+  end
+end
 
 if ARGV.length != 1
   STDERR.puts "Usage: #{$0} <positive integer>"
@@ -17,15 +32,4 @@ if ARGV.length != 1
 end
 
 p = ARGV[0].to_i
-
-def generate(p,i=0)
-  chars = "abcdefg".to_a
-  if i == p
-    return "#{chars[i]}"
-  else
-    s = generate(p,i+1)
-    return "#{s}#{chars[i]}#{s}"
-  end
-end
-
-puts "#{generate(p)}"
+puts generate(p)
