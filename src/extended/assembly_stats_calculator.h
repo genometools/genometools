b1d80acb3814a3db367a3bb52cfb350395dc5d3a
Dirk Willrodt <willrodt@zbh.uni-hamburg.de>
2013-12-19 09:27:47 +0100

    cosmetics

--- src/extended/assembly_stats_calculator.h
+++ src/extended/assembly_stats_calculator.h
@@ -1,39 +1,41 @@
 /*
-  Copyright (c) 2011 Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
-  Copyright (c) 2011 Center for Bioinformatics, University of Hamburg
+   Copyright (c) 2011 Giorgio Gonnella <gonnella@zbh.uni-hamburg.de>
+   Copyright (c) 2011 Center for Bioinformatics, University of Hamburg
 
-  Permission to use, copy, modify, and distribute this software for any
-  purpose with or without fee is hereby granted, provided that the above
-  copyright notice and this permission notice appear in all copies.
+   Permission to use, copy, modify, and distribute this software for any
+   purpose with or without fee is hereby granted, provided that the above
+   copyright notice and this permission notice appear in all copies.
 
-  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-*/
+   THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+   WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+   MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+   ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+   WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+   ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+   OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+   */
 
 #ifndef ASSEMBLY_STATS_CALCULATOR_H
 #define ASSEMBLY_STATS_CALCULATOR_H
 
 #include "core/logger.h"
+#include "core/types_api.h"
 
 typedef struct GtAssemblyStatsCalculator GtAssemblyStatsCalculator;
 
 GtAssemblyStatsCalculator *gt_assembly_stats_calculator_new(void);
-void gt_assembly_stats_calculator_delete(GtAssemblyStatsCalculator *asc);
 
-void gt_assembly_stats_calculator_add(GtAssemblyStatsCalculator *asc,
-    GtUword length);
+void                       gt_assembly_stats_calculator_delete(GtAssemblyStatsCalculator *asc);
+
+void                       gt_assembly_stats_calculator_add(GtAssemblyStatsCalculator *asc,
+                                      GtUword length);
 
 /* if the following is set to a value > 0 (which is the default value)
  * then the NG50 and NG80 are also calculated */
 void gt_assembly_stats_calculator_set_genome_length(
-    GtAssemblyStatsCalculator *asc, GtUword genome_length);
+                                                    GtAssemblyStatsCalculator *asc, GtUword genome_length);
 
 void gt_assembly_stats_calculator_show(GtAssemblyStatsCalculator *asc,
-    GtLogger *logger);
+                                       GtLogger *logger);
 
 #endif
