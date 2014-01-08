57279c3e48ce031baf287ba4ad01776c94622e98
Daniel Standage <daniel.standage@gmail.com>
2014-01-08 16:20:50 -0500

    Minor change in hopes that the stalled Travis build will re-run

--- src/extended/feature_out_stream.c
+++ src/extended/feature_out_stream.c
@@ -1,5 +1,5 @@
 /*
-  Copyright (c) 2013 Daniel S. Standage <daniel.standage@gmail.com>
+  Copyright (c) 2013-2014 Daniel S. Standage <daniel.standage@gmail.com>
 
   Permission to use, copy, modify, and distribute this software for any
   purpose with or without fee is hereby granted, provided that the above
