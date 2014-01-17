cf223ed7cb090f682e4ce78a044642539e532677
Sascha Steinbiss <ss34@sanger.ac.uk>
2014-01-17 10:24:47 +0000

    fix mail address

--- gtruby/extended/check_boundaries_visitor.rb
+++ gtruby/extended/check_boundaries_visitor.rb
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2014 Sascha Steinbiss <ss23@sanger.ac.uk>
+# Copyright (c) 2014 Sascha Steinbiss <ss34@sanger.ac.uk>
 #
 # Permission to use, copy, modify, and distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
