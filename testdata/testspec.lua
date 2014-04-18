4f6cbef786dd6c30fafbe2cbe7c30feb3f6cc216
Sascha Steinbiss <sascha@steinbiss.name>
2014-04-18 14:26:37 +0100

    add stop codon check

--- testdata/testspec.lua
+++ testdata/testspec.lua
@@ -56,7 +56,12 @@ describe.feature("CDS", function(cds)
             or cds:appears_as_child_of_type("rRNA")) then
     it("does not contain internal stop codons", function()
       local seq = cds:extract_sequence("CDS", false, rm)
-      expect(gt.translate_dna(string.sub(seq, 1, -3))).should_not_match("*")
+      expect(gt.translate_dna(string.sub(seq, 1, -3))).should_not_match("\*")
+    end)
+
+    it("ends on a stop codon", function()
+      local seq = cds:extract_sequence("CDS", false, rm)
+      expect(gt.translate_dna(string.sub(seq, -3, -1))).should_match("\*")
     end)
   end
 end)
