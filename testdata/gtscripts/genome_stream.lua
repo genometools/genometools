20f74238d35fbd7e8e966af9d3976e929f82557c
Sascha Steinbiss <sascha@steinbiss.name>
2014-07-07 15:40:25 +0100

    add test cases for custom streams

--- testdata/gtscripts/genome_stream.lua
+++ testdata/gtscripts/genome_stream.lua
@@ -53,3 +53,64 @@ gs = gt.gff3_in_stream_new_sorted(testdata.."/unsorted_gff3_file.txt")
 rval, err = pcall(gs.next_tree, gs)
 assert(not rval)
 assert(string.find(err, "is not sorted"))
+
+
+-- test custom streams, missing override
+cs = gt.custom_stream_new_sorted()
+rval, err = pcall(cs.next_tree, cs)
+assert(not rval)
+assert(string.find(err, "method defined in custom stream"))
+
+-- test custom streams, wrong return type
+gs = gt.gff3_in_stream_new_sorted(testdata.."/eden.gff3")
+cs = gt.custom_stream_new_sorted()
+function cs:next_tree()
+  if gs:next_tree() then
+    return 1
+  end
+end
+rval, err = pcall(gs.next_tree, cs)
+assert(not rval)
+assert(string.find(err, "return a genome node"))
+
+-- test custom streams, runtime error
+gs = gt.gff3_in_stream_new_sorted(testdata.."/eden.gff3")
+cs = gt.custom_stream_new_sorted()
+function cs:next_tree()
+  n = gs:next_tree()
+  x = 1 + nil
+  return n
+end
+rval, err = pcall(gs.next_tree, cs)
+assert(not rval)
+assert(string.find(err, "perform arithmetic on a nil"))
+
+-- test custom streams
+gs = gt.gff3_in_stream_new_sorted(testdata.."/eden.gff3")
+cs = gt.custom_stream_new_sorted()
+cs.instream = gs
+function cs:next_tree()
+  return cs.instream:next_tree()
+end
+rval, err = pcall(gs.next_tree, cs)
+assert(rval)
+
+-- test custom streams
+cs = gt.custom_stream_new_sorted()
+cs.count = 1
+function cs:next_tree()
+  if cs.count <= 5 then
+    n = gt.feature_node_new("test", "gene", 1+cs.count, 100+cs.count, "+")
+    cs.count = cs.count + 1
+  else
+    n = nil
+  end
+  return n
+end
+nodes = {}
+local gn = cs:next_tree()
+while (gn) do
+  table.insert(nodes, gn)
+  gn = cs:next_tree()
+end
+assert(#nodes == 5)
