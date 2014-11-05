e0d38af8f62b6e9e0797b28842402ab78bf3dbcc
Sascha Steinbiss <ss34@sanger.ac.uk>
2014-11-05 15:10:15 +0000

    streamline/update example filter function

--- gtdata/doc/luafilter_function.lua
+++ gtdata/doc/luafilter_function.lua
@@ -1,16 +1,9 @@
   function filter(gn)
     target = "exon"
-    gfi = gt.feature_node_iterator_new(gn)
-
-    curnode = gfi:next()
-
-    while not(curnode == nil) do
-
+    for curnode in gn:children() do
       if (curnode:get_type() == target) then
         return false
       end
-      curnode = gfi:next()
     end
-
     return true
   end
