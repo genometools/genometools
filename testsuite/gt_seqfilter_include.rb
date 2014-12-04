1f0425806e1908e077edfa85f5e11c7fcba11af1
Dirk Willrodt <willrodt@zbh.uni-hamburg.de>
2014-12-04 16:54:42 +0100

    more testsuite parallelism issues fixed

--- testsuite/gt_seqfilter_include.rb
+++ testsuite/gt_seqfilter_include.rb
@@ -1,38 +1,44 @@
+require "fileutils"
 Name "gt seqfilter -minlength"
 Keywords "gt_seqfilter"
 Test do
-  run_test "#{$bin}gt seqfilter -minlength 1000 " +
-           "#{$testdata}nGASP/protein_100.fas"
+  FileUtils.copy("#{$testdata}nGASP/protein_100.fas", ".")
+  run_test "#{$bin}gt seqfilter -minlength 1000 " \
+           "nGASP/protein_100.fas"
   run "diff #{last_stdout} #{$testdata}nGASP/protein_long.fas"
 end
 
 Name "gt seqfilter -maxlength"
 Keywords "gt_seqfilter"
 Test do
-  run_test "#{$bin}gt seqfilter -maxlength 499 " +
-           "#{$testdata}nGASP/protein_100.fas"
+  FileUtils.copy("#{$testdata}nGASP/protein_100.fas", ".")
+  run_test "#{$bin}gt seqfilter -maxlength 499 " \
+           "nGASP/protein_100.fas"
   run "diff #{last_stdout} #{$testdata}nGASP/protein_short.fas"
 end
 
 Name "gt seqfilter -maxseqnum"
 Keywords "gt_seqfilter"
 Test do
+  FileUtils.copy("#{$testdata}nGASP/protein_100.fas", ".")
   run_test "#{$bin}gt seqfilter -maxseqnum 10 " +
-           "#{$testdata}nGASP/protein_100.fas"
+           "nGASP/protein_100.fas"
   run "diff #{last_stdout} #{$testdata}nGASP/protein_10.fas"
 end
 
 Name "gt seqfilter -step"
 Keywords "gt_seqfilter"
 Test do
+  FileUtils.copy("#{$testdata}nGASP/protein_100.fas", ".")
   run_test "#{$bin}gt seqfilter -step 10 " +
-           "#{$testdata}nGASP/protein_100.fas"
+           "$testdata}nGASP/protein_100.fas"
   run "diff #{last_stdout} #{$testdata}nGASP/protein_10th.fas"
 end
 
 Name "gt seqfilter -sample"
 Keywords "gt_seqfilter"
 Test do
+  FileUtils.copy("#{$testdata}nGASP/protein_100.fas", ".")
   run_test "#{$bin}gt seqfilter -sample 0.5 " +
-           "#{$testdata}nGASP/protein_100.fas"
+           "$testdata}nGASP/protein_100.fas"
 end
