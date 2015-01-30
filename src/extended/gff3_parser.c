genometools/genometools

commit 1b93258223442c4e6b9e41b8a899ad556cff289a

gff3 parser: prevent multi-feature orphans
These lead to memory corruption later on, we do not try to handle this very
esoteric case right now.

src/extended/gff3_parser.c
@@ -833,7 +833,12 @@ static int process_parent_attr(char *parent_attr, GtGenomeNode *feature_node,
     parent_gf = (GtGenomeNode*) gt_feature_info_get(parser->feature_info,
                                                     parent);
     if (!parent_gf) {
-      if (parser->strict) {
+      /* In strict mode or if the feature is a multi-feature orphan we fail
+         here. Multi-features cannot be added to the orphanage (who owns them)
+         without memory problems later on, because they are already owned by
+         their pseudo-parent. */
+      if (parser->strict ||
+          gt_feature_node_is_multi((GtFeatureNode*) feature_node)) {
         gt_error_set(err, "%s \"%s\" on line %u in file \"%s\" was not "
                      "previously defined (via \"%s=\")", GT_GFF_PARENT, parent,
                      line_number, filename, GT_GFF_ID);

testsuite/gt_gff3_include.rb
@@ -858,7 +858,7 @@ Keywords "gt_gff3 multi-feature"
 Test do
   run_test("#{$bin}gt gff3 #{$testdata}multi_feature_undefined_parent.gff3",
            :retval => 1)
-  grep last_stderr, "Parent .* on line 3 .* was not defined"
+  grep last_stderr, "Parent .* on line 4 .* was not previously defined"
 end
 
 Name "gt gff3 multi-feature (different sequence ID)"
@@ -1316,6 +1316,13 @@ Keywords "gt_gff3"
 Test do
   run_test "#{$bin}gt gff3 #{$testdata}orphaned_parent.gff3", :retval => 1
   grep last_stderr, "was not defined"
+end
+
+Name "gt gff3 multi-feature orphan"
+Keywords "gt_gff3"
+Test do
+  run_test "#{$bin}gt gff3 #{$testdata}multi_feature_orphan.gff3", :retval => 1
+  grep last_stderr, "was not previously defined"
 end
 
 Name "gt gff3 uppercase attributes"

testdata/multi_feature_orphan.gff3
@@ -1,0 +1,3 @@
+##gff-version   3
+seq	.	gene	1	100	.	+	.	ID=id1
+seq	.	mRNA	1	100	.	+	.	ID=id1;Parent=id2

commit 6644cea8a767576c86b7208aec18fed3340ac1af

gff3 parser: check multi-feature constrains first
before we process the parent attribute. This prevents problems with
multi-features with different parents and allows to process multi-features with
orphaned parents at the same time.

Fixes a regression introduced by 1b93258223442c4e6b9e41b8a899ad556cff289a

Resolves #542

src/extended/gff3_parser.c
@@ -833,12 +833,7 @@ static int process_parent_attr(char *parent_attr, GtGenomeNode *feature_node,
     parent_gf = (GtGenomeNode*) gt_feature_info_get(parser->feature_info,
                                                     parent);
     if (!parent_gf) {
-      /* In strict mode or if the feature is a multi-feature orphan we fail
-         here. Multi-features cannot be added to the orphanage (who owns them)
-         without memory problems later on, because they are already owned by
-         their pseudo-parent. */
-      if (parser->strict ||
-          gt_feature_node_is_multi((GtFeatureNode*) feature_node)) {
+      if (parser->strict) {
         gt_error_set(err, "%s \"%s\" on line %u in file \"%s\" was not "
                      "previously defined (via \"%s=\")", GT_GFF_PARENT, parent,
                      line_number, filename, GT_GFF_ID);
@@ -1414,16 +1409,16 @@ static int parse_attributes(char *attributes, GtGenomeNode *feature_node,
     }
   }
 
+  /* process ID attribute */
   if (!had_err && id_value) {
     had_err = store_id(id_value, (GtFeatureNode*) feature_node, is_child,
                        parser, genome_nodes, filename, line_number, err);
   }
-  if (!had_err && parent_value) {
-    had_err = process_parent_attr(parent_value, feature_node, id_value,
-                                  is_child, parser, genome_nodes, filename,
-                                  line_number, err);
-  }
 
+  /* we check multi-feature contrains before we process the Parent attribute,
+     because that prevents problems with multi-features with different parents
+     and allows to process multi-features with orphaned parents at the same
+     time. */
   if (!had_err && gt_feature_node_is_multi((GtFeatureNode*) feature_node)) {
     had_err =
       check_multi_feature_constrains(feature_node, (GtGenomeNode*)
@@ -1432,6 +1427,13 @@ static int parse_attributes(char *attributes, GtGenomeNode *feature_node,
                     gt_feature_node_get_attribute((GtFeatureNode*) feature_node,
                                                   GT_GFF_ID),
                                      parser, filename, line_number, err);
+  }
+
+  /* finally, process Parent attribute */
+  if (!had_err && parent_value) {
+    had_err = process_parent_attr(parent_value, feature_node, id_value,
+                                  is_child, parser, genome_nodes, filename,
+                                  line_number, err);
   }
 
   gt_splitter_delete(parent_splitter);

testdata/multi_feature_orphan_fail.gff3
@@ -1,3 +1,3 @@
 ##gff-version   3
 seq	.	gene	1	100	.	+	.	ID=id1
-seq	.	mRNA	1	100	.	+	.	ID=id1;Parent=id2
+seq	.	gene	1	100	.	+	.	ID=id1;Parent=id2

testdata/multi_feature_orphan_succ.gff3
@@ -1,0 +1,6 @@
+##gff-version 3
+NC_004354.3	RefSeq	tRNA	21175740	21175776	.	+	.	Parent=gene.trna.CR32525;ID=tRNA26;Note=tRNA:CR32525-RA%3B Dmel%5CtRNA:CR32525-RA%3B CR32525-RA%3B Dmel%5CCR32525-RA;Dbxref=FLYBASE:FBgn0052525,GeneID:3771861;gbkey=tRNA;product=tRNA-Cys
+NC_004354.3	RefSeq	exon	21175740	21175776	.	+	.	Parent=tRNA26;Note=tRNA:CR32525-RA%3B Dmel%5CtRNA:CR32525-RA%3B CR32525-RA%3B Dmel%5CCR32525-RA;Dbxref=FLYBASE:FBgn0052525,GeneID:3771861;gbkey=tRNA;product=tRNA-Cys
+NC_004354.3	RefSeq	exon	21175812	21175847	.	+	.	Parent=tRNA26;Note=tRNA:CR32525-RA%3B Dmel%5CtRNA:CR32525-RA%3B CR32525-RA%3B Dmel%5CCR32525-RA;Dbxref=FLYBASE:FBgn0052525,GeneID:3771861;gbkey=tRNA;product=tRNA-Cys
+NC_004354.3	RefSeq	tRNA	21175812	21175847	.	+	.	Parent=gene.trna.CR32525;ID=tRNA26;Note=tRNA:CR32525-RA%3B Dmel%5CtRNA:CR32525-RA%3B CR32525-RA%3B Dmel%5CCR32525-RA;Dbxref=FLYBASE:FBgn0052525,GeneID:3771861;gbkey=tRNA;product=tRNA-Cys
+NC_004354.3	RefSeq	gene	21175740	21175847	.	+	.	ID=gene.trna.CR32525;Name=tRNA:CR32525;Note=alternate names: transfer RNA:tyr1:19F;Dbxref=FLYBASE:FBgn0052525,GeneID:3771861;description=transfer RNA:CR32525;gbkey=Gene;gene=tRNA:CR32525;gene_synonym=AE002620.trna4-TyrGTA,CR32525,Dmel%5CCR32525,tRNA:tyr1:19F,tRNA:Y1:19F,tRNA:Y:GTA:AE002620-b,tRNA%5BTyr%5D%5B%5B1gamma%5D%5D;locus_tag=Dmel_CR32525

testsuite/gt_gff3_include.rb
@@ -858,7 +858,7 @@ Keywords "gt_gff3 multi-feature"
 Test do
   run_test("#{$bin}gt gff3 #{$testdata}multi_feature_undefined_parent.gff3",
            :retval => 1)
-  grep last_stderr, "Parent .* on line 4 .* was not previously defined"
+  grep last_stderr, "Parent .* on line 3 .* was not defined"
 end
 
 Name "gt gff3 multi-feature (different sequence ID)"
@@ -1318,12 +1318,18 @@ Test do
   grep last_stderr, "was not defined"
 end
 
-Name "gt gff3 multi-feature orphan"
+Name "gt gff3 multi-feature orphan (success)"
+Keywords "gt_gff3"
+Test do
+  run_test "#{$bin}gt gff3 #{$testdata}multi_feature_orphan_succ.gff3"
+end
+
+Name "gt gff3 multi-feature orphan (failure)"
 Keywords "gt_gff3"
 Test do
   run_test "#{$bin}gt gff3 #{$testdata}multi_feature_orphan_fail.gff3",
            :retval => 1
-  grep last_stderr, "was not previously defined"
+  grep last_stderr, "does not have a 'Parent' attribute which is present"
 end
 
 Name "gt gff3 uppercase attributes"