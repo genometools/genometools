2e745bf778064c30f4694dba7ff7c35d1f2f9273
Stefan Kurtz <kurtz@zbh.uni-hamburg.de>
2015-03-09 15:30:42 +0100

    Added script to prepend copyright notice to all files given as Arguments
    Requirements: set environment variables
                  MYEMAIL
                  MYNAME
                  optionally set environment variable
                  MYAFFILIATION

---
+++ scripts/add-copyright.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+if [ -z "$MYEMAIL" ]; then
+    echo "Need to set environment variable MYEMAIL"
+    exit 1
+fi
+
+if [ -z "$MYNAME" ]; then
+    echo "Need to set environment variable MYNAME"
+    exit 1
+fi
+
+YEAR=`date +"%Y"`
+if [ -z "$MYAFFILIATION" ]; then
+  AFF=""
+else
+  AFF="Copyright (c) ${YEAR} ${MYAFFILIATION}"
+fi
+
+TMPFILE=`mktemp TMP.XXXXXX` || exit 1
+for filename in $*
+do
+  cat > ${TMPFILE} <<EOF
+/*
+  Copyright (c) ${YEAR} ${MYEMAIL} <${MYEMAIL}>
+  ${AFF}
+
+  Permission to use, copy, modify, and distribute this software for any
+  purpose with or without fee is hereby granted, provided that the above
+  copyright notice and this permission notice appear in all copies.
+
+  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+EOF
+  cat ${filename} >> ${TMPFILE}
+  mv ${TMPFILE} ${filename}
+done
