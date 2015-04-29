aa416ccc4ac4b7bf4d47709d2b6a5854cde94089
Stefan Kurtz <kurtz@zbh.uni-hamburg.de>
2015-04-29 16:46:32 +0200

    Start with generalization to sorting seed pairs.

--- scripts/gen-radixsort-ip.sh
+++ scripts/gen-radixsort-ip.sh
@@ -2,3 +2,4 @@
 
 gen-radixsort-ip.rb > src/core/radixsort-ip-ulong.inc
 gen-radixsort-ip.rb --ulongpair > src/core/radixsort-ip-ulongpair.inc
+gen-radixsort-ip.rb --seedpair > src/core/radixsort-ip-seedpair.inc
