44f8a0a39c99bf86e07dd4a82de780a3ad157f5e
Annika <annika.seidel@studium.uni-hamburg.de>
2015-08-21 23:55:57 +0200

    diagonalband_align_checkfunction for distance

--- src/extended/diagonalband_align.h
+++ src/extended/diagonalband_align.h
@@ -1,14 +1,29 @@
-#include "core/types_api.h"
+/*
+  Copyright (c) 2015 Annika <annika.seidel@studium.uni-hamburg.de>
+  Copyright (c) 2015 Center for Bioinformatics, University of Hamburg
 
-GtUword diagonalband_affine_distance_only(const GtUchar *useq,
-                                         const GtUword ustart,
-                                         const GtUword ulen,
-                                         const GtUchar *vseq,
-                                         const GtUword vstart,
-                                         const  GtUword vlen,
-                                         const GtWord left_dist,
-                                         const GtWord right_dist,
-                                         const GtWord matchcost,
-                                         const GtWord mismatchcost,
-                                         const GtWord gap_opening,
-                                         const GtWord gap_extension);
+  Permission to use, copy, modify, and distribute this software for any
+  purpose with or without fee is hereby granted, provided that the above
+  copyright notice and this permission notice appear in all copies.
+
+  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+
+#ifndef DIAGONALBAND_ALIGN_H
+#define DIAGONALBAND_ALIGN_H
+#include "core/error.h"
+#include "core/types_api.h"
+#include "core/unused_api.h"
+
+void gt_checkdiagnonalbandalign(GT_UNUSED bool forward,
+                                       const GtUchar *useq,
+                                       GtUword ulen,
+                                       const GtUchar *vseq,
+                                       GtUword vlen);
+#endif
