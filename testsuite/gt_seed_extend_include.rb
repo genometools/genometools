588e3424e1250e05a3ae5f8c76736039e48a4adc
Jörg Winkler <joerg.winkler@studium.uni-hamburg.de>
2015-09-10 15:24:07 +0200

    testsuite for gt seed_extend

--- testsuite/gt_seed_extend_include.rb
+++ testsuite/gt_seed_extend_include.rb
@@ -1,23 +1,26 @@
-Name "gt seed_extend selfcompare"
-Keywords "gt_seed_extend seed extend encseq"
+Name "gt seed_extend small polysequence with mirror"
+Keywords "gt_seed_extend seedpair kmer polysequence xdrop extend"
 Test do
-  run_test "#{$bin}gt encseq encode -des no -sds no -md5 no #{$testdata}gt_bioseq_succ_3.fas"
-  run_test "#{$bin}gt seed_extend -k 10 gt_bioseq_succ_3.fas"
-  run "diff #{last_stdout} #{$testdata}seedextend1.out"
+  run_test "#{$bin}gt encseq encode -des no -sds no -md5 no " +
+           "-indexname #{$testdata}small_poly.fas #{$testdata}small_poly.fas"
+  run_test "#{$bin}gt seed_extend -seedlength 10 -mirror -debug-kmer " +
+           "-debug-seedpair #{$testdata}small_poly.fas"
+  run "cmp -s #{last_stdout} #{$testdata}seedextend1.out"
+  run_test "#{$bin}gt seed_extend -seedlength 10 -mirror -extendxdrop 97 -l 10 " +
+           "-mincoverage 11 #{$testdata}small_poly.fas"
+  run "cmp -s #{last_stdout} #{$testdata}seedextend3.out"
 end
 
-Name "gt seed_extend many reads k=14"
-Keywords "gt_seed_extend encseq"
+Name "gt seed_extend at1MB memlimit"
+Keywords "gt_seed_extend seedpair at1MB memlimit maxfreq verbose"
 Test do
-  run_test "#{$bin}gt encseq encode -des no -sds no -md5 no #{$testdata}condenser/varlen_longer_ids_200.fas"
-  run_test "#{$bin}gt seed_extend -k 14 varlen_longer_ids_200.fas"
-  run "diff #{last_stdout} #{$testdata}seedextend2.out"
-end
-
-Name "gt seed_extend mirror"
-Keywords "gt_seed_extend encseq mirror"
-Test do
-  run_test "#{$bin}gt seed_extend -k 8 -mirror #{$testdata}foo.64"
-  run "diff #{last_stdout} #{$testdata}seedextend3.out"
+  run_test "#{$bin}gt encseq encode -des no -sds no -md5 no " +
+           "-indexname #{$testdata}at1MB #{$testdata}at1MB"
+  run_test "#{$bin}gt seed_extend -verify -debug-seedpair -memlimit 10MB " +
+           "#{$testdata}at1MB"
+  run "cmp -s #{last_stdout} #{$testdata}seedextend2.out"
+  run_test "#{$bin}bin/gt seed_extend -v -maxfreq 5 #{$testdata}at1MB"
+  run "grep -v '^# Found and sorted 582230 k-mers' #{last_stdout}"
+  run "grep -v '^# Collected and sorted 68577 seed pairs' #{last_stdout}"
 end
 
