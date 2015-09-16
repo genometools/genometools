c6449bffec50b19423a6b934ffac1c5c15df5062
Stefan Kurtz <kurtz@zbh.uni-hamburg.de>
2015-09-16 10:55:07 +0200

    Added missing parantheses.

--- src/match/ft-front-prune.c
+++ src/match/ft-front-prune.c
@@ -200,8 +200,8 @@ static GtUchar sequenceobject_get_char(Sequenceobject *seq,GtUword pos)
                                       GT_READMODE_FORWARD);
   }
   gt_assert(seq->bytesequenceptr != NULL &&
-            (seq->forward && seq->startpos + pos < seq->totallength ||
-            !seq->forward && seq->startpos - pos < seq->totallength));
+            ((seq->forward && seq->startpos + pos < seq->totallength) ||
+             (!seq->forward && seq->startpos - pos < seq->totallength)));
   return seq->bytesequenceptr[seq->forward ? seq->startpos + pos
                                            : seq->startpos - pos];
 }
