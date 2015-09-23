4177d7c30b2e57ad9dde2e0e77d22c588b58b139
Stefan Kurtz <kurtz@zbh.uni-hamburg.de>
2015-09-23 12:41:33 +0200

    Addd default keywords for calltestcase.sh

--- scripts/calltestcase.sh
+++ scripts/calltestcase.sh
@@ -1,17 +1,35 @@
 #!/bin/sh 
 
-if test $# -eq 0
-then
-  echo "Usage: $0 <keyword1> [keyword2 ...]"
-  exit 1
-fi
-
 cd testsuite
 
-for keyword in $*
+TMPFILE=`mktemp TMP.XXXXXX` || exit 1
+if test $# -eq 0
+then
+  echo ${GTTESTKEYWORDS} | tr ' ' '\n' > ${TMPFILE}
+else
+  touch ${TMPFILE}
+  for keyword in $*
+  do
+    echo $keyword > ${TMPFILE}
+  done
+fi
+
+for keyword in `cat ${TMPFILE}`
+do
+  grep -q $keyword *.rb
+  if test $? -ne 0
+  then
+    echo "$0: keyword ${keyword} not in testsuite/*.rb"
+    exit 1
+  fi
+done
+
+for keyword in `cat ${TMPFILE}`
 do
   echo "run testsuite for keyword ${keyword}"
   env -i ./testsuite.rb -gttestdata ${GTTESTDATA} -keywords $keyword
 done
+
+rm -f ${TMPFILE}
 
 cd ..
