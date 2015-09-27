266846f6bf236f39b07743dc9d0c2e242f277c46
Jörg Winkler <joerg.winkler@studium.uni-hamburg.de>
2015-09-27 21:16:32 +0200

    remove indexfiles from testdata: fingerprint test

--- testsuite/gt_fingerprint_include.rb
+++ testsuite/gt_fingerprint_include.rb
@@ -1,7 +1,10 @@
+require 'fileutils'
+
 Name "fingerprint"
 Keywords "gt_fingerprint"
 Test do
-  run_test "#{$bin}gt fingerprint #{$testdata}U89959_ests.fas | sort | uniq"
+  FileUtils.copy "#{$testdata}U89959_ests.fas", "."
+  run_test "#{$bin}gt fingerprint U89959_ests.fas | sort | uniq"
   run "diff #{last_stdout} #{$testdata}U89959_ests.checklist_uniq"
 end
 
@@ -14,20 +17,23 @@ end
 Name "fingerprint (case insensitive)"
 Keywords "gt_fingerprint"
 Test do
-  run_test("#{$bin}gt fingerprint #{$testdata}U89959_ests_gi_8690080_soft_masked.fas")
+  FileUtils.copy "#{$testdata}U89959_ests_gi_8690080_soft_masked.fas", "."
+  run_test("#{$bin}gt fingerprint U89959_ests_gi_8690080_soft_masked.fas")
   run "diff #{last_stdout} #{$testdata}U89959_ests_gi_8690080_unmasked.checklist"
 end
 
 Name "fingerprint -check (success)"
 Keywords "gt_fingerprint"
 Test do
+  FileUtils.copy "#{$testdata}U89959_ests.fas", "."
   run_test "#{$bin}gt fingerprint -check #{$testdata}U89959_ests.checklist " +
-           "#{$testdata}U89959_ests.fas"
+           "U89959_ests.fas"
 end
 
 Name "fingerprint -check (stdin)"
 Keywords "gt_fingerprint"
 Test do
+  FileUtils.copy "#{$testdata}U89959_ests.fas", "."
   run "cat #{$testdata}U89959_ests.checklist | #{$memcheck} #{$bin}gt " +
       "fingerprint -check - #{$testdata}U89959_ests.fas"
 end
@@ -35,16 +41,18 @@ end
 Name "fingerprint -check (failure)"
 Keywords "gt_fingerprint"
 Test do
+  FileUtils.copy "#{$testdata}U89959_ests.fas", "."
   run_test("#{$bin}gt fingerprint -check " +
            "#{$testdata}U89959_ests.checklist_uniq " +
-           "#{$testdata}U89959_ests.fas", :retval => 1)
+           "U89959_ests.fas", :retval => 1)
   grep last_stderr, /fingerprint comparison failed/
 end
 
 Name "fingerprint -check (failure)"
 Keywords "gt_fingerprint"
 Test do
-  run_test("#{$bin}gt sequniq #{$testdata}U89959_ests.fas | #{$memcheck}" +
+  FileUtils.copy "#{$testdata}U89959_ests.fas", "."
+  run_test("#{$bin}gt sequniq U89959_ests.fas | #{$memcheck}" +
            "#{$bin}gt fingerprint -check #{$testdata}U89959_ests.checklist -",
            :retval => 1)
   grep last_stderr, /fingerprint comparison failed/
@@ -53,13 +61,15 @@ end
 Name "fingerprint -collisions"
 Keywords "gt_fingerprint"
 Test do
-  run_test "#{$bin}gt fingerprint -collisions #{$testdata}U89959_ests.fas"
+  FileUtils.copy "#{$testdata}U89959_ests.fas", "."
+  run_test "#{$bin}gt fingerprint -collisions U89959_ests.fas"
 end
 
 Name "fingerprint -duplicates (found)"
 Keywords "gt_fingerprint"
 Test do
-  run_test("#{$bin}gt fingerprint -duplicates #{$testdata}U89959_ests.fas",
+  FileUtils.copy "#{$testdata}U89959_ests.fas", "."
+  run_test("#{$bin}gt fingerprint -duplicates U89959_ests.fas",
            :retval => 1)
   grep last_stderr, /duplicates found/
 end
@@ -67,21 +77,24 @@ end
 Name "fingerprint -duplicates (none found)"
 Keywords "gt_fingerprint"
 Test do
-  run_test "#{$bin}gt fingerprint -duplicates #{$testdata}U89959_genomic.fas"
+  FileUtils.copy "#{$testdata}U89959_genomic.fas", "."
+  run_test "#{$bin}gt fingerprint -duplicates U89959_genomic.fas"
 end
 
 Name "fingerprint -extract (found)"
 Keywords "gt_fingerprint"
 Test do
+  FileUtils.copy "#{$testdata}U89959_ests.fas", "."
   run_test "#{$bin}gt fingerprint -extract 6d3b4b9db4531cda588528f2c69c0a57 " +
-           "#{$testdata}U89959_ests.fas"
+           "U89959_ests.fas"
   run "diff #{last_stdout} #{$testdata}gt_fingerprint_extract.out"
 end
 
 Name "fingerprint -extract (not found)"
 Keywords "gt_fingerprint"
 Test do
+  FileUtils.copy "#{$testdata}U89959_ests.fas", "."
   run_test "#{$bin}gt fingerprint -extract 7d3b4b9db4531cda588528f2c69c0a57 " +
-           "#{$testdata}U89959_ests.fas", :retval => 1
+           "U89959_ests.fas", :retval => 1
   grep last_stderr, /could not find sequence with fingerprint/
 end
