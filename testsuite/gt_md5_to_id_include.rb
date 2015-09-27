b19c14c5551650c6dc53ae69c0dd52c2af56549f
Jörg Winkler <joerg.winkler@studium.uni-hamburg.de>
2015-09-27 21:37:57 +0200

    remove indexfiles from testdata: md5_to_id test

--- testsuite/gt_md5_to_id_include.rb
+++ testsuite/gt_md5_to_id_include.rb
@@ -1,8 +1,12 @@
+require 'fileutils'
+
 Name "gt md5_to_id (U89959_sas.gff3, old)"
 Keywords "gt_md5_to_id"
 Test do
+  FileUtils.copy "#{$testdata}U89959_ests.fas", "."
+  FileUtils.copy "#{$testdata}U89959_genomic.fas", "."
   run_test "#{$bin}gt md5_to_id -seqfiles " +
-           "#{$testdata}U89959_genomic.fas #{$testdata}U89959_ests.fas -- " +
+           "U89959_genomic.fas U89959_ests.fas -- " +
            "#{$testdata}U89959_sas.gff3md5old"
   run "diff #{last_stdout} #{$testdata}U89959_sas.gff3"
 end
@@ -17,8 +21,10 @@ end
 Name "gt md5_to_id (U89959_csas.gff3, old)"
 Keywords "gt_md5_to_id"
 Test do
+  FileUtils.copy "#{$testdata}U89959_ests.fas", "."
+  FileUtils.copy "#{$testdata}U89959_genomic.fas", "."
   run_test "#{$bin}gt md5_to_id -seqfiles " +
-           "#{$testdata}U89959_genomic.fas #{$testdata}U89959_ests.fas -- " +
+           "U89959_genomic.fas U89959_ests.fas -- " +
            "#{$testdata}U89959_csas.gff3md5old"
   run "diff #{last_stdout} #{$testdata}U89959_csas.gff3"
 end
