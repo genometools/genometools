3b14330b2c59ee6cf95a079934cc2c15b973e9d6
Stefan Kurtz <kurtz@zbh.uni-hamburg.de>
2015-10-17 20:55:28 +0200

    reorganize and polish script

--- scripts/sensitivity-test.rb
+++ scripts/sensitivity-test.rb
@@ -15,7 +15,19 @@ def makesystemcall(argstring)
   end
 end
 
-gtcall = "env -i bin/gt"
+def matchinfile(filename)
+  if open(filename).grep(/^\d+ \d+ \d+ . \d+ \d+ \d+ \d+ \d+ \d+/).length > 0
+    return true
+  else
+    return false
+  end
+end
+
+def showresult(prog,minidentity,found,fails)
+  print "#{prog}(minid=#{minidentity}):\tfound #{found} of #{found+fails}, "
+  printf("miss #{fails}, sensitivity %.2f%%\n",
+         100.0 * found.to_f/(found+fails).to_f)
+end
 
 num_tests = ARGV[0].to_i
 minidentity_first = ARGV[1].to_i
@@ -38,18 +50,19 @@ if progspec == "se" or progspec == "dase"
   runse = true
 end
 if not runse and not runda
-  STDERR.puts "third argument must be one of the strings " + 
+  STDERR.puts "third argument must be one of the strings " +
                keystrings.join(", or")
   exit 1
 end
 
 if num_tests < 20 then
-  lengths = [*90..110].sample(num_tests)
+  lengthtab = [*90..110].sample(num_tests)
 else
-  lengths = num_tests.times.map{ 90 + Random.rand(21) }
+  lengthtab = num_tests.times.map{ 90 + Random.rand(21) }
 end
+gtcall = "env -i bin/gt"
 minidentity_first.upto(minidentity_last).each do |minidentity|
-  tempdir = "dalign-cmp-#{minidentity}"
+  tempdir = "dalign-cmp/minid-#{minidentity}"
   makesystemcall("mkdir -p #{tempdir}")
   gtfails = 0
   gtfound = 0
@@ -57,7 +70,7 @@ minidentity_first.upto(minidentity_last).each do |minidentity|
   dafound = 0
   gtcrash = 0
 
-  for length in lengths do
+  for length in lengthtab do
     # generate random sequences
     makesystemcall("ruby scripts/gen-randseq.rb --minidentity #{minidentity} " +
 		   "--seedlength #{seedlength} --length #{length+10} -c 35 " +
@@ -65,52 +78,55 @@ minidentity_first.upto(minidentity_last).each do |minidentity|
     # convert sequences to PacBio format
     # create db file
     if runda
-      makesystemcall("ruby convert2myersformat.rb #{tempdir}/rand-#{length}.fas " +
+      makesystemcall("ruby convert2myersformat.rb " +
+                     "#{tempdir}/rand-#{length}.fas " +
 		     "> #{tempdir}/rand-#{length}.fasta")
-      makesystemcall("rm #{tempdir}/rand-#{length}.fas")
+      File.delete("#{tempdir}/rand-#{length}.fas")
       makesystemcall("../DAZZ_DB/fasta2DB #{tempdir}/rand-#{length}.db " +
 		     "#{tempdir}/rand-#{length}.fasta")
     end
     # create GtEncseq
     if runse
-      makesystemcall("#{gtcall} encseq encode -des no -sds no -md5 no -indexname " +
+      makesystemcall("#{gtcall} encseq encode -des no -sds no -md5 no " +
+                     "-indexname " +
 		     "#{tempdir}/rand-#{length} #{tempdir}/rand-#{length}.fas")
-    end
-    # run gt seed_extend
-    if runse
       makesystemcall("#{gtcall} seed_extend -t 21 -l #{length} " +
 		    "-seedlength #{seedlength} -minidentity #{minidentity} " +
 		    "-seed-display -bias-parameters -extendgreedy " +
 		    "-overlappingseeds " +
 		    "-ii #{tempdir}/rand-#{length} > " +
 		    "#{tempdir}/gtout#{length}.txt")
-      if open("#{tempdir}/gtout#{length}.txt").grep(/^\d+ \d+ \d+ . \d+ \d+ \d+ \d+ \d+ \d+/).length > 0
+      if matchinfile("#{tempdir}/gtout#{length}.txt")
 	gtfound = gtfound + 1
       else
 	gtfails = gtfails + 1
       end
-      system("rm -f #{tempdir}/rand-#{length}.esq #{tempdir}/rand-#{length}.ssp")
+      ["esq","ssp"].each do |suffix|
+        if File.exists?("#{tempdir}/rand-#{length}.#{suffix}")
+          File.delete("#{tempdir}/rand-#{length}.#{suffix}")
+        end
+      end
     end
     # run daligner
     if runda
       makesystemcall("../DALIGNER/daligner -t21 -I -A -Y -e0.#{minidentity} " +
-		     "-l#{length} -k#{seedlength} #{tempdir}/rand-#{length}.db " +
-		     "#{tempdir}/rand-#{length}.db > #{tempdir}/daout#{length}.txt")
-      if open("#{tempdir}/daout#{length}.txt").grep(/^\d+ \d+ \d+ . \d+ \d+ \d+ \d+ \d+ \d+/).length > 0 then
+		     "-l#{length} -k#{seedlength} " +
+                     "#{tempdir}/rand-#{length}.db " +
+		     "#{tempdir}/rand-#{length}.db > " +
+                     "#{tempdir}/daout#{length}.txt")
+      if matchinfile("#{tempdir}/daout#{length}.txt")
 	dafound = dafound + 1
       else
 	dafails = dafails + 1
       end
-      system("rm -f #{tempdir}/rand-#{length}.db *.las")
+      File.delete("#{tempdir}/rand-#{length}.db")
+      File.delete("#{tempdir}/rand-#{length}.las")
     end
   end
-
   if runse
-    puts "gt seed_extend(minid=#{minidentity}):\tfound #{gtfound} of #{gtfound+gtfails}, " +
-	 "miss #{gtfails}, sensitivity #{100.0*gtfound/(gtfound+gtfails)}%"
+    showresult("gt_seedextend",minidentity,gtfound,gtfails)
   end
   if runda
-    puts "daligner(minid=#{minidentity}:\tfound #{dafound} of #{dafound+dafails}, " +
-	 "miss #{dafails}, sensitivity #{100.0*dafound/(dafound+dafails)}%"
+    showresult("daligner",minidentity,dafound,dafails)
   end
 end
