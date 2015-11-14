ed51e347d229974da9b7b51d25cfbdc5f294918f
Stefan Kurtz <kurtz@zbh.uni-hamburg.de>
2015-11-14 21:30:16 +0100

    Remove restriction of file size when applying gt seed extend.

--- scripts/esq-in-bytes.sh
+++ scripts/esq-in-bytes.sh
@@ -6,10 +6,7 @@ set -e -x
 for filename in `${GTDIR}/scripts/findfasta.rb`
 do
   len=`cat ${filename} | wc -c`
-  if test ${len} -ge 20000
-  then
-    ${GTDIR}/bin/gt encseq encode -indexname sfx $filename
-    # Now do something with the sequence
-    ${GTDIR}/bin/gt seed_extend -extendgreedy -ii sfx -a -maxfreq 20
-  fi
+  ${GTDIR}/bin/gt encseq encode -indexname sfx $filename
+  # Now do something with the sequence
+  ${GTDIR}/bin/gt seed_extend -extendgreedy -ii sfx -a -maxfreq 20 -seed-display
 done
