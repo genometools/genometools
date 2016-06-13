c9f1f5aeb384e203fa69285a223927bb32898b56
Annika Seidel <annika.seidel@studium.uni-hamburg.de>
2016-06-13 11:56:14 +0200

    create module evalue

---
+++ src/match/evalue.h
@@ -0,0 +1,29 @@
+/*
+  Copyright (c) 2016 Annika Seidel <annika.seidel@studium.uni-hamburg.de>
+  Copyright (c) 2016 Center for Bioinformatics, University of Hamburg
+
+  Permission to use, copy, modify, and distribute this software for any
+  purpose with or without fee is hereby granted, provided that the above
+  copyright notice and this permission notice appear in all copies.
+
+  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*/
+
+#ifndef EVALUE_H
+#define EVALUE_H
+
+#include "core/types_api.h"
+#include "match/karlin_altschul_stat.h"
+#include "querymatch.h"
+
+double gt_evalue_calculate(const GtKarlinAltschulStat *ka,
+                           GtUword searchspace,
+                           double bit_score);
+
+#endif
