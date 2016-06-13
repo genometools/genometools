55697078c96be4d8697e3edd0dadb95df60a8a37
Annika Seidel <annika.seidel@studium.uni-hamburg.de>
2016-06-13 12:10:25 +0200

    add calculation of effective searchspace

--- src/match/evalue.c
+++ src/match/evalue.c
@@ -49,6 +49,109 @@ double gt_evalue_calculate_bit_score(double raw_score,
   return bit_score;
 }
 
+static GtUword gt_evalue_calculate_length_adjustment(GtUword query_length,
+                                                     GtUword db_length,
+                                                     GtUword num_of_db_seqs,
+                                                     double alpha_div_lambda,
+                                                     double beta,
+                                                     double K)
+{
+  unsigned int i;
+  const int kMaxIterations = 20;
+  double l_min, l_max, l_next, l, l_bar;
+  GtUword length_adjustment;
+  bool converged = false;
+
+  /* l_max is the largest nonnegative solution of
+     K * (m - l) * (n - N * l) > MAX(m,n) */
+
+  double c = db_length * query_length - MAX(query_length, db_length)/K;
+  if (c < 0)
+    return 0; /* length_adjustnment = 0 */
+
+  double a = num_of_db_seqs;
+  double mb = query_length * num_of_db_seqs + db_length;
+  l_max = 2 * c / (mb + sqrt(mb * mb - 4 * a * c));  /* quadratic formula */
+
+  l_next = 0;
+  for (i = 0; i < kMaxIterations; i++)
+  {
+    l = l_next;
+    l_bar = beta + alpha_div_lambda *
+            (log(K) + log((query_length-l)*(db_length-num_of_db_seqs*l)));
+    if (l_bar >= l)
+    {
+      l_min = l;
+      if (l_bar -l_min <= 1.0)
+      {
+        converged = true;
+        break;
+      }
+      if (l_min == l_max)
+        break;
+    }
+    else
+    {
+      l_max = l;
+    }
+    if (l_min <= l_bar && l_bar <= l_max)
+      l_next = l_bar;
+    else if (i == 0)
+      l_next = l_max;
+    else
+      l_next = (l_min+l_max)/2;
+  }
+
+  length_adjustment = (GtUword) l_min; /* floor(fixed point ) */
+  if (converged)
+  {
+    l = ceil(l_min);
+    if (l <= l_max)
+    {
+      if (alpha_div_lambda * (log(K) + log((query_length-l) *
+                             (db_length-num_of_db_seqs*l))) + beta >= l)
+        length_adjustment = (GtUword) l;
+    }
+  }
+
+  return length_adjustment;
+}
+
+GtUword gt_evalue_calculate_searchspace(const GtQuerymatch *querymatch,
+                                        const GtEncseq *encseq,
+                                        double alpha_div_lambda,
+                                        double beta,
+                                        double K)
+{
+  GtUword total_length_of_db,
+          num_of_db_seqs,
+          actual_db_length,
+          actual_query_length,
+          effective_db_length,
+          effective_query_length,
+          length_adjustment;
+
+  total_length_of_db = gt_encseq_total_length(encseq);
+  num_of_db_seqs = gt_encseq_num_of_sequences(encseq);
+  /* db length without seperators */
+  actual_db_length = total_length_of_db - num_of_db_seqs;
+
+  actual_query_length = gt_querymatch_querylen(querymatch);
+
+  length_adjustment = gt_evalue_calculate_length_adjustment(actual_query_length,
+                                                            actual_db_length,
+                                                            num_of_db_seqs,
+                                                            alpha_div_lambda,
+                                                            beta,
+                                                            K);
+
+  effective_query_length = actual_query_length - length_adjustment;
+  effective_db_length = actual_db_length -
+                                      (num_of_db_seqs * length_adjustment);
+
+  return effective_query_length * effective_db_length;
+}
+
 double gt_evalue_calculate(const GtKarlinAltschulStat *ka,
                            GtUword searchspace,
                            double bit_score)
