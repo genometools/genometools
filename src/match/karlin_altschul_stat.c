ddbbdff9d2d76217029b3974c9bebc113afb749a
Annika Seidel <annika.seidel@studium.uni-hamburg.de>
2016-06-13 11:22:04 +0200

    calculate probabilities of scores

--- src/match/karlin_altschul_stat.c
+++ src/match/karlin_altschul_stat.c
@@ -32,6 +32,27 @@ struct GtKarlinAltschulStat
          H;
 };
 
+typedef struct{
+  double *sprob;
+  GtWord low_score,
+         high_score,
+         min_score,
+         max_score;
+} ScoringFrequency;
+
+typedef struct{
+  char   ch;
+  double p;
+} LetterProb;
+
+/* provisional solution */
+static LetterProb nt_prob[] = {
+  { 'A', 25.00 },
+  { 'C', 25.00 },
+  { 'G', 25.00 },
+  { 'T', 25.00 }
+};
+
 GtKarlinAltschulStat *gt_ka_new(void)
 {
   GtKarlinAltschulStat *ka;
@@ -58,4 +79,38 @@ double gt_ka_get_logK(const GtKarlinAltschulStat *ka)
 {
   gt_assert(ka);
   return ka->logK;
+}
+
+/* calculate probabilities of scores */
+ScoringFrequency *gt_karlin_altschul_stat_scoring_freuqnecy(
+                                            const GtAlphabet *alphabet,
+                                            const GtScoreHandler *scorehandler)
+{
+  unsigned int i, j, numofchars;
+  GtWord score, range;
+
+  ScoringFrequency *sf = gt_malloc(sizeof(*sf));
+  gt_assert(sf);
+
+  /* TODO: make generalizations of score/cost functionn,
+   * min/max in scorehandler? */
+  sf->low_score = -2;
+  sf->high_score = 2;
+  range = 2 - (-2) + 1; /*TODO: range = score_max-score_min+1*/
+  sf->sprob = gt_malloc(range * sizeof (*sf->sprob));
+
+  numofchars = gt_alphabet_num_of_chars(alphabet);
+
+  for (i = 0; i < numofchars; i++)
+  {
+    for (j = 0; j < numofchars; j++)
+      score = gt_scorehandler_get_replacement(scorehandler, i, j);
+
+      if (score >= sf->low_score) /* TODO: error check */
+        sf->sprob[score-sf->low_score] += nt_prob[i].p * nt_prob[j].p;
+        /* TODO: make generalizations of alphabet probabilities,
+           for now nt_prob */
+  }
+
+  return sf;
 }
