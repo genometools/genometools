be6823db2c2ec190b7e42b83f853bce7ac53e81d
Sascha Steinbiss <sascha@steinbiss.name>
2016-09-29 11:56:32 +0100

    travis: do not run brew as root

--- scripts/travis_installdeps.sh
+++ scripts/travis_installdeps.sh
@@ -2,10 +2,10 @@
 
 echo $TRAVIS_OS_NAME
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-  apt-get update -q
-  apt-get install ncbi-blast+ gcc-multilib -y
-  apt-get install binutils-mingw-w64-i686 gcc-mingw-w64-i686 -y
-  apt-get install binutils-mingw-w64-x86-64 gcc-mingw-w64-x86-64 -y
+  sudo apt-get update -q
+  sudo apt-get install ncbi-blast+ gcc-multilib -y
+  sudo apt-get install binutils-mingw-w64-i686 gcc-mingw-w64-i686 -y
+  sudo apt-get install binutils-mingw-w64-x86-64 gcc-mingw-w64-x86-64 -y
 else
   brew update
   brew install pango cairo
