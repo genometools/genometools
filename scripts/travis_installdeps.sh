3171a942b94f0aa4fb88c1afd04e545d58b97216
Sascha Steinbiss <sascha@steinbiss.name>
2017-10-12 21:28:22 +0200

    add missing build-deps
    No idea why there were missing in the Travis config in the first
    place...

--- scripts/travis_installdeps.sh
+++ scripts/travis_installdeps.sh
@@ -3,6 +3,7 @@
 echo $TRAVIS_OS_NAME
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   sudo apt-get update -q
+  sudo apt-get install libpango1.0-dev libcairo2-dev
   sudo apt-get install ncbi-blast+ gcc-multilib -y
   sudo apt-get install binutils-mingw-w64-i686 gcc-mingw-w64-i686 -y
   sudo apt-get install binutils-mingw-w64-x86-64 gcc-mingw-w64-x86-64 -y
